a1f93b868121df22998b31b60af5d19b3a0051e4
sanjaykumar <sanjay.kumar@shopkick.com>
2019-10-08 23:33:48 -0400

    Initial commit

---
+++ hashset.py
@@ -0,0 +1,56 @@
+"""
+HashSet implemented using double hashing.
+"""
+class MyHashSet(object):
+    
+    arr = []
+    hash_1 = 25*25*25
+    hash_2 = 4*4*4
+    
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.arr = [None for i in range(self.hash_1)]
+    
+    
+    def add(self, key):
+        """
+        :type key: int
+        :rtype: None
+        """
+        h1 = key % self.hash_1
+        if not self.arr[h1] :
+            self.arr[h1] = [False for i in range(self.hash_2)]
+        self.arr[h1][key % self.hash_2] = True
+    
+    
+    def remove(self, key):
+        """
+        :type key: int
+        :rtype: None
+        """
+        h1 = key % self.hash_1
+        if self.arr[h1] :
+            self.arr[h1][key % self.hash_2] = False
+    
+    
+    
+    
+    def contains(self, key):
+        """
+        Returns true if this set contains the specified element
+        :type key: int
+        :rtype: bool
+        """
+        h1 = key % self.hash_1
+        if self.arr[h1] :
+            return self.arr[h1][key % self.hash_2]
+        return False
+
+
+# Your MyHashSet object will be instantiated and called as such:
+# obj = MyHashSet()
+# obj.add(key)
+# obj.remove(key)
+# param_3 = obj.contains(key)
