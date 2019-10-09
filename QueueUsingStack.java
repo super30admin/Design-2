737f5e081040bd116d5a57df99689b39b81f9251
Akhilesh Borgaonkar <aborgaonkar@onemarketnetwork.com>
2019-10-08 22:38:09 -0700

    optimized queue using stacks

--- QueueUsingStack.java
+++ QueueUsingStack.java
@@ -2,7 +2,7 @@
 Author: Akhilesh Borgaonkar
 Approach: I am using two stacks here. The main stack maintains the sequence in which elements were pushed in queue at any given time.
     Reverse stack is used for poping out the elements from main stack and getting the first element in the main stack.
-Time Complexity: O(1) for push() & O(n) for pop() and peek() methods
+Time Complexity: O(1) for all methods
 Space Complexity: O(n)  where n is number of elements pushed in stack
 LC verified
 */
@@ -11,7 +11,7 @@ class MyQueue {
 
     Stack<Integer> mainStack = new Stack<>();   //main stack to store elements
     Stack<Integer> revStack = new Stack<>();    //stack for reversing the elements while getting first element in mainStack
-    /** Initialize your data structure here. */
+
     public MyQueue() {
         
     }
@@ -21,40 +21,35 @@ class MyQueue {
         mainStack.push(x);
     }
     
+    private void reverseStack(){
+         while(!mainStack.empty())
+                revStack.push(mainStack.pop()); //getting all elements from mainStack and putting in revStack to reverse the order and get the first element pushed in mainStack
+    }
+    
     /** Removes the element from in front of queue and returns that element. */
     public int pop() {
-        int popElement = -1;                    //default value if stack is empty
+        int popElement = -1;            //default value if stack is empty
         
-        if(!mainStack.empty()){
-            while(!mainStack.empty())
-                revStack.push(mainStack.pop()); //getting all elements from mainStack and putting in revStack to reverse the order and get the first element pushed in mainStack
-            
-            popElement = revStack.pop();        //poping the first element in queue
-            
-            while(!revStack.empty())            //reverting the sequence to mainStack
-                mainStack.push(revStack.pop());
-        }
+        if(revStack.empty())
+           reverseStack();              //populate the reverse stack
+        
+        popElement = revStack.pop();    //poping the first element in queue
         return popElement;
     }
     
     /** Get the front element. */
     public int peek() {
-        int peekElement = -1;                   //default value if stack is empty
+        int peekElement = -1;           //default value if stack is empty
         
-        if(!mainStack.empty()){
-            while(!mainStack.empty())
-                revStack.push(mainStack.pop()); //getting all elements from mainStack and putting in revStack to reverse the order and get the first element pushed in mainStack
-            
-            peekElement = revStack.peek();      //peeking the first element in queue w/o poping
-            
-            while(!revStack.empty())        
-                mainStack.push(revStack.pop()); //reverting the sequence to mainStack
-        }
+        if(revStack.empty())
+           reverseStack();              //populate the reverse stack
+        
+        peekElement = revStack.peek();  //peeking the first element in queue w/o poping
         return peekElement;
     }
     
     /** Returns whether the queue is empty. */
     public boolean empty() {
-        return mainStack.empty();               //return true if stack is empty
+        return (mainStack.empty() && revStack.empty());     //return true if stack is empty
     }
 }
