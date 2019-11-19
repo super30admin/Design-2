633d8b49d2637256a7964c795af93a19d4d063fb
Rishika Machina <39758648+RishikaMachina@users.noreply.github.com>
2019-11-19 00:09:16 -0800

    Queue using 2 stacks

--- 1. Queue.py
+++ 1. Queue.py
@@ -1,17 +1,28 @@
 class MyQueue:
     def __init__(self):
-        self.queue = []
+        self.stack1 = []
+        self.stack2 = []
 
-    def push(self, x: int) -> None:
-        self.queue.append(x)
+    def push(self, x):
+        self.stack1.append(x)
 
-    def pop(self) -> int:
-        a = self.queue[0]
-        del self.queue[0]
+    def move(self):
+        if len(self.stack2)!=0:
+            return self.stack2[-1]
+        else:
+            while len(self.stack1)!=0:
+                self.stack2.append(self.stack1.pop())
+            return self.stack2[-1]
+
+    def pop(self):
+        a = self.move()
+        self.stack2.remove(a)
         return a
 
-    def peek(self) -> int:
-        return self.queue[0]
+    def peek(self):
+        return self.move()
 
-    def empty(self) -> bool:
-        return len(self.queue)==0
+    def empty(self):
+        if len(self.stack1)==0 and len(self.stack2)==0:
+            return True
+        return False
