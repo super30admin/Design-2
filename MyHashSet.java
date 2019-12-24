c8f40b95e931f0db4ce8a96a1d3232793219db71
Lohith <a.lohith@yahoo.in>
2019-12-24 14:43:52 -0600

    Design-2 completed

--- MyHashSet.java
+++ MyHashSet.java
@@ -1,30 +1,27 @@
-class MyHashSet {
-    
-    boolean[] arr;
+// Time Complexity : O(1)
+// Space Complexity :O(n)
+// Did this code successfully run on Leetcode : yes
+// Any problem you faced while coding this : couldn't decide what data structure to be used intially
 
+class MyHashSet {
+    private HashMap<Integer, Integer> map = new HashMap();		// HashMap declaration
     /** Initialize your data structure here. */
     public MyHashSet() {
-        arr = new boolean[1000000];
+
     }
-    
-    public void add(int key) {
-        arr[key] = true;
+
+    public void add(int key) {		// using the standard operation to insert key-value pair, value is 1 since we are more concerned about adding only key. Takes O(1) time.
+        map.put(key, 1);
     }
-    
+
     public void remove(int key) {
-        arr[key] = false;
+        map.remove(key);			// using the standard operation to remove the key from the HashMap. Takes O(1) time.
     }
-    
+
     /** Returns true if this set contains the specified element */
     public boolean contains(int key) {
-        return arr[key];
+        if(map.containsKey(key))	// calculates the HashCode and directly checks if the key is present or not, takes O(1) time.
+            return true;
+        return false;
     }
 }
-
-/**
- * Your MyHashSet object will be instantiated and called as such:
- * MyHashSet obj = new MyHashSet();
- * obj.add(key);
- * obj.remove(key);
- * boolean param_3 = obj.contains(key);
- */
--- MyQueue.java
+++ MyQueue.java
@@ -1,58 +1,62 @@
 package com.company;
 
+// Time Complexity :O(2N)
+// Space Complexity :O(2N)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : No
+
 import java.util.Stack;
 
 public class MyQueue {
-    Stack<Integer> stack = new Stack<>();
+    Stack<Integer> stack = new Stack<>(); // Stack declaration
     public MyQueue() {
-        int pointer = 0;
-        int pointerIndex = -1;
+        
     }
 
     /** Push element x to the back of queue. */
     public void push(int x) {
-        stack.push(x);
+        stack.push(x); // push the given element on to the stack which can be done in O(N) time
     }
 
-    public int reverse(int x){
+    public int reverse(int x){ // Since stack is FIFO, created a temp stack to get the first element on the top
         Stack<Integer> temp = new Stack<>();
-        if(x == 0){
+        if(x == 0){ // condition for the peek operation
             while(stack.empty() == false){
-                temp.push(stack.pop());
+                temp.push(stack.pop());		// temp is filled with values from stack, the first element which was added to stack is on top, can be done in O(N) time
             }
-            int topValue = temp.peek();
+            int topValue = temp.peek();		// peek is used to just get the value from the top of temp, i.e front of the queue
             while(temp.empty() == false){
-                stack.push(temp.pop());
+                stack.push(temp.pop());		// temp stack element is now put back into the main stack to maintain the order of the push operation, can be done in O(N) time
             }
-            return topValue;
+            return topValue;				// total time complexity O(N) + O(N) = O(2N)
         }
 
-        if(x == -1){
-            while(stack.empty() == false){
+        if(x == -1){	// condition for the pop operation
+            while(stack.empty() == false){ 	// temp is filled with values from stack, the first element which was added to stack is on top, can be done in O(N) time
                 temp.push(stack.pop());
             }
-            int topValue = temp.pop();
+            int topValue = temp.pop();		// pop is used to remove the value from the top of temp, i.e front of the queue
             while(temp.empty() == false){
-                stack.push(temp.pop());
+                stack.push(temp.pop());		// temp stack element is now put back into the main stack to maintain the order of the push operation, can be done in O(N-1) time
             }
-            return topValue;
+            return topValue;				// total time complexity O(N) + O(N-1) = O(2N)
         }
         return -1;
     }
 
     /** Removes the element from in front of queue and returns that element. */
     public int pop() {
-        return reverse(-1);
+        return reverse(-1);					// reverse function is called with the argument as -1 to indicate pop operation 
     }
 
     /** Get the front element. */
     public int peek() {
-        return reverse(0);
+        return reverse(0);					// reverse function is called with the argument as 0 to indicate peep operation 
     }
 
     /** Returns whether the queue is empty. */
     public boolean empty() {
-        if(stack.empty()){
+        if(stack.empty()){					// checks if the stack is empty or not using the standard operation of stack. Takes O(1) time
             return true;
         }
         return false;
