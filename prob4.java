4550a85ebc4eb77bf9884fafc86b6c6700cef95d
rutulusc <rutulbap@usc.edu>
2019-12-25 13:22:47 -0800

    Solved problem 4

--- prob4.java
+++ prob4.java
@@ -1,8 +1,8 @@
 // 705. Design HashSet
 // S30 Big N Problem #4 {Easy}
-// Time Complexity :
-// Space Complexity :
-// Did this code successfully run on Leetcode :
+// Time Complexity : O(1) for add(),remove(), contains()
+// Space Complexity : worst case O(n*m) n*m=total values
+// Did this code successfully run on Leetcode :Yes
 // Any problem you faced while coding this :
 
 
@@ -10,21 +10,49 @@
 class MyHashSet {
 
     /** Initialize your data structure here. */
+    int buckets = 1000;
+    int bucketItems = 1000;
+    boolean [][] storage = new boolean[buckets][]; //[,,,,,,,,] not [[,,,,,][,,,,,]]
+    //only initialise nested array when req
+    //hash functions
+    private int bucket(int key){
+        return key%buckets;
+    }
+    private int bucketItem(int key){
+        return key/bucketItems;
+    }
+    
     public MyHashSet() {
         
     }
     
-    public void add(int key) {
-        
+    public void add(int key) { // O(1)
+        int bucket = bucket(key);
+        int bucketItem = bucketItem(key);
+        if(storage[bucket] == null){
+            storage[bucket] = new boolean[bucketItems];
+        }
+        storage[bucket][bucketItem]=true;
     }
     
-    public void remove(int key) {
-        
+    public void remove(int key) { //O(1)
+        int bucket = bucket(key);
+        int bucketItem = bucketItem(key);
+        if(storage[bucket] != null){
+            storage[bucket][bucketItem] = false; 
+        }
     }
     
     /** Returns true if this set contains the specified element */
-    public boolean contains(int key) {
-        
+    public boolean contains(int key) { //O(1)
+        int bucket = bucket(key);
+        int bucketItem = bucketItem(key);
+        if(storage[bucket]!=null){
+            return storage[bucket][bucketItem];
+        }
+        else{
+            return false;
+        } 
     }
 }
 
