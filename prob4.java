f55fbef79085ffa400e4777fceeaf00b4739dde9
rutulusc <rutulbap@usc.edu>
2019-12-24 20:07:03 -0800

    Solved problem 3

---
+++ prob4.java
@@ -0,0 +1,37 @@
+// 705. Design HashSet
+// S30 Big N Problem #4 {Easy}
+// Time Complexity :
+// Space Complexity :
+// Did this code successfully run on Leetcode :
+// Any problem you faced while coding this :
+
+
+// Your code here along with comments explaining your approach
+class MyHashSet {
+
+    /** Initialize your data structure here. */
+    public MyHashSet() {
+        
+    }
+    
+    public void add(int key) {
+        
+    }
+    
+    public void remove(int key) {
+        
+    }
+    
+    /** Returns true if this set contains the specified element */
+    public boolean contains(int key) {
+        
+    }
+}
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet obj = new MyHashSet();
+ * obj.add(key);
+ * obj.remove(key);
+ * boolean param_3 = obj.contains(key);
+ */
