df80d75c77a554b7e65d8c12bc9772a01040fd3d
Megha Rajam Rao <megha.rrao90@gmail.com>
2020-02-04 15:36:30 -0800

    Create Problem1_Queue using stacks.py
    Queues using stacks - operations with a single stack

---
+++ Problem1_Queue using stacks.py
@@ -0,0 +1,49 @@
+class MyQueue(object):
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.s1 = []
+        self.s2 = []
+        
+
+    def push(self, x):
+        """
+        Push element x to the back of queue.
+        :type x: int
+        :rtype: None
+        """
+        return self.s1.append(x)
+
+    def pop(self):
+        """
+        Removes the element from in front of queue and returns that element.
+        :rtype: int
+        """
+        return self.s1.pop(0)
+
+    def peek(self):
+        """
+        Get the front element.
+        :rtype: int
+        """
+        return self.s1[0]
+        
+
+    def empty(self):
+        """
+        Returns whether the queue is empty.
+        :rtype: bool
+        """
+        return not self.s1
+        
+        
+
+
+# Your MyQueue object will be instantiated and called as such:
+# obj = MyQueue()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.peek()
+# param_4 = obj.empty()
