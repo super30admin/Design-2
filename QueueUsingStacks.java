a596b5f0fe99686749487e7e60ff0f8e3b0b8be6
SomiDeev <sowmyadvn.92@gmail.com>
2020-02-04 22:08:18 -0800

    HashSet and QueueUsingStacks After class

--- QueueUsingStacks.java
+++ QueueUsingStacks.java
@@ -1,3 +1,60 @@
+// Time Complexity : O(1) push, O(1) average pop, O(1) average peek and O(1) isEmpty()
+// Space Complexity : O(N) where N is number of elements pushed to stack
+// Did this code successfully run on Leetcode :yes
+// Any problem you faced while coding this : I did transfer values from s1 to s2 for every push operation before class, but learnt that it is wise to do transfers during pop and not during push due to frequency of transfers. 
+
+
+/*
+1. Always push values to stack1
+2. When pop or peek is performed
+	i. If stack2 is not empty, remove or return stack2's top 
+	ii. If stack2 is empty, transfer from stack1 to stack2 and then return stack2's top
+3. If both stack1 and stack2 are empty, then isEmpty() should return true, else false
+*/
+
+class MyQueue {
+
+    Stack<Integer> stack1, stack2;
+    
+    public MyQueue() {
+        stack1 = new Stack<>();
+        stack2 = new Stack<>();
+    }
+    
+    /** Push element x to the back of queue. */
+    public void push(int x) {
+        stack1.push(x);
+    }
+    
+    /** Removes the element from in front of queue and returns that element. */
+    public int pop() {
+        if(stack2.isEmpty()) {
+            transfer(stack1,stack2);
+        }
+        return stack2.pop();
+    }
+    
+    /** Get the front element. */
+    public int peek() {
+        if(stack2.isEmpty()) {
+            transfer(stack1,stack2);
+        }
+        return stack2.peek();
+    }
+    
+    /** Returns whether the queue is empty. */
+    public boolean empty() {
+        return stack1.isEmpty() && stack2.isEmpty();
+    }
+    
+    private void transfer(Stack<Integer> stack1, Stack<Integer> stack2) {
+        while(!stack1.isEmpty()) 
+            stack2.push(stack1.pop());
+    }
+}
+
+// Implementation before class: 
+
 class MyQueue {
 
     Stack<Integer> stack1, stack2;
