16e3f223c418cb0a2a36d055e550267268806289
Megha Rajam Rao <megha.rrao90@gmail.com>
2020-03-12 04:39:19 -0700

    Update Design2_Exercise2.py
    Please review

--- Design2_Exercise_1.py
+++ Design2_Exercise_1.py
@@ -1,71 +1,64 @@
-// Time Complexity : Given for each operation below
-	// Space Complexity : O(2n) as 2 stacks are being used
-	// Did this code successfully run on Leetcode : Yes
-	// Any problem you faced while coding this : I was uncertain intially and used a single stack. Revising the same after class.
+// Time Complexity : push, peek, empty - O(1), pop - O(n), 
+// Space Complexity : O(n)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : Questions - As we are using 2 stack, would space complexity be O(2n)? For pop, can we call
+time complexity as amortized O(1)?
 	
 
-	// Your code here along with comments explaining your approach - For pop and peek operation, we utilized a secondary stack to empty the 
-	elements of the initial stack and reverse the order during the process. Therefore, the first element in initial stack, which turns out 
-	to be the last element in the secondary stack, is removed and fetched using pop function. As a result, we had to incorporate an 
-	additional step in push operation to empty the contents of secondary stack back into the initial stack before inputting a new value 
-	using append function. For empty operation, we used 'not' operator to check if both the stacks are empty.
-	 
+// Your code here along with comments explaining your approach - 
+1. For push, we simply append the value to stack 1.
+2. Initially, for pop and peek operation, we utilized a secondary stack s2 (nicknamed the "queue" stack as it resembles queue) to empty the 
+elements of the initial stack and reverse the order during the process. Therefore, the first element in initial stack, which turns out 
+to be the last element in the secondary stack, is removed and fetched using pop function. 
+3. Thereafter, I tried using a variable to store the peek value and reduce it's time complexity from O(n) in worst case to O(1).
+4. For empty operation, we used 'not' operator to check if both the stacks are empty.
 	
+class MyQueue:
 
-	class MyQueue(object):
-	
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.s1 = []
+        self.s2 = []
+        self.first = None
 
-	    def __init__(self):
-	        """
-	        Initialize your data structure here.
-	        """
-	        self.s1 = []
-	        self.s2 = []
-	
+    def push(self, x: int) -> None:
+        """
+        Push element x to the back of queue.
+        """
+        if not self.s1:
+            self.first = x
+        self.s1.append(x)
+            
 
-	    def push(self, x):
-	        """
-	        Push element x to the back of queue.
-	        :type x: int
-	        :rtype: None
-	        """
-	        # O(n) - linear time complexity in worst-case scenario, which is less frequent
-	        while len(self.s2) != 0:
-	            self.s1.append(self.s2.pop())
-	        
-	        # O(1) - Constant time Complexity for append operation (best case scenario, which may also be the average time complexity)
-	        return self.s1.append(x)
-	
+    def pop(self) -> int:
+        """
+        Removes the element from in front of queue and returns that element.
+        """
+        if not self.s2:
+            while self.s1:
+                self.s2.append(self.s1.pop())
+        return self.s2.pop()
+        
 
-	    def pop(self):
-	        """
-	        Removes the element from in front of queue and returns that element.
-	        :rtype: int
-	        """
-	        # O(n) - Linear time Complexity 
-	        while len(self.s1) != 0: 
-	            self.s2.append(self.s1.pop())
-	        return self.s2.pop()
-	      
-	    
-	    def peek(self):
-	        """
-	        Get the front element.
-	        :rtype: int
-	        """
-	        # O(n) - Linear time Complexity 
-	        while len(self.s1) != 0: 
-	            self.s2.append(self.s1.pop())
-	        peek = self.s2.pop()
-	        self.s2.append(peek)
-	        return peek
-	  
-	
+    def peek(self) -> int:
+        """
+        Get the front element.
+        """
+        """
+        if not self.s2:
+            while self.s1:
+                self.s2.append(self.s1.pop())
+        return self.s2[-1]
+        """
+        if self.s2:
+            return self.s2[-1]   #If this isn't allowed, we can pop and assign to a variable and subsequently append it to s2. Return variable.
+        return self.first
 
-	    def empty(self):
-	        """
-	        Returns whether the queue is empty.
-	        :rtype: bool
-	        """
-	        # O(1) - constant time Complexity 
-	        return not self.s1 and not self.s2
+
+    def empty(self) -> bool:
+        """
+        Returns whether the queue is empty.
+        """
+        return not self.s1 and not self.s2
