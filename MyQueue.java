2b7d41463d655515b9b5fe4667048c5b37e44926
unknown <harale.s@husky.neu.edu>
2020-05-25 16:49:13 -0400

    Added Time & Space Complexity

--- MyQueue.java
+++ MyQueue.java
@@ -1,4 +1,14 @@
 import java.util.Stack;
+//Time Complexity : 
+//Push O(1)
+//Pop O(n)
+//Peek O(n)
+//Space Complexity : O(1)
+//Did this code successfully run on Leetcode : Yes
+//Any problem you faced while coding this : No
+
+
+//Your code here along with comments explaining your approach
 
 public class MyQueue {
     
