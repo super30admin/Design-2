0b2ea81185f2513a0ea69536f8c4f6b0f52c9e52
Samarth Juneja <samarthj@buffalo.edu>
2020-05-27 00:29:32 +0530

    QueueUtilStacks.py simplified pop() method

--- QueueUtilStacks.py
+++ QueueUtilStacks.py
@@ -1,3 +1,9 @@
+# Time Complexity : O(1)
+# Space Complexity : Worst Case, O(self.bucketNum * self.bucketNum)
+# Did this code successfully run on Leetcode : Yes!
+# Any problem you faced while coding this : No
+
+# Your code here along with comments explaining your approach
 class MyQueue(object):
 
     def __init__(self):
@@ -8,9 +14,7 @@ class MyQueue(object):
         self.inStack.append(x)
 
     def pop(self):
-        if len(self.outStack) == 0:
-            while len(self.inStack) != 0:
-                self.outStack.append(self.inStack.pop())
+        self.peek()
         return self.outStack.pop()
 
     def peek(self):
