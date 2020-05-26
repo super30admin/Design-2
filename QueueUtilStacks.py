0db8d918747acc33c96b85dc233f6233de110a50
Samarth Juneja <samarthj@buffalo.edu>
2020-05-27 00:37:55 +0530

    QueueUtilStacks.py, added time & space complexity with comments.

--- QueueUtilStacks.py
+++ QueueUtilStacks.py
@@ -1,5 +1,5 @@
-# Time Complexity : O(1)
-# Space Complexity : Worst Case, O(self.bucketNum * self.bucketNum)
+# Time Complexity : push: O(1), pop & peek: O(n), empty: O(1)
+# Space Complexity : O(n), where n is the number of elements in both stacks
 # Did this code successfully run on Leetcode : Yes!
 # Any problem you faced while coding this : No
 
@@ -7,17 +7,22 @@
 class MyQueue(object):
 
     def __init__(self):
+        # always appending to this stack
         self.inStack = []
+        # always popping from this stack
         self.outStack = []
 
     def push(self, x):
         self.inStack.append(x)
 
     def pop(self):
+        # calling peek because it does not mutate the containers
+        # and has the same underlying logic to pop before returning
         self.peek()
         return self.outStack.pop()
 
     def peek(self):
+        # shifting all elements from inStack to outStack and returning the top of outStack.
         if len(self.outStack) == 0:
             while len(self.inStack) != 0:
                 self.outStack.append(self.inStack.pop())
@@ -25,6 +30,7 @@ class MyQueue(object):
 
 
     def empty(self):
+        # if both stacks are empty.
         return len(self.inStack) == 0 and len(self.outStack) == 0
 
 # Your MyQueue object will be instantiated and called as such:
