b2bddd65b2e5b030b2c89b7c8b674a8973762488
Krutik Tatibandwale <krutik@Krutiks-MacBook-Pro.local>
2020-05-26 17:22:00 -0400

    updated the solution... implemented hash function and used 2D array instead of 1D in the updated solution

--- MyHashSet.java
+++ MyHashSet.java
@@ -8,24 +8,51 @@
 
 class MyHashSet {
 
-    boolean arr[];
+    private boolean storage[][];
+    private int buckets;
+    private int bucketItems;
     /** Initialize your data structure here. */
     public MyHashSet() {
-        arr = new boolean[1000001];
+        buckets = 1001;
+        bucketItems = 1001 ;
+        storage = new boolean[buckets][];
+    }
+    
+    private int bucket(int key) {
+        return key % buckets;
+    }
+    
+    private int bucketItem(int key) {
+        return key / bucketItems;
     }
     
     public void add(int key) {
-        arr[key] = true;
+        int bucket = bucket(key);
+        int bucketItem = bucketItem(key);
+        if (storage[bucket] == null) {
+            storage[bucket] = new boolean[bucketItems];   
+        }
+        storage[bucket][bucketItem] = true;
     }
     
     public void remove(int key) {
-        arr[key] = false;
+        int bucket = bucket(key);
+        int bucketItem = bucketItem(key);
+        if (storage[bucket] != null) {
+            storage[bucket][bucketItem] = false;
+        }
     }
     
     /** Returns true if this set contains the specified element */
     public boolean contains(int key) {
-        if (arr[key] == true) return true;
-        else return false;
+        int bucket = bucket(key);
+        int bucketItem = bucketItem(key);
+        
+        if (storage[bucket] != null) {
+            return storage[bucket][bucketItem];
+        }
+        
+        return false;
     }
 }
 
