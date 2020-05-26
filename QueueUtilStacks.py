523f761d2b8a1eaff86b9d3a79f0a5487ad645ec
Samarth Juneja <samarthj@buffalo.edu>
2020-05-27 00:03:15 +0530

    QueueUtilStacks.py with two, in and out stack.

--- QueueUtilStacks.py
+++ QueueUtilStacks.py
@@ -1,38 +1,32 @@
 class MyQueue(object):
 
     def __init__(self):
-        """
-        Initialize your data structure here.
-        """
+        self.inStack = []
+        self.outStack = []
 
     def push(self, x):
-        """
-        Push element x to the back of queue.
-        :type x: int
-        :rtype: None
-        """
+        self.inStack.append(x)
 
     def pop(self):
-        """
-        Removes the element from in front of queue and returns that element.
-        :rtype: int
-        """
+        if len(self.outStack) == 0:
+            while len(self.inStack) != 0:
+                self.outStack.append(self.inStack.pop())
+        return self.outStack.pop()
 
     def peek(self):
-        """
-        Get the front element.
-        :rtype: int
-        """
+        if len(self.outStack) == 0:
+            while len(self.inStack) != 0:
+                self.outStack.append(self.inStack.pop())
+        return self.outStack[len(self.outStack)-1]
+
 
     def empty(self):
-        """
-        Returns whether the queue is empty.
-        :rtype: bool
-        """
+        return len(self.inStack) == 0 and len(self.outStack) == 0
 
 # Your MyQueue object will be instantiated and called as such:
-# obj = MyQueue()
-# obj.push(x)
-# param_2 = obj.pop()
-# param_3 = obj.peek()
-# param_4 = obj.empty()
+obj = MyQueue()
+obj.push(1)
+obj.push(2)
+print obj.pop()
+print obj.peek()
+print obj.empty()
