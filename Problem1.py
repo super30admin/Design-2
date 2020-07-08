c10c6a1a7eaf2de51d1745e0e67acf5e14493fa0
hackerrank <garima.adlakha2450@gmail.com>
2020-07-08 14:29:47 -0700

    Completed Design-2  Problem 2

--- Problem1.py
+++ Problem1.py
@@ -1,48 +1,65 @@
 class MyHashSet:
 
+    
     def __init__(self):
         """
         Initialize your data structure here.
         """
-        self.list_HashSet=[]
+        self.buckets=1000
+        self.bucketItems=1000
+        self.storage = [[] for _ in range(self.buckets)]
+        
+    def bucket(self,value):
+        return value%self.buckets
+    
+    def bucketItem(self,value):
+        return value//self.buckets
 
-    def add(self, key) :
-        if key not in self.list_HashSet:
-            self.list_HashSet.append(key)
-            print("Element added in Hashset...Current HashSet")
-            print(self.list_HashSet)
-
-    def remove(self, key) :
-        if not self.list_HashSet:
-            print("HashSet Empty")
-        elif key in self.list_HashSet:
-            self.list_HashSet.remove(key)
-            print("Element removed from Hashset...Current HashSet")
-            print(self.list_HashSet)
-        else:
-            print("key not found")
+    def add(self, value):
+        """
+        :type key: int
+        :rtype: None
+        """
+        bucket=self.bucket(value)
+        bucketItem=self.bucketItem(value)
+        if not self.storage[bucket]:
             
+            self.storage[bucket] = [False]*self.bucketItems
+            
+        self.storage[bucket][bucketItem] = True
+        #print(self.storage)
 
-    def contains(self, key) :
+    def remove(self, value):
         """
-        Returns true if this set contains the specified element
+        :type key: int
+        :rtype: None
         """
-        return key in self.list_HashSet
+        bucket=self.bucket(value)
+        bucketItem=self.bucketItem(value)
+        if self.storage[bucket] and self.storage[bucket][bucketItem]:
+            self.storage[bucket][bucketItem]=False
         
 
+    def contains(self, value):
+        """
+        Returns true if this set contains the specified element
+        :type key: int
+        :rtype: bool
+        """
+        bucket=self.bucket(value)
+        bucketItem=self.bucketItem(value)
+        return self.storage[bucket] and self.storage[bucket][bucketItem]
 
 # Your MyHashSet object will be instantiated and called as such:
 obj = MyHashSet()
 obj.add(1)
 obj.add(2)
-obj.add(1)
-obj.add(3)
-pop_element=2
-print("Element to be removed")
-print(pop_element)
-obj.remove(pop_element)
-param_3 = obj.contains(3)
-print("If element present in hashset??")
+obj.add(2002)
+obj.add(3001)
+obj.add(100)
+obj.add(5002)
+obj.remove(100)
+param_3 = obj.contains(2002)
 print(param_3)
 
 #Time Complexity of add,pop,contains:O(1) Overall :O(1)
