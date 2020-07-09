571e95a8015410b20cbfabec9760fbfdc5e55f0e
Arpit Sharma <arpit317@gmail.com>
2020-07-08 17:38:49 -0700

    Update design-hashset.py

--- design-hashset.py
+++ design-hashset.py
@@ -1,3 +1,16 @@
+  
+# Time Complexity : O(1) for all the three operations
+# Space Complexity : O(n^2), may be, as we have an array of arrays, not sure
+# Did this code successfully run on Leetcode : yes
+# Any problem you faced while coding this : No
+
+#Approach:
+#1. We will take an array of arrays, with size = sqrt(our range), we will use hashing to store the elements of hashset in our array.
+#2. As we receive two elements with same hash for the array, we will use another hashing algorithm to store the element in our sub array. (like H = key%1000 for 
+# main array and H2 = key/1000 for sub array
+#3. Now, as we have an array of arrays with hashes for every index in this 2-D array, we know that which element will go where, so instead of storing the element 
+# itself, we can just store a boolean or a flag at the index like a True of False, which indicates that whether the element is there in the HashSet or not.
+
 class MyHashSet(object):
 
     def __init__(self):
