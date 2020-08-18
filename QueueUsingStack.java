2451f85fe7361d6bacb2a73a8c13612523c5468c
laxmikantbpandhare <laxmikantbhaskar.pandhare@sjsu.edu>
2020-08-18 12:55:53 -0700

    added explnanation

--- QueueUsingStack.java
+++ QueueUsingStack.java
@@ -1,4 +1,14 @@
 import java.util.Stack;
+
+    /*  Explanation
+    Time Complexity for operators : o(1)   
+    Extra Space Complexity for operators : o(n) .. Using two stacks  
+    Did this code successfully run on Leetcode : Yes
+    Any problem you faced while coding this : No
+    The implemntation consists of two stack, the first stack will have all the elements in LIFO order while same element 
+    will be popped from first stack and inserted into second. That's way the second stack works like Queue FIFO. 
+    */
+
 public class QueueUsingStack{
     Stack<Integer> pushStack;
     Stack<Integer> popAndPeekStack;
