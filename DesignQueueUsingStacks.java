1ad0ed31ffaf6a03f01a7be70644ddc5bd2c1521
Manav Ahlawat <manavahlawat95@gmail.com>
2020-08-17 21:16:55 -0400

    Design Question 2

---
+++ DesignQueueUsingStacks.java
@@ -0,0 +1,33 @@
+class MyQueue {
+
+    Stack<Integer> input = new Stack();
+    Stack<Integer> output = new Stack();
+    
+    public void push(int x) {
+        input.push(x);
+    }
+
+    public void pop() {
+        peek();
+        output.pop();
+    }
+
+    public int peek() {
+        if (output.empty())
+            while (!input.empty())
+                output.push(input.pop());
+        return output.peek();
+    }
+
+    public boolean empty() {
+        return input.empty() && output.empty();
+    }
+}
+
+// Time Complexity : O(n)
+// Space Complexity : O(n)
+// Did this code successfully run on Leetcode : No
+// Any problem you faced while coding this :
+
+
+// Your code here along with comments explaining your approach
