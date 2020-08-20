c1bec3ae10aaf11cdc6d40ad5ca0ce2488e37b23
anudeepareddy <40484243+anudeepareddy@users.noreply.github.com>
2020-08-20 17:02:26 -0500

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -47,6 +47,7 @@ class MyQueue {
     }
 }
 
+
 /**
  * Your MyQueue object will be instantiated and called as such:
  * MyQueue obj = new MyQueue();
@@ -54,4 +55,65 @@ class MyQueue {
  * int param_2 = obj.pop();
  * int param_3 = obj.peek();
  * boolean param_4 = obj.empty();
+ */
+
+
+//Design Hashset
+// Time Complexity :O(1)
+// Space Complexity :O(1)
+// Did this code successfully run on Leetcode :yes
+// Any problem you faced while coding this :No
+
+// Your code here along with comments explaining your approach
+class MyHashSet {
+
+    private int bucket =1000;
+    private int bucketitem=1000;
+    private int hashfunction1(int key)
+    {
+        return key%bucket;
+    }
+    private int hashfunction2(int key){
+        return key/bucketitem;
+    }
+    private boolean[][] hashset;
+    /** Initialize your data structure here. */
+    public MyHashSet() {
+        hashset= new boolean[bucket][];
+    }
+    
+    public void add(int key) {
+        int index1=hashfunction1(key);
+        int index2=hashfunction2(key);
+        if(hashset[index1]==null)
+        {
+            hashset[index1]=new boolean[bucketitem];
+        }
+        hashset[index1][index2]=true;
+    }
+    
+    public void remove(int key) {
+        int index1=hashfunction1(key);
+        int index2=hashfunction2(key);
+        if(hashset[index1]==null)
+        {
+           return;
+        }
+        hashset[index1][index2]=false;
+    }
+    
+    /** Returns true if this set contains the specified element */
+    public boolean contains(int key) {
+        int index1=hashfunction1(key);
+        int index2=hashfunction2(key);
+        return hashset[index1]!=null && hashset[index1][index2];
+    }
+}
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet obj = new MyHashSet();
+ * obj.add(key);
+ * obj.remove(key);
+ * boolean param_3 = obj.contains(key);
  */
