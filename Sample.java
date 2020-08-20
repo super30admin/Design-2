bae0ba7100ae965a9fa7b20f4fbb6d7b6a3b503a
Priyanka Maheshwari <maheshwari.pr@northeastern.edu>
2020-08-19 21:08:31 -0400

    Updated Problem 2 solution using the approach discussed in class

--- Sample.java
+++ Sample.java
@@ -77,27 +77,51 @@ class MyQueue {
 // Your code here along with comments explaining your approach
 // Implementing HashSet using HashMap
  class MyHashSet {
-    Map<Integer, Integer> hashSet;
+    
+    private int bucket = 1000;
+    private int bucketItem = 1000;
+    
+    private int hashFunc1(int key) {
+        return key % bucket;
+    }
+    
+    private int hashFunc2(int key) {
+        return key / bucketItem;
+    }
+    
+    private boolean[][] hashSet;
+    
     /** Initialize your data structure here. */
     public MyHashSet() {
-        hashSet = new HashMap<>();
+        hashSet = new boolean[bucket][];
     }
     
     public void add(int key) {
-        if (!hashSet.containsKey(key)) {
-            hashSet.put(key, 1);
+        int index1 = hashFunc1(key);
+        int index2 = hashFunc2(key);
+        
+        if (hashSet[index1] == null) {
+            hashSet[index1] = new boolean[bucketItem];
         }
+        hashSet[index1][index2] = true;
     }
     
     public void remove(int key) {
-        if (hashSet.containsKey(key)) {
-            hashSet.remove(key);
+        int index1 = hashFunc1(key);
+        int index2 = hashFunc2(key);
+        
+        if (hashSet[index1] == null) {
+            return;
         }
+        hashSet[index1][index2] = false;
     }
     
     /** Returns true if this set contains the specified element */
     public boolean contains(int key) {
-        return hashSet.containsKey(key);
+        int index1 = hashFunc1(key);
+        int index2 = hashFunc2(key);
+        
+        return hashSet[index1] != null && hashSet[index1][index2];
     }
 }
 
