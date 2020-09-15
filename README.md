af716eb398aeba0392a9dc14c4100db41203f9fe
Piyush Kurkure <piyushk10@gmail.com>
2020-09-15 12:37:56 -0700

    Completed Design2

--- README.md
+++ README.md
@@ -1,9 +1,22 @@
 # Design-2
 
 ## Problem 1: (https://leetcode.com/problems/implement-queue-using-stacks/)
+Implement the following operations of a queue using stacks.
 
+push(x) -- Push element x to the back of queue.<br>
+pop() -- Removes the element from in front of queue.<br>
+peek() -- Get the front element.<br>
+empty() -- Return whether the queue is empty.<br>
 
 ## Problem 2:(https://leetcode.com/problems/design-hashset/)
+
+Design a HashSet without using any built-in hash table libraries.
+To be specific, your design should include these functions:
+
+add(value): Insert a value into the HashSet. <br>
+contains(value) : Return whether the value exists in the HashSet or not.<br>
+remove(value): Remove a value in the HashSet. If the value does not exist in the HashSet, do nothing.<br>
+
 
 
 
---
+++ queue_using_stacks.py
@@ -0,0 +1,39 @@
+# Time Complexity: O(n) for push, O(1) for other operations
+# Space Complexity: O(n) for storing the array.
+
+# Did this code successfully run on Leetcode :Yes
+# Any problem you faced while coding this : No
+
+
+class MyQueue:
+
+    def __init__(self):
+        
+        self.stack1 = []
+        self.stack2 = []
+        
+    def push(self, x: int) -> None:
+        
+        while self.stack1:
+            self.stack2.append(self.stack1.pop())
+        self.stack1.append(x)
+        while self.stack2:
+            self.stack1.append(self.stack2.pop())
+        
+    def pop(self) -> int:
+        
+        return self.stack1.pop()
+
+    def peek(self) -> int:
+        return self.stack1[-1]
+
+    def empty(self) -> bool:
+        return not self.stack1
+
+
+# Your MyQueue object will be instantiated and called as such:
+# obj = MyQueue()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.peek()
+# param_4 = obj.empty()
