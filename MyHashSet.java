78ea8fba442512e461ade4e2ff1afec92e0eb4a7
Pratik Dhumal <pratik.dhumal@netapp.com>
2020-10-20 20:29:09 -0700

    improved previous solution using binary array

--- MyHashSet.java
+++ MyHashSet.java
@@ -5,11 +5,17 @@
 
 
 // Your code here along with comments explaining your approach
+/*
+* 2 things are fixe elements range and array size
+   So we can keep boolean array
+   when we add element we can make its index true
+   when we remove element we can make its index false
+* */
 
 
 class MyHashSet {
 
-    int[] arr=new int[10000];
+    boolean[] arr=new boolean[1000000];
     int index=0;
 
 
@@ -19,26 +25,23 @@ class MyHashSet {
     }
 
     public void add(int key) {
-        arr[index]=key;
-        index++;
+        arr[key]=true;
     }
 
     public void remove(int key) {
-
-        for(int i=0;i<index;i++)
-            if(arr[i]==key)
-            {
-                arr[i]=-1;
-            }
+        arr[key]=false;
     }
 
     /** Returns true if this set contains the specified element */
     public boolean contains(int key) {
-        for(int i=0;i<arr.length;i++)
-        {
-            if(arr[i]==key)
-                return true;
-        }
-        return false;
+        return arr[key];
     }
 }
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet obj = new MyHashSet();
+ * obj.add(key);
+ * obj.remove(key);
+ * boolean param_3 = obj.contains(key);
+ */
