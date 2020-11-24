ee2f18109dd3ecce43c03ef8943a237f9b814ce8
Rishi Raj <rishiraj824@gmail.com>
2020-11-24 03:56:06 -0500

    Create queue-using-stacks.java

---
+++ queue-using-stacks.java
@@ -0,0 +1,49 @@
+Time Complexity - O(1) for push (amortized), pop (amortized), peek (amortized), isEmpty
+Space Complexity - O(N), N being the size of the elements
+
+class MyQueue {
+    
+    Stack<Integer> s1;
+    Stack<Integer> s2;
+
+    /** Initialize your data structure here. */
+    public MyQueue() {
+        s1 = new Stack<Integer>();
+        s2 = new Stack<Integer>();
+    }
+    
+    /** Push element x to the back of queue. */
+    public void push(int x) {
+        s1.push(x);
+    }
+    
+    /** Removes the element from in front of queue and returns that element. */
+    public int pop() {
+        peek();
+        return s2.pop();
+    }
+    
+    /** Get the front element. */
+    public int peek() {
+        if(s2.isEmpty()) {
+            while(!s1.isEmpty()) {
+                s2.push(s1.pop());
+            }
+        }
+        return s2.peek();
+    }
+    
+    /** Returns whether the queue is empty. */
+    public boolean empty() {
+        return s1.isEmpty() && s2.isEmpty();
+    }
+}
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue obj = new MyQueue();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.peek();
+ * boolean param_4 = obj.empty();
+ */
