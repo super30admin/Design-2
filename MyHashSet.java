85deb6ef714cbd457840b7df94fcc7427a428e68
shrijeetrupnar <shrijeetrupnar777@gmail.com>
2020-12-23 14:31:24 -0500

    completed Design -2

--- MyHashSet.java
+++ MyHashSet.java
@@ -4,60 +4,64 @@ import java.util.List;
 
 public class MyHashSet {
 	
-		    /** Initialize your data structure here. */
-	    
-	            
-	        private final int MAX_VALUE=1000000;
-	        private final int ARRAY_SIZE=100;
-	        private List<List<Integer>> parentList;
-	    
-	    
-	    public MyHashSet() {
-	        
-	        parentList = new ArrayList<>(ARRAY_SIZE);
-	        for(int i=0;i<ARRAY_SIZE;i++)
-	            parentList.add(null);     
-	        
-	    }
-	    
-	    public void add(int key) {
-	        
-	        int index= key % ARRAY_SIZE;
-	        List<Integer> childList= parentList.get(index);
-	        if(childList == null){
-	            List<Integer> list= new LinkedList();
-	            list.add(key);
-	            parentList.set(index,list);
-	        }
-	        else{
-	            
-	            if(!childList.contains(key))
-	                    childList.add(key);
-	        }
-	     
-	    }
-	    
-	    public void remove(int key) {
-	        
-	        int index= key % ARRAY_SIZE;
-	        List<Integer> childList= parentList.get(index);
-	        if(childList != null){
-	            
-	            childList.remove(Integer.valueOf(key));
-	        }
-	        
-	        
-	    }
-	    
-	    /** Returns true if this set contains the specified element */
-	    public boolean contains(int key) {
-	        
-	        int index= key % ARRAY_SIZE;
-	        List<Integer> childList= parentList.get(index);
-	     
-	        return (childList!=null && childList.contains(key));
-	        
-	     }
+
+    /** Initialize your data structure here. */
+    
+    private int bucket;
+    private int bucketItem;
+    private boolean[][] storage;
+    
+    
+    public MyHashSet() {
+        
+        this.bucket=1000;
+        this.bucketItem=1001;
+        storage= new boolean[bucket][];
+        
+    }
+    
+    public void add(int key) {
+        
+        int index= key % bucket;
+        int indexTwo= key / bucketItem;
+        
+        if(storage[index]==null){
+            
+            storage[index]= new boolean[bucketItem];
+            
+            
+        }
+        
+        storage[index][indexTwo]= true;
+        
+    }
+    
+    public void remove(int key) {
+        
+        int index= key % bucket;
+        int indexTwo= key / bucketItem;
+        
+        if(storage[index]==null){
+            return;
+        }
+        
+        storage[index][indexTwo] =false;
+        
+        
+    }
+    
+    /** Returns true if this set contains the specified element */
+    public boolean contains(int key) {
+        
+           int index= key % bucket;
+           int indexTwo= key / bucketItem;
+        
+           if(storage[index]==null)
+               return false;
+        
+        return storage[index][indexTwo];
+        
+    }
 	
 	
 
