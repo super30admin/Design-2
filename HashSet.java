d623c001a184690059d2774ead8659a7de9b6d65
Ashish Mishra <ashishk861@gmail.com>
2020-12-23 17:45:09 -0500

    Update HashSet.java

--- HashSet.java
+++ HashSet.java
@@ -1,5 +1,5 @@
 // Time Complexity :O(log(m*n))
-// Space Complexity :O(1)
+// Space Complexity :all the operation will take O(1)
 // Did this code successfully run on Leetcode :yes
 // Any problem you faced while coding this :mapping the the midpoint element to matrix.
 
@@ -7,39 +7,54 @@
 // Your code here along with comments explaining your approach
 
 //one way is the brute force approach which will take O(n^2)
-class SolutionBrute {
-    public boolean searchMatrix(int[][] matrix, int target) {
-        for(int i=0;i<matrix.length;i++){
-            for(int j=0;j<matrix[i].length;j++){
-                if(matrix[i][j]==target){
-                    return true;
-                }
-            }
-        }
-        return false;
-    }
+class MyHashSet {
+
+   int firstBucket;
+	int secondBucket;
+	boolean[][] arr;
+
+	public int getFirstBucket(int value) {
+		return (value % firstBucket);
+	}
+
+	public int getSecondBucket(int value) {
+		return (value / secondBucket);
+	}
+
+	public MyHashSet() {
+		firstBucket = 1000;
+		secondBucket = 1000;
+		arr = new boolean[firstBucket][];
+	}
+
+	public void add(int key) {
+		if (arr[getFirstBucket(key)] == null) {
+			arr[getFirstBucket(key)] = new boolean[secondBucket];
+			
+		} 
+			arr[getFirstBucket(key)][getSecondBucket(key)] = true;
+		
+	}
+
+	public void remove(int key) {
+		if (arr[getFirstBucket(key)] != null) {
+			arr[getFirstBucket(key)][getSecondBucket(key)] = false;
+		}
+	}
+
+	/** Returns true if this set contains the specified element */
+	public boolean contains(int key) {
+		if (arr[getFirstBucket(key)] != null) {
+			return (arr[getFirstBucket(key)][getSecondBucket(key)]);
+		}
+		return false;
+	}
 }
-//But as it is given that the rows of the matrix are sorted.So we can employ binary search here.
-class Solution {
-    public boolean searchMatrix(int[][] matrix, int target) {
-        if(matrix.length==0){
-            return false;
-        }
-        int start=0;
-        int end=(matrix.length*matrix[0].length)-1;
-        int midPoint=0;
-        int element=0;
-        while(start<=end){
-             midPoint=start+(end-start)/2;
-             element=matrix[midPoint/matrix[0].length][midPoint%matrix[0].length];
-            if(element==target){
-                return true;
-            }else if(target<element){
-                end=midPoint-1;
-            }else if(target>element){
-                start=midPoint+1;
-            }
-        }
-        return false;
-    }
-}
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet obj = new MyHashSet();
+ * obj.add(key);
+ * obj.remove(key);
+ * boolean param_3 = obj.contains(key);
+ */
