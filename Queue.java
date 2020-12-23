8b580d877843b7bb45c4887d2605ce85dc7f5014
Ashish Mishra <ashishk861@gmail.com>
2020-12-23 17:45:06 -0500

    Update Queue.java

--- Queue.java
+++ Queue.java
@@ -1,4 +1,7 @@
-// Time Complexity :O(log(m*n))
+// Time Complexity :
+//push operation :O(1)
+//peek operation :O(n)
+//pop operation :O(n)
 // Space Complexity :O(1)
 // Did this code successfully run on Leetcode :yes
 // Any problem you faced while coding this :mapping the the midpoint element to matrix.
@@ -6,40 +9,62 @@
 
 // Your code here along with comments explaining your approach
 
-//one way is the brute force approach which will take O(n^2)
-class SolutionBrute {
-    public boolean searchMatrix(int[][] matrix, int target) {
-        for(int i=0;i<matrix.length;i++){
-            for(int j=0;j<matrix[i].length;j++){
-                if(matrix[i][j]==target){
-                    return true;
-                }
+//We can inmplement th solution using two stacks
+class MyQueue {
+    Stack<Integer> inStack;
+    Stack<Integer> outStack;
+
+    /** Initialize your data structure here. */
+    public MyQueue() {
+        inStack=new Stack<Integer>();
+        outStack=new Stack<Integer>();
+    }
+    
+    /** Push element x to the back of queue. */
+    public void push(int x) {
+        inStack.push(x);
+    }
+    
+    /** Removes the element from in front of queue and returns that element. */
+    public int pop() {
+        if(!outStack.isEmpty()){
+            return outStack.pop();
+        }else{
+            while(!inStack.isEmpty()){
+                outStack.push(inStack.pop());
             }
         }
-        return false;
+        return outStack.pop();
     }
-}
-//But as it is given that the rows of the matrix are sorted.So we can employ binary search here.
-class Solution {
-    public boolean searchMatrix(int[][] matrix, int target) {
-        if(matrix.length==0){
+    
+    /** Get the front element. */
+    public int peek() {
+        if(!outStack.isEmpty()){
+            return outStack.peek();
+        }else{
+            while(!inStack.isEmpty()){
+                outStack.push(inStack.pop());
+            }
+            return outStack.peek();
+        }
+        
+    }
+    
+    /** Returns whether the queue is empty. */
+    public boolean empty() {
+        if(inStack.isEmpty() && outStack.isEmpty()){
+            return true;
+        }else{
             return false;
         }
-        int start=0;
-        int end=(matrix.length*matrix[0].length)-1;
-        int midPoint=0;
-        int element=0;
-        while(start<=end){
-             midPoint=start+(end-start)/2;
-             element=matrix[midPoint/matrix[0].length][midPoint%matrix[0].length];
-            if(element==target){
-                return true;
-            }else if(target<element){
-                end=midPoint-1;
-            }else if(target>element){
-                start=midPoint+1;
-            }
-        }
-        return false;
     }
 }
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue obj = new MyQueue();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.peek();
+ * boolean param_4 = obj.empty();
+ */
