86f6e7a726625c30da9e9c628aaeaa7742b7510c
Nikhita Sagi <saginikhita98@gmail.com>
2020-12-24 18:17:10 -0500

    Updated solutions

--- MyHashSet.java
+++ MyHashSet.java
@@ -1,8 +1,5 @@
-// Time Complexity : The time complexities for add, remove and contains operations is O(n) since in worst case scenario there
-// is a chance that all the elements would map to the same index in the array and we would traverse through the whole list in that
-// particular index.
-// Space Complexity : The space complexity is O(n) since as said in worst case scenario there would be a chance that all the elements
-// would map to the same index in the array and a linked List of length n would be created.
+// Time Complexity : The time complexities for add, remove and contains operations is O(1) because they are done by directly accessing the index
+// Space Complexity : The space complexity is O(n) since in worst case scenario all the elements in the arrays would be full
 // Did this code successfully run on Leetcode : Yes
 // Any problem you faced while coding this : No
 
@@ -10,82 +7,63 @@
 // Your code here along with comments explaining your approach
 class MyHashSet {
 
-    LinkedList<Integer>[] set;
+    boolean[][] set;
 
     /** Initialize your data structure here. */
     public MyHashSet() {
 
         // Since the maximum number of elements would be 10^6, an array of length 1000 is created and each index would
-        // manage a linked list
-        set = new LinkedList[1000];
+        // manage another array of length 1000
+        set = new boolean[1000][];
 
     }
 
     public void add(int key) {
 
-        int position = key % set.length; // Index of the array is obtained through hashing
+        // Index of the first and second arrays is obtained through hashing
+        int index = key % set.length;
+        int secondIndex = key / set.length;
 
-        // If the linked list is empty then a new linked list is created
-        if(set[position] == null){
-            set[position] = new LinkedList<Integer>();
-            set[position].add(key);
+        // If the array is empty then a new array is created
+        if(set[index] == null){
+            set[index] = new boolean[1000];
+            set[index][secondIndex] = true;
             return;
         }
 
-        // Traverse through the linked list to check if the key exists or not
-        for(int i=0;i<set[position].size();i++){
-
-            if(set[position].get(i) == key){
-                return;
-            }
-
-        }
-
-        // Key is added if it doesnt exist in the linked list
-        set[position].add(key);
+        // Add the key in the set
+        set[index][secondIndex] = true;
 
     }
 
     public void remove(int key) {
 
-        int position = key % set.length; // Index of the array is obtained through hashing
+        // Index of the first and second arrays is obtained through hashing
+        int index = key % set.length;
+        int secondIndex = key / set.length;
 
-        // If the linked list is empty then there are no elements to remove
-        if(set[position] == null){
+        // If the array is empty then there are no elements to remove
+        if(set[index] == null){
             return;
         }
-
-        // Traverse through the linked list to remove the key if exists
-        for(int i=0;i<set[position].size();i++){
-
-            if(set[position].get(i) == key){
-                set[position].remove(i);
-            }
-
-        }
+        // Remove the key in the set
+        set[index][secondIndex] = false;
 
     }
 
     /** Returns true if this set contains the specified element */
     public boolean contains(int key) {
 
-        int position = key % set.length; // Index of the array is obtained through hashing
+        // Index of the first and second arrays is obtained through hashing
+        int index = key % set.length;
+        int secondIndex = key / set.length;
 
-        // If the linked list is empty then the key doesnt exists
-        if(set[position] == null){
+        // If the array is empty then the key doesnt exists
+        if(set[index] == null){
             return false;
         }
 
-        // Traverse through the linked list to check if the key exists
-        for(int i=0;i<set[position].size();i++){
-
-            if(set[position].get(i) == key){
-                return true;
-            }
-
-        }
-
-        return false;
+        return set[index][secondIndex];
 
     }
 }
