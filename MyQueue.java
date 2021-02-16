f8c5a814e6cb2b051dbdb56a9e6af04d3de04d0d
omkar <omkarvaidya13@gmail.com>
2021-02-16 09:52:53 -0500

    Updated Implementation

--- MyQueue.java
+++ MyQueue.java
@@ -1,50 +1,43 @@
 class MyQueue {
 
     /** Initialize your data structure here. */
-    private Stack<Integer> st1, st2;
-    private int front;
+    private Stack<Integer> in, out;
     public MyQueue() {
-        st1 = new Stack<>();
-        st2 = new Stack<>();
-        front = -1;
+        in = new Stack<>();
+        out = new Stack<>();
     }
     
     /** Push element x to the back of queue. */
     // Time Complexity: O(1)
     // Space Complexity: O(n)
     public void push(int x) {
-        if(st1.isEmpty())
-            front = x;
-        st1.push(x);
+        in.push(x);
     }
     
     /** Removes the element from in front of queue and returns that element. */
-    // Time Complexity: O(n)
-    // Space Complexity: O(n)
+    // Time Complexity: O(1) (Amortized)
+    // Space Complexity: O(1)
     public int pop() {
-        while(st1.size() > 1)
-            st2.push(st1.pop());
-        int result = st1.pop();
-        
-        front = st2.isEmpty() ? -1 : st2.peek();
-        while(!st2.isEmpty()){
-            st1.push(st2.pop());
-        }
-        return result;
+        peek();
+        return out.pop();
     }
     
     /** Get the front element. */
-    // Time Complexity: O(1)
+    // Time Complexity: O(1) (Amortized)
     // Space Complexity: O(1)
     public int peek() {
-        return front;
+        if(out.isEmpty()){
+            while(!in.isEmpty())
+                out.push(in.pop());
+        }
+        return out.peek();
     }
     
     /** Returns whether the queue is empty. */
     // Time Complexity: O(1)
     // Space Complexity: O(1)
     public boolean empty() {
-        return st1.isEmpty();
+        return in.isEmpty() && out.isEmpty();
     }
 }
 
