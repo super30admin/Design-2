c4ec1df42aeae9f668a24a1b9eefa0e0bf510f9c
Tirthaj Rikame <tirthaj.rikame@gmail.com>
2021-02-22 00:34:33 +0530

    Completed Design 2: Queue using stack

---
+++ Queue using stack.py
@@ -0,0 +1,40 @@
+class MyQueue:
+
+    def __init__(self):
+        self.IN = []
+        self.OUT = []
+        """
+        Initialize your data structure here.
+        """
+
+    def push(self, x: int) -> None:
+        self.IN.append(x)
+        """
+        Push element x to the back of queue.
+        """
+
+    def pop(self) -> int:
+        if len(self.OUT) == 0:
+            while len(self.IN) != 0:
+                self.OUT.append(self.IN.pop())
+
+        return self.OUT.pop()
+
+        """
+        Removes the element from in front of queue and returns that element.
+        """
+
+    def peek(self) -> int:
+        if len(self.OUT) == 0:
+            while len(self.IN) != 0:
+                self.OUT.append(self.IN.pop())
+
+        return self.OUT[-1]
+        """
+        Get the front element.
+        """
+
+    def empty(self) -> bool:
+        if len(self.IN) == 0 and len(self.OUT) == 0:
+            return True
+        return False
