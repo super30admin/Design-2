da7fd63c82e6c66337c2c2e154a7e7f007370e7f
MaryShilpaThumma <49599145+MaryShilpaThumma@users.noreply.github.com>
2021-02-23 23:11:41 -0500

    Create DesignQueueWithTwoStacks.java

---
+++ DesignQueueWithTwoStacks.java
@@ -0,0 +1,39 @@
+class MyQueue {
+    
+    Stack<Integer> in;
+    Stack<Integer> out;
+
+    /** Initialize your data structure here. */
+    public MyQueue() {
+        in = new Stack<>();
+        out = new Stack<>();
+    }
+    
+    /** Push element x to the back of queue. */
+    public void push(int x) {
+        in.push(x);
+        
+    }
+    
+    /** Removes the element from in front of queue and returns that element. */
+    public int pop() {
+        peek();
+        return out.pop();
+    }
+    
+    /** Get the front element. */
+    public int peek() {
+        if(out.isEmpty()){
+            while(!in.isEmpty()){
+                out.push(in.pop());
+            }
+        }
+        return out.peek();
+        
+    }
+    
+    /** Returns whether the queue is empty. */
+    public boolean empty() {
+        return in.isEmpty() && out.isEmpty();
+    }
+}
