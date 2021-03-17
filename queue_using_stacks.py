28f30d9be2b38efa18926dbba5341db6e1205d08
Prathamesh Kumkar <prathameshkumkar@gmail.com>
2021-03-17 16:59:27 -0700

    Completed Design-2

---
+++ queue_using_stacks.py
@@ -0,0 +1,42 @@
+class MyQueue:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.in_stack = []
+        self.out_stack = []
+
+    def push(self, x: int) -> None:
+        """
+        Push element x to the back of queue.
+        """
+        self.in_stack.append(x)
+
+    def pop(self) -> int:
+        """
+        Removes the element from in front of queue and returns that element.
+        """
+        if self.out_stack:
+            return self.out_stack.pop()
+        else:
+            while self.in_stack:
+                self.out_stack.append(self.in_stack.pop())
+            return self.out_stack.pop()
+
+    def peek(self) -> int:
+        """
+        Get the front element.
+        """
+        if self.out_stack:
+            return self.out_stack[-1]
+        else:
+            while self.in_stack:
+                self.out_stack.append(self.in_stack.pop())
+            return self.out_stack[-1]
+
+    def empty(self) -> bool:
+        """
+        Returns whether the queue is empty.
+        """
+        return len(self.in_stack) == 0 and len(self.out_stack) == 0
