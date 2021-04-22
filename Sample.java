74fb7b9f82ba77f1d8fbf2a348529ee736b17e46
Shrinivasmiskin <64209820+Shrinivasmiskin@users.noreply.github.com>
2021-04-22 14:14:09 +0530

    completed design 2, problem 1

--- Sample.java
+++ Sample.java
@@ -5,3 +5,50 @@
 
 
 // Your code here along with comments explaining your approach
+class MyQueue {
+    
+    private Stack<Integer> inStack;
+    private Stack<Integer> outStack;
+    /** Initialize your data structure here. */
+    public MyQueue() {
+        inStack = new Stack<>();
+        outStack = new Stack<>();
+    }
+    
+    /** Push element x to the back of queue. */
+    public void push(int x) {
+        inStack.push(x);
+    }
+    
+    /** Removes the element from in front of queue and returns that element. */
+    public int pop() {
+        peek();
+        return outStack.pop();
+    }
+    
+    /** Get the front element. */
+    public int peek() {
+        if(outStack.isEmpty()){
+            while(!inStack.isEmpty()){
+                outStack.push(inStack.pop());
+         
+            }
+            
+        }
+        return outStack.peek();
+    }
+    
+    /** Returns whether the queue is empty. */
+    public boolean empty() {
+        return inStack.isEmpty() && outStack.isEmpty();
+    }
+}
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue obj = new MyQueue();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.peek();
+ * boolean param_4 = obj.empty();
+ */
