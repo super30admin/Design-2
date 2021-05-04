d230cb2c335c57662dc43ed3a994670297f3e3bc
Deepikasri5 <deepikasrigr.1995@gmail.com>
2021-05-04 03:08:57 -0400

    Design2 Completed

---
+++ HashSet.java
@@ -0,0 +1,48 @@
+// Time Complexity :-
+//      Get - O(1)
+//      Put - O(1)
+//      Remove - O(1)
+// Space Complexity : O(n)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : No
+
+class MyHashSet {
+
+    boolean[][] matrix;
+
+    public MyHashSet() {
+        matrix = new boolean[1000][]; 
+    }
+
+    public int getHashKey(int key){
+        return key % 1000;                  // Since the range is from 0-10^6, we use a 1000*1001 matrix
+    }
+
+    public int getHashKey2(int key){
+        return key / 1001;                  // To accommodate the last value 10^6 and to avoid overflow, we use 1001, instead of 1000
+    }
+
+    public void add(int key) {
+        int hashKey = getHashKey(key);
+        int hashKey2 = getHashKey2(key);
+        if (matrix[hashKey] == null){
+            matrix[hashKey] = new boolean[1001];
+        }
+        matrix[hashKey][hashKey2] = true;
+    }
+
+    public void remove(int key) {
+        int hashKey = getHashKey(key);
+        int hashKey2 = getHashKey2(key);
+        if (matrix[hashKey] == null) return;     // In case the Nested Array is not present, we don't do anything
+        matrix[hashKey][hashKey2] = false;       // While removing the value, we set the default value false
+    }
+
+    /** Returns true if this set contains the specified element */
+    public boolean contains(int key) {
+        int hashKey = getHashKey(key);
+        int hashKey2 = getHashKey2(key);
+        if (matrix[hashKey] == null) return false; // In case the Nested Array is not present, we return the default value false
+        return matrix[hashKey][hashKey2];           
+    }
+}
