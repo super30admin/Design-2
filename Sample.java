869ac609fa4dcf923d3aaa867edc3279a1d38513
Radhika Kulkarni <radhikankulkarni@gmail.com>
2021-05-26 23:57:00 +0530

    Completed Design-2

--- Sample.java
+++ Sample.java
@@ -1,7 +1,14 @@
-// Time Complexity :
-// Space Complexity :
-// Did this code successfully run on Leetcode :
-// Any problem you faced while coding this :
+// Implement Queue using stacks
+// Time Complexity : push - O(n), pop - O(1), peek - O(1), empty O(1)
+// Space Complexity : O(n)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : No
+
+// HasSet
+// Time Complexity : O(1)
+// Space Complexity : O(range)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : I tried solving in C++, but got stuck in pointers implementation
 
 
 // Your code here along with comments explaining your approach
