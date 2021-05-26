740b9d7ce02a08a49d2254cf7258aac0bc889820
Divya <divthaz@amazon.com>
2021-05-26 16:28:05 -0700

    Completed design 2

--- Sample.java
+++ Sample.java
@@ -1,7 +1,53 @@
-// Time Complexity :
-// Space Complexity :
-// Did this code successfully run on Leetcode :
-// Any problem you faced while coding this :
+// Time Complexity : O(1)
+// Space Complexity : O(1)
+// Did this code successfully run on Leetcode : yes
+// Any problem you faced while coding this : -
 
+class MyHashSet {
+    boolean[][] storage;
+    private int bucketLength;
+    private int bucketItemsLength;
 
-// Your code here along with comments explaining your approach
+    MyHashSet(){
+        bucketLength = 1000;
+        bucketItemsLength = 1000;
+        storage = new boolean[bucketLength][];
+    }
+
+    // Returns bucket index for the given key
+    private int bucketIndex(int key){
+        return key % bucketLength;
+    }
+
+    // Returns bucket item index for the given key
+    private int bucketItemIndex(int key){
+        return key / bucketItemsLength;
+    }
+
+    public void add(int key) {
+        int bucketIndex = bucketIndex(key);
+        int bucketItemIndex = bucketItemIndex(key);
+        if(storage[bucketIndex] == null){
+            int size = bucketIndex == 0 ? bucketItemsLength+1 : bucketItemsLength;
+            storage[bucketIndex] = new boolean[size];
+        }
+        storage[bucketIndex][bucketItemIndex] = true;
+    }
+
+    public void remove(int key) {
+        int bucketIndex = bucketIndex(key);
+        int bucketItemIndex = bucketItemIndex(key);
+        if(storage[bucketIndex] != null)
+            storage[bucketIndex][bucketItemIndex] = false;
+    }
+
+    /** Returns true if this set contains the specified element */
+    public boolean contains(int key) {
+        int bucketIndex = bucketIndex(key);
+        int bucketItemIndex = bucketItemIndex(key);
+        if(storage[bucketIndex] == null)
+            return false;
+        else
+            return storage[bucketIndex][bucketItemIndex];
+    }
+}
