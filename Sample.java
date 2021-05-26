ed23e713c113f4f87424626b81a11298b5a51e86
DharmeshNarola <narola.fenil@gmail.com>
2021-05-25 21:04:32 -0500

    Please review it-Design-2

--- Sample.java
+++ Sample.java
@@ -1,7 +1,29 @@
-// Time Complexity :
-// Space Complexity :
-// Did this code successfully run on Leetcode :
-// Any problem you faced while coding this :
+class MinStack {
 
+    /** initialize your data structure here. */
+   Stack<Integer> stack=new Stack<>();
+int min=Integer.MAX_VALUE;
+public void push(int x) {
+    if(x<=min) {stack.push(min); min=x;}
+    stack.push(x);
+}
+public void pop() {
+    if(stack.peek()==min){ stack.pop(); min=stack.pop(); }
+    else stack.pop();
+}
+public int top() {
+    return stack.peek();
+}
+public int getMin() {
+    return min;
+}
+}
 
-// Your code here along with comments explaining your approach
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(val);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
