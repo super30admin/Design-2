8b44a1d2ab24738d74d008b6a637922b00de07c4
broy2808 <42786350+broy2808@users.noreply.github.com>
2021-06-23 16:19:33 -0700

    Create problem1.py

---
+++ problem1.py
@@ -0,0 +1,58 @@
+class MyQueue:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.stack1=[]
+        self.stack2=[]
+        
+
+    def push(self, x: int) -> None:
+        """
+        Push element x to the back of queue.
+        """
+        self.stack1.append(x)
+        
+
+    def pop(self) -> int:
+        """
+        Removes the element from in front of queue and returns that element.
+        """
+        if len(self.stack1)==0:
+            return None
+        x=len(self.stack1)
+        while(x>0):
+            x-=1
+            self.stack2.append(self.stack1.pop())
+        v=self.stack2.pop()
+        x=len(self.stack2)
+        while(x>0):
+            x-=1
+            self.stack1.append(self.stack2.pop())
+        return v
+
+    def peek(self) -> int:
+        """
+        Get the front element.
+        """
+        if len(self.stack1)==0:
+            return None
+        x=len(self.stack1)
+        while(x>0):
+            x-=1
+            self.stack2.append(self.stack1.pop())
+        v=self.stack2[-1]
+        x=len(self.stack2)
+        while(x>0):
+            x-=1
+            self.stack1.append(self.stack2.pop())
+        return v
+
+    def empty(self) -> bool:
+        """
+        Returns whether the queue is empty.
+        """
+        if  len(self.stack1)==0:
+            return True
+        return False
