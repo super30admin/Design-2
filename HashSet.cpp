2c9c50a63e56b7f0976c1cde793d8c49cc34701e
Jujjuru Tarun Sai <tarunsai@Jujjurus-MBP.attlocal.net>
2021-07-21 15:36:47 -0500

    Completed HashSet implementation with approach discussed in class

--- HashSet.cpp
+++ HashSet.cpp
@@ -1,22 +1,49 @@
+// Time Complexity : O(1) for add, remove and contains
+// Space Complexity : In the worst case I would use up 10^6 size if all the buckets got created
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : Nope
+
 class MyHashSet {
-    vector<bool> hashSet;
+    vector<bool> hashSet[1000];
 public:
     /** Initialize your data structure here. */
-    MyHashSet():hashSet(1000001) {
+    MyHashSet() {
         
     }
     
     void add(int key) {
-        hashSet[key] = true;
+        int modVal = key % 1000;
+        int divVal = key / 1000;
+        
+        if(hashSet[modVal].size() == 0){
+            if(modVal == 0)
+                hashSet[modVal] = vector<bool>(1001);
+            else
+                hashSet[modVal] = vector<bool>(1000);
+        }
+        
+        hashSet[modVal][divVal] = true;
     }
     
     void remove(int key) {
-        hashSet[key] = false;
+        int modVal = key % 1000;
+        int divVal = key / 1000;
+        
+        if(hashSet[modVal].size() == 0)
+            return;
+        
+        hashSet[modVal][divVal] = false;
     }
     
     /** Returns true if this set contains the specified element */
     bool contains(int key) {
-        return hashSet[key];
+        int modVal = key % 1000;
+        int divVal = key / 1000;
+        
+        if(hashSet[modVal].size() == 0)
+            return false;
+        
+        return hashSet[modVal][divVal];
     }
 };
 
