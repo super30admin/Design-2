90ecea64c03cff865802afc37f0db20516b45c4d
VishLivecoder <vxv190045@utdallas.edu>
2021-07-22 11:33:49 -0500

    Hashmap efficient code

--- Sample.java
+++ Sample.java
@@ -59,3 +59,93 @@ class MyQueue {
  * int param_3 = obj.peek();
  * boolean param_4 = obj.empty();
  */
+
+
+ //Design HashMap Efficieant code
+
+ class MyHashMap {
+
+    /** Initialize your data structure here. */
+    
+    class Node{
+        int key,value;
+        Node next;
+        Node(int key,int value){
+            this.key=key;
+            this.value=value;
+            
+        }
+    }
+    int size=10000;
+    
+    Node nodes[]; 
+    public MyHashMap() {
+        nodes=new Node[size];
+        
+    }
+    public int getKey(int key){
+        return Integer.hashCode(key)%size;
+    }
+    public Node find(Node head,int key){
+        Node prev=null;
+        Node cur=head;
+        while(cur!=null && cur.key!=key){
+            prev=cur;
+            cur=cur.next;
+        }
+        return prev;
+    }
+    
+    /** value will always be non-negative. */
+    public void put(int key, int value) {
+        int index=getKey(key);
+        if(nodes[index]==null){
+            nodes[index]=new Node(-1,-1);
+        }
+        Node prev=find(nodes[index],key);
+        if(prev.next==null){
+            prev.next=new Node(key,value);
+        }else{
+            prev.next.value=value;
+        }
+        
+    }
+    
+    /** Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key */
+    public int get(int key) {
+        int index=getKey(key);
+        if(nodes[index]==null){
+            return -1;
+
+        }
+        Node prev=find(nodes[index],key);
+        if(prev.next==null){
+            return -1;
+        }
+        else{
+            return prev.next.value;
+        }
+        
+    }
+    
+    /** Removes the mapping of the specified value key if this map contains a mapping for the key */
+    public void remove(int key) {
+        int index=getKey(key);
+        if(nodes[index]==null){
+            return;
+        }
+        Node prev=find(nodes[index],key);
+        if(prev.next==null){
+            return;
+        }
+        prev.next=prev.next.next;
+    }
+}
+
+/**
+ * Your MyHashMap object will be instantiated and called as such:
+ * MyHashMap obj = new MyHashMap();
+ * obj.put(key,value);
+ * int param_2 = obj.get(key);
+ * obj.remove(key);
+ */
