cf8add3862c7d129fdb78e5644db4ceff1ebffe4
Ramyasai Uppuluri <ramyasaiuppuluri@Ramyasais-Air.home>
2021-07-24 07:01:39 -0500

    please review

--- ImplementQueue.java
+++ ImplementQueue.java
@@ -12,32 +12,24 @@ class MyQueue {
     /** Push element x to the back of queue. */
     public void push(int x) {
         //if stack is empty push the first element else push the element to the top of the stack
-        if (s1.empty())
-            front = x;
         s1.push(x);
     }
 
     /** Removes the element from in front of queue and returns that element. */
     public int pop() {
-        // if the helper stack s2 is empty then pop the top element from the stack and push it to s2
-        // else pop pop the top element which satisfies the rule of FIFO as
-        // the top will have the first element of the main stack
-        if (s2.isEmpty()) {
-            while (!s1.isEmpty())
-                s2.push(s1.pop());
-        }
-        return  s2.pop();
+        peek();
+        retunn s2.pop();
 
     }
 
     /** Get the front element. */
     public int peek() {
-        //If s2 is empty then return front which the front element in main stack else return front element in s2
-        if (!s2.isEmpty()) {
-            return s2.peek();
-        }
-        return front;
-
+       if(s2.empty){
+           while(!s1.isEmpty()){
+               s2.push(s1.pop());
+           }
+       }
+        return s2.peek();
     }
 
     /** Returns whether the queue is empty. */
