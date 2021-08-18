9f22305b7d19aaea0709cfc55af7d1ab0fe7d6a9
sandhya <goswami.sa@northeastern.edu>
2021-08-18 10:03:47 -0400

    Design Queue using Stack Completed

--- Sample.java
+++ Sample.java
@@ -1,7 +1,66 @@
-// Time Complexity :
-// Space Complexity :
-// Did this code successfully run on Leetcode :
-// Any problem you faced while coding this :
+// Time Complexity : O(n)
+// Space Complexity :O(n)
+// Did this code successfully run on Leetcode : yes
+// Any problem you faced while coding this : no
 
 
 // Your code here along with comments explaining your approach
+
+class MyQueue {
+   Stack st1;
+    Stack st2;
+    /** Initialize your data structure here. */
+    public MyQueue() {
+        st1 = new Stack<Integer>();
+        st2 = new Stack<Integer>();
+    }
+    
+    /** Push element x to the back of queue. */
+    public void push(int x) {
+        st1.push(x);
+    }
+    
+    /** Removes the element from in front of queue and returns that element. */
+    public int pop() {
+        int result ;
+        while(!st1.isEmpty())
+        {
+            st2.push(st1.pop());
+        }
+        result = (int)st2.pop();
+         while(!st2.isEmpty())
+        {
+            st1.push(st2.pop());
+        }
+        return result;
+    }
+    
+    /** Get the front element. */
+    public int peek() {
+        int result ;
+        while(!st1.isEmpty())
+        {
+            st2.push(st1.pop());
+        }
+        result = (int)st2.peek();
+         while(!st2.isEmpty())
+        {
+            st1.push(st2.pop());
+        }
+        return result;
+    }
+    
+    /** Returns whether the queue is empty. */
+    public boolean empty() {
+        return st1.isEmpty();
+    }
+}
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue obj = new MyQueue();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.peek();
+ * boolean param_4 = obj.empty();
+ */
