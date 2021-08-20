780a02f2d4c5a271d73a161bdb835b51055fb36b
ShashankReddyGopireddy <53836765+ShashankReddyGopireddy@users.noreply.github.com>
2021-08-19 22:31:29 -0500

    Add files via upload
    Problem3 can u review

---
+++ implementation_queues_usingstack.py
@@ -0,0 +1,39 @@
+class MyQueue:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.inStack = []
+        self.outStack = []
+
+    def push(self, x: int) -> None:
+        """
+        Push element x to the back of queue.
+        """
+        self.inStack.append(x)
+
+    def pop(self) -> int:
+        """
+        Removes the element from in front of queue and returns that element.
+        """
+        self.peek()
+        return self.outStack.pop()
+
+    def peek(self) -> int:
+        """
+        Get the front element.
+        """
+        if len(self.outStack)==0:
+            for i in range(len(self.inStack)):
+                self.outStack.append(self.inStack.pop())
+        return self.outStack[-1]
+
+    def empty(self) -> bool:
+        """
+        Returns whether the queue is empty.
+        """
+        if not self.inStack and not self.outStack:
+            return True
+        else:
+            return False
