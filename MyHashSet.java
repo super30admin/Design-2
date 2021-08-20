a3c94f099b1248cc87978b87f325069ec1acce69
pranjalpandey1511 <pranjalpandey1115@gmail.com>
2021-08-19 19:36:01 -0700

    Completed Sprint Design2

--- MyHashSet.java
+++ MyHashSet.java
@@ -1,3 +1,8 @@
+// Time Complexity : O(1)
+// Space Complexity : O(N)
+// Did this code successfully run on Leetcode :yes
+// Any problem you faced while coding this : No
+
 class MyHashSet {
 
     /** Initialize your data structure here. */
