662a9ab6e0b86e33e4880fd9d37aeceaf1bf0fc2
Swopnil Shrestha <7950326+swopnilnep@users.noreply.github.com>
2021-08-23 17:10:53 -0500

    Add spacing to HashSet

--- HashSet.py
+++ HashSet.py
@@ -17,18 +17,14 @@ Problems Occurred: N/A
 
 
 class MyHashSet:
-
     def __init__(self):
-        self.keys = [False] * 1000001 # Max Size limit set by problem
-        
-        
+        self.keys = [False] * 1000001  # Max Size limit set by problem
+
     def add(self, key: int) -> None:
         self.keys[key] = True
-        
-        
+
     def remove(self, key: int) -> None:
         self.keys[key] = False
 
-        
     def contains(self, key: int) -> bool:
         return self.keys[key]
