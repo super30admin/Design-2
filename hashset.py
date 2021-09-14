c4263369237bcbc9f2438ca3e73fb5faac4e3798
Subhash <svuppal6@asu.edu>
2021-09-14 16:17:49 -0700

    Please verify them

---
+++ hashset.py
@@ -0,0 +1,30 @@
+class MyHashSet:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.set = []
+
+    def add(self, key: int) -> None:
+        if key not in self.set:
+            self.set.append(key)
+
+    def remove(self, key: int) -> None:
+        if key in self.set:
+            self.set.remove(key)
+
+    def contains(self, key: int) -> bool:
+        """
+        Returns true if this set contains the specified element
+        """
+        if key in self.set:
+            return True
+        else:
+            return False
+
+# Your MyHashSet object will be instantiated and called as such:
+# obj = MyHashSet()
+# obj.add(key)
+# obj.remove(key)
+# param_3 = obj.contains(key)
---
+++ implement queue using stacks.py
@@ -0,0 +1,42 @@
+class MyQueue:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.queue = []
+
+    def push(self, x: int) -> None:
+        """
+        Push element x to the back of queue.
+        """
+        self.queue.append(x)
+
+    def pop(self) -> int:
+        """
+        Removes the element from in front of queue and returns that element.
+        """
+        pop = self.queue[0]
+        self.queue.remove(self.queue[0])
+        return pop
+
+    def peek(self) -> int:
+        """
+        Get the front element.
+        """
+        return self.queue[0]
+
+    def empty(self) -> bool:
+        """
+        Returns whether the queue is empty.
+        """
+        if len(self.queue) == 0:
+            return True
+        return False
+
+# Your MyQueue object will be instantiated and called as such:
+# obj = MyQueue()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.peek()
+# param_4 = obj.empty()
