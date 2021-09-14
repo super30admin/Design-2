83669eef818dd5bb95eecab5a96dedf60af8cef0
panth1921 <panthdev96@gmail.com>
2021-09-13 23:46:26 -0400

    design hashset problem done

--- Sample.java
+++ Sample.java
@@ -1,7 +1,35 @@
-// Time Complexity :
-// Space Complexity :
-// Did this code successfully run on Leetcode :
+// Time Complexity : 0(1)
+// Space Complexity : 0(n)
+// Did this code successfully run on Leetcode : yes
 // Any problem you faced while coding this :
 
+class MyHashSet {
 
-// Your code here along with comments explaining your approach
+    /** Initialize your data structure here. */
+    var array : [Int]
+    init() {
+      array = Array(repeating: -1, count: 1000002) 
+    }
+    
+    func add(_ key: Int) {
+        if array[key] == -1 {
+        array[key] = key
+        }
+    }
+    
+    func remove(_ key: Int) {
+        array[key] = -1
+    }
+
+    
+    
+    /** Returns true if this set contains the specified element */
+    func contains(_ key: Int) -> Bool {
+        if array[key] != -1  {
+            return true
+        }
+        return false
+    }
+}
+
+
