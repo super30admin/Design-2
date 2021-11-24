1486e4e99df352c923c6c57de8571c215556a775
saipha06 <62794146+saipha06@users.noreply.github.com>
2021-11-24 16:16:33 -0500

    executed problem

--- Sample.java
+++ Sample.java
@@ -1,7 +1,63 @@
-// Time Complexity :
-// Space Complexity :
-// Did this code successfully run on Leetcode :
-// Any problem you faced while coding this :
+// Time Complexity : O(1)
+// Space Complexity :O(1)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : No
 
 
 // Your code here along with comments explaining your approach
+
+class MyHashSet {
+    boolean[][] storage;
+    int bucketItemsSize = 1000;
+    public MyHashSet() {
+        int bucketSize = 1000;
+        storage = new boolean[bucketSize][];
+        
+    }
+    public int getHash1(int val){
+        return val%bucketItemsSize;
+    }
+    public int getHash2(int val){
+        return val/bucketItemsSize;
+    }
+    public void add(int key) {
+        int val1 = getHash1(key);
+        if(storage[val1]==null){
+            if(val1==0){
+                storage[val1] = new boolean[bucketItemsSize+1];
+            }
+            else{
+            storage[val1] = new boolean[bucketItemsSize];}
+        }
+        int val2 = getHash2(key);
+        storage[val1][val2] = true;
+    }
+    
+    public void remove(int key) {
+        int val1 = getHash1(key);
+        if(storage[val1]!=null){
+            int val2 = getHash2(key);
+            storage[val1][val2] = false;
+        }
+
+    }
+    
+    public boolean contains(int key) {
+
+        int val1 = getHash1(key);
+        if(storage[val1]!=null){
+            int val2 = getHash2(key);
+            return storage[val1][val2];
+        }
+        return false;
+
+    }
+}
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet obj = new MyHashSet();
+ * obj.add(key);
+ * obj.remove(key);
+ * boolean param_3 = obj.contains(key);
+ */
