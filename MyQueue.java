1c47d3b5c70903aa1efc76c0e86ef25fae10ce33
sai <srsaikumarreddy@gmail.com>
2021-11-25 13:17:01 -0700

    Question 2 commited

---
+++ MyQueue.java
@@ -0,0 +1,48 @@
+import java.util.Stack;
+
+public class MyQueue {
+    
+    Stack<Integer>inStack;
+    Stack<Integer>outStack;
+
+    public MyQueue() {
+        
+        inStack=new Stack<>();
+        outStack=new Stack<>();
+    }
+    
+    public void push(int x) {
+        
+        inStack.push(x);
+    }
+    
+    public int pop() {
+       
+        peek();
+        return outStack.pop();
+        
+    }
+    
+    public int peek() {
+        if(outStack.isEmpty())
+        {
+            while(!inStack.isEmpty())
+                outStack.push(inStack.pop());
+        }
+        return outStack.peek();
+    }
+    
+    public boolean empty() {
+        
+        return inStack.isEmpty() && outStack.isEmpty();
+    }
+}
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue obj = new MyQueue();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.peek();
+ * boolean param_4 = obj.empty();
+ */
