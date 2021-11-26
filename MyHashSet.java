55145fad54d67e99e5847fcc4b024020c379ceb9
shkamtek <shkamtek@cisco.com>
2021-11-25 16:11:45 -0800

    Add solution for queue

--- MyHashSet.java
+++ MyHashSet.java
@@ -3,13 +3,12 @@
 // Did this code successfully run on Leetcode : Yes
 // Any problem you faced while coding this : No
 
-
-// Your code here along with comments explaining your approach
+// Using double hashing technique
 class MyHashSet {
 
     private int buckets;
     private int bucketItems;
-    boolean [][] set;
+    private boolean [][] set;
     
     public MyHashSet() {
         buckets = 1000; // using bucket size as root of the requirements
