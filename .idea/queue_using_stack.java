082f4c42698a98a671b172f255368bcde22853ac
jayanthvarma1 <jayanthgorinta@gmail.com>
2021-11-26 17:08:04 -0700

    Completed Sprint Desgin-2

---
+++ .idea/queue_using_stack.java
@@ -0,0 +1,35 @@
+class MyQueue {
+    private Stack<Integer> in;
+    private Stack<Integer> out;
+
+    public MyQueue() {
+        in = new Stack<>();
+        out = new Stack<>();
+    }
+
+    public void push(int x) {
+        in.push(x);
+
+    }
+
+    public int pop() {
+        peek();
+        return out.pop();
+
+    }
+
+    public int peek() {
+        if(out.isEmpty()){
+            while(!in.isEmpty()){
+                out.push(in.pop());
+            }
+        }
+        return out.peek();
+
+    }
+
+    public boolean empty() {
+        if(in.isEmpty() && out.isEmpty()) return true;
+        else return false;
+    }
+}
