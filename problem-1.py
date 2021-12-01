c1411e7c44c5b13d62b3887c5782c29f76771754
Munazzaruddin M <mdmunazzar@hotmail.com>
2021-12-01 02:15:34 -0600

    Design 2 completed

--- problem-1.py
+++ problem-1.py
@@ -1,41 +1,31 @@
-class MyHashSet:
+# // Time Complexity : omortized O(1)
+# // Space Complexity :O(n)
+# // Did this code successfully run on Leetcode :
+class MyQueue:
 
     def __init__(self):
-        self.list1=[None]*1000
-        
-    def loc(self, key:int):
-        first=key%1000
-        second=key//1000
-        return [first,second]
-    
-    def add(self, key: int) -> None:
-        ind=self.loc(key)
-        if self.list1[ind[0]]==None:
-            self.list1[ind[0]]=[None]*10000
-        self.list1[ind[0]][ind[1]]=key
+        self.pushS=[]
+        self.popS=[]
+
+    def push(self, x: int) -> None:
+        self.pushS.append(x)
         
 
-    def remove(self, key: int) -> None:
-        ind=self.loc(key)
-        if self.list1[ind[0]]==None:
-            return
-        if self.list1[ind[0]][ind[1]]==key:
-            self.list1[ind[0]][ind[1]]=None
-    
-    def contains(self, key: int) -> bool:
-        ind=self.loc(key)
-  
-        if self.list1[ind[0]]==None:
-            return False
-        if self.list1[ind[0]][ind[1]]==key:
+    def pop(self) -> int:
+        if not self.popS:
+            for x in range(len(self.pushS)):
+                self.popS.append(self.pushS.pop())
+        return self.popS.pop()
+        
+
+    def peek(self) -> int:
+        if not self.popS:
+            for x in range(len(self.pushS)):
+                self.popS.append(self.pushS.pop())
+        return self.popS[-1]
+        
+
+    def empty(self) -> bool:
+        if not self.popS and not self.pushS:
             return True
-        else:
-            return False
-        
-
-
-# Your MyHashSet object will be instantiated and called as such:
-# obj = MyHashSet()
-# obj.add(key)
-# obj.remove(key)
-# param_3 = obj.contains(key)
+        return False
--- problem-2.py
+++ problem-2.py
@@ -1,26 +1,36 @@
-class MinStack:
+# // Time Complexity :O(1)
+# // Space Complexity :O(n)
+# // Did this code successfully run on Leetcode :
+class MyHashSet:
 
     def __init__(self):
-        self.minStack=[]
-        self.min=float('inf')
-
-    def push(self, val: int) -> None:
-        if(self.min>=val):
-            self.minStack.append(self.min)
-            self.min=val
-        self.minStack.append(val)
+        self.list1=[None]*1000
+        
+    def loc(self, key:int):
+        first=key%1000
+        second=key//1000
+        return [first,second]
+    
+    def add(self, key: int) -> None:
+        ind=self.loc(key)
+        if self.list1[ind[0]]==None:
+            self.list1[ind[0]]=[None]*10000
+        self.list1[ind[0]][ind[1]]=key
         
 
-    def pop(self) -> None:
-        popped=self.minStack.pop()
-        if(self.min==popped):
-            self.min=self.minStack.pop()
-        
-
-    def top(self) -> int:
-        return self.minStack[-1]
-        
-
-    def getMin(self) -> int:
-        return self.min
-        
+    def remove(self, key: int) -> None:
+        ind=self.loc(key)
+        if self.list1[ind[0]]==None:
+            return
+        if self.list1[ind[0]][ind[1]]==key:
+            self.list1[ind[0]][ind[1]]=None
+    
+    def contains(self, key: int) -> bool:
+        ind=self.loc(key)
+  
+        if self.list1[ind[0]]==None:
+            return False
+        if self.list1[ind[0]][ind[1]]==key:
+            return True
+        else:
+            return False
