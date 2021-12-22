8588ee5cd6b4a6e3819c24cc9128fac106d4bbda
Chayanica18 <52338384+Chayanica18@users.noreply.github.com>
2021-12-22 18:18:23 -0500

    queue using stack

--- Sample.java
+++ Sample.java
@@ -5,3 +5,62 @@
 
 
 // Your code here along with comments explaining your approach
+// implement queue using stack 
+
+class MyQueue {
+    private Stack<Integer> pushst;
+    private Stack<Integer> popst;
+    int top;
+
+    public MyQueue() {
+        
+        pushst = new Stack<>();
+        popst = new Stack<>();
+        top = -1;
+        
+    }
+    
+    public void push(int x) {
+         top++;
+        pushst.push(x);
+       
+        
+    }
+    
+    public int pop() {
+        while(top!=-1)
+        {
+        popst.push(pushst.pop());
+            top--;
+        }
+        
+        return popst.pop();
+              
+    }
+    
+    public int peek() {
+        while(top != -1)
+        {
+        popst.push(pushst.pop());
+            top--;
+        }
+        
+        return popst.peek();
+        
+    }
+    
+    public boolean empty() {
+
+        return popst.isEmpty();
+        
+    }
+}
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue obj = new MyQueue();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.peek();
+ * boolean param_4 = obj.empty();
+ */
