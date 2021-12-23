7ff3aa8e729c1dc384075a9ba1e52a5057b18c97
Arpan Solanki <solankiarpan@gmail.com>
2021-12-22 21:11:44 -0500

    Design-2 submission

--- MyHashSet.java
+++ MyHashSet.java
@@ -1,27 +1,53 @@
-class MyHashSet {
-    int  hash[];
-    public MyHashSet() {
-        hash = new int[1000001];
+class MyQueue {
+    Stack<Integer> s1;
+    Stack<Integer> s2;
+
+    public MyQueue() {
+        s1 = new Stack();
+        s2 = new Stack();
     }
 
-    public void add(int key) {
-        hash[key]=1;
+    public void push(int x) {
+        s1.push(x);
+
     }
 
-    public void remove(int key) {
-        hash[key]=0;
+    public int pop() {
+
+        if(!s2.isEmpty()){
+            return s2.pop();
+        }
+
+        reverseStack(s1,s2);
+        return s2.pop();
     }
 
-    public boolean contains(int key) {
-        if(hash[key]==0) return false;
-        return true;
+    public int peek() {
+        if(!s2.isEmpty()){
+            return s2.peek();
+        }
+
+        reverseStack(s1,s2);
+        return s2.peek();
+    }
+
+    public boolean empty() {
+        if(s1.isEmpty() && s2.isEmpty()) return true;
+        return false;
+    }
+
+    public void reverseStack(Stack s1, Stack s2){
+        while(!s1.isEmpty()){
+            s2.push(s1.pop());
+        }
     }
 }
 
 /**
- * Your MyHashSet object will be instantiated and called as such:
- * MyHashSet obj = new MyHashSet();
- * obj.add(key);
- * obj.remove(key);
- * boolean param_3 = obj.contains(key);
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue obj = new MyQueue();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.peek();
+ * boolean param_4 = obj.empty();
  */
