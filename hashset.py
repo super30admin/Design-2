48321d6fc79e05ed3d22dfb301741e9fe86969f8
Aniket Hirpara <gdg4dev@gmail.com>
2021-12-29 17:31:14 +0530

    added TC and SC

--- hashset.py
+++ hashset.py
@@ -1,23 +1,23 @@
+# Time Complexity : O(1) - still not sure how to calculate it but there seems no loops
+# Space Complexity : O(n) - still not sure how to calculate it but in contains function there is python
+# " in " keyword whose time complexity is O(n)
+# Did this code successfully run on Leetcode : well yes
+# Any problem you faced while coding this : A LOT. I've my exams and s30 classes going on simultaneously 
+# so i couldn't focus too much in the classes. I'm not from english background and newbie in python 
+# it took me 3-4 days to realise what is hashmap, hash functions, hash set. I've watched many solutions 
+# for the same problem, most time took me was to figuring out what is happening after each line is executed
+# I constantly printout and debug the code to understand the structure better.'
+# It'd be great if we discuss the python or javascript solutions too
+
 class MyHashSet(object):
 
-    def __init__(self):
-        # self.bucketSize = 1000 #main array column for 1000*1000 matrix
-        # self.bucketItems = 1000 #sub array
-        # self.storage = [[None for i in range(self.bucketSize)] for i in range(self.bucketItems)]
-        # [[None] * self.bucketSize] * self.bucketItems 
+    def __init__(self): 
         self.storage = [[] for _ in range(1000)]
         
     def bucketItem(self, key): return key % 1000
-        
-    # def bucketIte(self, key: int) -> int:
-        # return key // self.bucketItems
-
+    
     def add(self, key):
-        # print(self.storage)
         if not self.contains(key):
-            # print(2, self.contains(key))
-            # print(3, self.storage[self.bucketItem(key)])
-            # print(6, self.storage[self.bucketItem(key)])
             self.storage[self.bucketItem(key)].append(key)
 
     def remove(self, key):
@@ -25,46 +25,20 @@ class MyHashSet(object):
             self.storage[self.bucketItem(key)].remove(key)
     
     def contains(self, key):
-        # print(4, self.storage[self.bucketItem(key)])
-        # print(5, key in self.storage[self.bucketItem(key)])
+        print(self.storage)
         return key in self.storage[self.bucketItem(key)]
-        
-        
-        
-        # bucket = self.bucket(key)
-        # bucket_Item = self.bucketItem(key)
-        # if not self.storage:
-        #     if bucket == 0:
-        #         self.storage[bucket] = [0]*(self.bucketItems + 1)
-        #     else:
-        #         self.storage[bucket] = [0] * self.bucketItems
-        # self.storage[bucket][bucket_Item] = True
-        
-        """
-        :type key: int
-        :rtype: None
-        """
-        
-
-    # def remove(self, key):
-    #     """
-    #     :type key: int
-    #     :rtype: None
-    #     """
-        
-
-    # def contains(self, key):
-    #     """
-    #     :type key: int
-    #     :rtype: bool
-    #     """
-        
-
 
 # Your MyHashSet object will be instantiated and called as such:
 obj = MyHashSet()
-for _ in range(100000): obj.add(_)
-print(obj.storage)
+# for _ in range(100000): obj.add(_)
+obj.add(5466)
+obj.add(7864775466)
+obj.add(5467)
+obj.add(54656)
+obj.add(9466)
+# obj.add(5465)
+# obj.add(5466)
+# print(obj.storage)
 print(obj.contains(5466))
 # print(obj.contains(5465))
 # print(obj.contains(5467))
--- stackusingqueue.py
+++ stackusingqueue.py
@@ -1,22 +1,18 @@
+# Time Complexity : for all functions O(1) because there's not search or complex thing like that
+# Space Complexity : I dont know how to calculate space complexity, here i assume it's O(2n) because we have
+# two queues, then again i feel like it's O(1) for no reason. 
+# Did this code successfully run on Leetcode :
+#  Any problem you faced while coding this :
 class MyQueue(object):
 
     def __init__(self):
         self.inQueue = []
         self.outQueue = []
-        
 
     def push(self, x):
-        self.inQueue.append(x)
-        """
-        :type x: int
-        :rtype: None
-        """
-        
+        self.inQueue.append(x)   
 
     def pop(self):
-        """
-        :rtype: int
-        """
         self.peek()
         return self.outQueue.pop()
 
@@ -27,18 +23,9 @@ class MyQueue(object):
             else:
                 print("main stack is empty")
         return self.outQueue[-1]
-        """
-        :rtype: int
-        """
-        
 
     def empty(self):
         return not self.inQueue and not self.outQueue
-        """
-        :rtype: bool
-        """
-        
-
 
 # Your MyQueue object will be instantiated and called as such:
 # obj = MyQueue()
