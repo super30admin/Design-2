e099859e5e9b43dbe241493937172b5ff9b495ee
DEEPAKRAJ DHARMAPURI SELVAKUMAR <deepak.rajds2014@gmail.com>
2022-01-17 21:12:30 -0500

    Change logic for amortized

--- queue_using_stacks.py
+++ queue_using_stacks.py
@@ -7,37 +7,32 @@ class MyQueue:
 
     def __init__(self):
         self.f_list = list()
-        self.len = 0
         self.s_list = list()
         
 
     def push(self, x: int) -> None:
-        self.empty()
         self.f_list.append(x)
-        self.len += 1
 
 
     def pop(self) -> int:
-        if not self.empty():
-            return self.s_list.pop()
-        return -1
+        self.peek()
+        if self.empty():
+            return -1
+        return self.s_list.pop()
         
 
     def peek(self) -> int:
-        if self.empty():
-            return -1
-        return self.s_list[-1]
+        if not self.s_list:
+            while self.f_list:
+                self.s_list.append(self.f_list.pop())
+        
+        return self.s_list[-1] if self.s_list else -1
         
 
     def empty(self) -> bool:
-        if self.s_list:
-            return False
-        if self.f_list:
-            while self.f_list:
-                self.s_list.append(self.f_list.pop())
-        if self.s_list:
-            return False
-        return True
+        if not self.f_list and not self.s_list:
+            return True
+        return False
 
 
 # Your MyQueue object will be instantiated and called as such:
