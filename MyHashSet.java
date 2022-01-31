7b8a74a72a9530ab3dca3fdef2de7aba0c250ed8
Parikshit Narang <parikshit3097@gmail.com>
2022-01-31 12:51:53 -0500

    complete problem2 : please review

---
+++ MyHashSet.java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+class MyHashSet {
+
+    ArrayList<Integer> list;
+    
+    public MyHashSet() {
+        this.list = new ArrayList<>();
+    }
+    
+    public void add(int key) {
+        this.list.add(key);
+    }
+    
+    public void remove(int key) {
+        Iterator itr = this.list.iterator();
+        while(itr.hasNext()){
+            if(itr.next().equals(key)){
+                itr.remove();
+            }
+        }
+    }
+    
+    public boolean contains(int key) {
+        for(int val : this.list){
+            if(val == key){
+                return true;
+            }
+        }
+        
+        return false;
+    }
+}
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet obj = new MyHashSet();
+ * obj.add(key);
+ * obj.remove(key);
+ * boolean param_3 = obj.contains(key);
+ */
