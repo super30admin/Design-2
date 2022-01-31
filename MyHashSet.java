689f7a349e1599d1fafd0db48ed0bb3ccadf4d4b
Parikshit Narang <parikshit3097@gmail.com>
2022-01-31 12:55:04 -0500

    complete problem2 : please review

--- MyHashSet.java
+++ MyHashSet.java
@@ -1,3 +1,14 @@
+/*
+	Add Time Complexity : O(1)
+	Remove Time Complexity : O(n)
+	Contains Time Complexity : O(n)
+	
+	Space Time Complexity : O(n)
+	
+	Problem successfully runs on Leetcode.
+	
+	Url to accepted solution on leetcode: https://leetcode.com/submissions/detail/631723869/ 
+*/
 import java.util.*;
 
 class MyHashSet {
