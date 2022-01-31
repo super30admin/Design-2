033fdb6a7853e9e3c7c75c21a55b1e345ab8731f
PRINCE CHOUDHARY <universe.mars@gmail.com>
2022-01-31 14:51:00 -0800

    Completed HashSet

--- MyHashSet.java
+++ MyHashSet.java
@@ -1,3 +1,12 @@
+//Consider size of hash k, we have chosen 769 as this would distribute comparitively evenly
+// Time Complexity : O(n/k)
+// Space Complexity : O(n+k)
+// Did this code successfully run on Leetcode :Yes
+// Any problem you faced while coding this : No
+// Your code here along with comments explaining your approach
+//We will create new datastructure called Bucket which would act as linked list chaining
+//We will create an array of buckets of size k.
+//We will implent the functions add, remove and contains accordingly.
 class MyHashSet {
     private Bucket[] buckets;
     private int keyRange;
