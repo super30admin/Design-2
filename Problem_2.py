b83314d57e03643be3d7ac52d089de009c09b854
aryyamajana <janaaryyama@gmail.com>
2022-02-01 12:36:56 -0700

    Completed S30 FAANMG Problem #1 - HashSet (Added new approach after class)

--- Problem_2.py
+++ Problem_2.py
@@ -1,3 +1,5 @@
+# ------------------------------- Approach 1 - Linear Chaining ----------------------------------------
+
 # Time Complexity : O(n/k) where k is size of hasharray
 # Space Complexity : O(k) where k is size of hasharray
 # Did this code successfully run on Leetcode : Yes
@@ -67,7 +69,7 @@ class LinkedList:
         return False
 
 # Define the MyHashSet class        
-class MyHashSet:
+class MyHashSet1:
 
     # MyHashSet constructor
     def __init__(self):
@@ -100,7 +102,64 @@ class MyHashSet:
         return self.hasharray[hasharrayindex].isPresent(key)
 
 
-hashSet = MyHashSet()
+# -------------------------------- Approach 2 - Double Hashing ----------------------------------------
+
+# Time Complexity : O(1)
+# Space Complexity : O(n)
+# Did this code successfully run on Leetcode : Yes
+# Any problem you faced while coding this : No
+
+# Define the MyHashSet class  
+class MyHashSet2:
+
+    # MyHashSet Constructor
+    def __init__(self):
+        self.h1_helper = 1000
+        self.h2_helper = 1000
+        self.arr = [None]*self.h1_helper
+    
+    # Hash Function 1
+    def getHash1(self, key):
+        return key%self.h1_helper
+    
+    # Hash Function 2
+    def getHash2(self, key):
+        return key//self.h2_helper
+
+    # If the hash arrays don't contain key, then check if an array already exists at the index of arr given by Hash1.
+    # if it exists, then go to the index corresponding to Hash2 and update the value at that index to "True".
+    # Else create an array at that index of arr and then go to the index corresponding to Hash2 and update the value at that index to "True".
+    def add(self, key: int) -> None:
+        if not self.contains(key):
+            hash1 = self.getHash1(key)
+            if not self.arr[hash1]:
+                # Handle edge case of 10^6
+                if hash1 == 0:
+                    self.arr[hash1] = [False]*(self.h1_helper+1)
+                else:
+                    self.arr[hash1] = [False]*self.h1_helper
+            hash2 = self.getHash2(key)
+            self.arr[hash1][hash2] = True
+                    
+    # Find index of arr by Hash1 and then the calculate corresponding index of the array at tht index of arr and then set the value at that index to False.
+    def remove(self, key: int) -> None:
+        if self.contains(key):
+            hash1 = self.getHash1(key)
+            hash2 = self.getHash2(key)
+            self.arr[hash1][hash2] = False
+
+    # Calculate hash1 index and check if an array exists at that index of arr. If not, then return False.
+    # If the array exists, then calculate hash2 index and return the value in that index. If it is True, the key exists.
+    def contains(self, key: int) -> bool:
+        hash1 = self.getHash1(key)
+        if not self.arr[hash1]:
+            return False
+        hash2 = self.getHash2(key)
+        return self.arr[hash1][hash2]
+
+
+# hashSet = MyHashSet1()
+hashSet = MyHashSet2()
 hashSet.add(1)
 hashSet.add(2)
 print(hashSet.contains(1))
