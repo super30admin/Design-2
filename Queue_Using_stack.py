e6f736cdf4faac539c0f1e5502add5395708e1fd
Jana <janaaharri97@gmail.com>
2022-02-01 15:39:52 -0800

    Completed Design 2 - Please Review

---
+++ Queue_Using_stack.py
@@ -0,0 +1,26 @@
+# // Time Complexity : push: O(1), pop: amortized O(1) 
+# // Space Complexity : O(1)
+# // Did this code successfully run on Leetcode : Yes
+# // Any problem you faced while coding this : No
+
+
+class MyQueue:
+    def __init__(self):
+        self.s1 = []
+        self.s2 = []
+
+    def push(self, x):
+        self.s1.append(x)
+
+    def pop(self):
+        self.peek()
+        return self.s2.pop()
+
+    def peek(self):
+        if not self.s2:
+            while self.s1:
+                self.s2.append(self.s1.pop())
+        return self.s2[-1]        
+
+    def empty(self):
+        return not self.s1 and not self.s2
