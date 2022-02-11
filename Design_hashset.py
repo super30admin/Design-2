c3602a4893ca18d997cfd47ec1848cd8e5b7f3cb
vinayak sonandkar <sonandkarvinayak@gmail.com>
2022-02-11 00:22:53 -0800

    Design-2 Vinayak

--- Design_hashset.py
+++ Design_hashset.py
@@ -1,3 +1,7 @@
+# Method 1: Linear Chaining using Linked List
+# TC: add - O(n) , remove - O(n), contains - O(n) because find function
+# SC: O(n)
+
 class Node:
     def __init__(self, key, nextnode = None):
         self.key = key
@@ -49,6 +53,58 @@ class MyHashSet:
     def contains(self, key: int) -> bool:
         hash_key = key % self.base_modulo
         return self.keys_arr[hash_key].find(key)
+        
+
+
+# Your MyHashSet object will be instantiated and called as such:
+# obj = MyHashSet()
+# obj.add(key)
+# obj.remove(key)
+# param_3 = obj.contains(key)
+
+
+# Method 2: Double hashing using array
+# TC: add - O(1) , remove - O(1), contains - O(1) 
+# SC: O(n)
+
+class MyHashSet:
+
+    def __init__(self):
+        self.H1_HELPER = 1000
+        self.H2_HELPER = 1000
+        self.bucket = [ ]
+        self.keys_arr = [ self.bucket for i in range(self.H1_HELPER)]
+        
+    def gethash1(self,key):
+        return key % self.H1_HELPER
+    
+    def gethash2(self,key):
+        return key // self.H2_HELPER
+
+    def add(self, key: int) -> None:
+        if self.contains(key) == False:
+            hash1 = self.gethash1(key)
+            hash2 = self.gethash2(key)
+            if self.keys_arr[hash1] == [ ]:
+                self.keys_arr[hash1] = [False for i in range(self.H1_HELPER + 1)]
+            self.keys_arr[hash1][hash2] = True
+                                    
+
+    def remove(self, key: int) -> None:
+        if self.contains(key):
+            hash1 = self.gethash1(key)
+            hash2 = self.gethash2(key)
+            self.keys_arr[hash1][hash2] = False
+                   
+
+    def contains(self, key: int) -> bool:
+        hash1 = self.gethash1(key)
+        hash2 = self.gethash2(key)
+        if self.keys_arr[hash1] == [ ]:
+            return False
+        return self.keys_arr[hash1][hash2]
+            
+            
         
 
 
