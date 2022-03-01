0ff05b35ad4e5a4dd2b9b5d437d734327a486a2a
Arteegupta <arteegupta@gmail.com>
2022-03-01 11:08:57 -0500

    Committed final Day2.

--- MinStack.java
+++ MinStack.java
@@ -1,56 +1,35 @@
 import java.util.Stack;
 
-public class MinStack {		
-	private int SIZE=1000;	
-	private Stack<Integer> stk = new Stack<>();
-	private Stack<Integer> minStk = new Stack<>();
-	private int stkCnt=0;
-	private int minStkCnt=0;
-	
-	public MinStack() {
-
-	}
-
-	public void push(int val) {
-		if(stkCnt < SIZE) {
-			stk.push(val);
-			stkCnt++;
-			if(minStk.isEmpty() || val <= minStk.peek()) {
-				minStk.push(val);
-				minStkCnt++;
-			}
-		}else {
-			System.out.println("Stack is full");
-		}
-	}
-
-	public void pop() {
-		if(stkCnt > 0) {
-			if(minStk.peek().equals(stk.peek())) {
-				minStk.pop();
-				minStkCnt--;
-			}
-			stk.pop();
-			stkCnt--;			
-		}else{
-			System.out.println("Stack is emppty");
-		}
-	}
-
-	public int top() {
-		return stk.peek();
-	}
-	
-	public int getMin() {
-		return minStk.peek();
-	}
-	
-	public boolean isEmpty() {
-		return (minStkCnt == 0);
-	}
-	
-	public boolean isFull() {
-		return (stkCnt == SIZE);
-	}
-	/**Time O(1) | Space O(n) **/
+public class MinStack {	
+	/**Using Stacks: Time O(1) | Space O(n) **/
+	Stack<Integer> stk;
+    Stack<Integer> minStk;
+    
+    public MinStack() {
+    	stk = new Stack<>();
+    	minStk = new Stack<>();
+    }
+    
+    public void push(int val) {
+        stk.push(val);
+        if(minStk.isEmpty() || val <= minStk.peek()){
+            minStk.push(val);
+        }
+    }
+    
+    public void pop() {
+        if(stk.peek().equals(minStk.peek())){
+            minStk.pop();
+        }
+        stk.pop();
+    }
+    
+    public int top() {
+        return stk.peek();
+    }
+    
+    public int getMin() {
+        return minStk.peek();
+        
+    }	
 }
--- MyHashSet.java
+++ MyHashSet.java
@@ -1,27 +1,47 @@
-import java.util.ArrayList;
-import java.util.List;
-
 public class MyHashSet {
-	private int bucketSize;
-	private List<Integer> bucketList;
-	
-	public MyHashSet() {
-        this.bucketSize=1000;
-        this.bucketList = new ArrayList<>(bucketSize);        
+	/**Double hashing| Time O(1) | Space O(n)**/
+	private boolean[][] storage;
+    private int buckets;
+    private int bucketItems;
+    
+    public MyHashSet() {
+        this.buckets=1000;
+        this.bucketItems=1000;
+        this.storage= new boolean[buckets][];
+    }
+    
+    private int bucket(int key){
+        return key%buckets;
+    }
+    private int bucketItem(int key){
+        return key/bucketItems;
     }
     
     public void add(int key) {
-        int index= (key%bucketSize);
-        bucketList.add(index, key);
+        int bucket= bucket(key);
+        int bucketItem= bucketItem(key);
+        if(storage[bucket] == null) {
+        	if(bucket == 0) {
+	        	storage[bucket]= new boolean[bucketItems+1]; //First bucket
+	        }else {
+	        	storage[bucket]= new boolean[bucketItems];
+	        }
+        }
+        storage[bucket][bucketItem] = true;
     }
     
     public void remove(int key) {
-        int index= (key%bucketSize);
-        bucketList.remove(index);
+    	int bucket= bucket(key);
+        int bucketItem= bucketItem(key);
+        if(storage[bucket] == null) return;
+        storage[bucket][bucketItem] = false;
     }
     
     public boolean contains(int key) {
-    	return bucketList.contains(key);
+    	int bucket= bucket(key);
+        int bucketItem= bucketItem(key);
+        if(storage[bucket] == null) return false;
+        return storage[bucket][bucketItem];
     }
 	
 	public static void main(String[] args) {
@@ -34,6 +54,5 @@ public class MyHashSet {
 		hashSet.contains(2); // returns true
 		hashSet.remove(2);
 		hashSet.contains(2); // returns false (already removed)
-	}
-	/**Time O(n) | Space O(n)**/
+	}	
 }
