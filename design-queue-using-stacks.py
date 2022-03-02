31d07be87e54a2430e5751915c75778d254a3e74
knatarasan <kannappan_natarasan@yahoo.com>
2022-03-02 07:02:15 -0800

    Fixed typo while moving from LC to github

--- design-queue-using-stacks.py
+++ design-queue-using-stacks.py
@@ -1,91 +1,87 @@
+#Solution 1
 '''
-
-// Time Complexity : O(N)
+// Time Complexity : Push O(N) Pop O(1)
 // Space Complexity : O(N)  where N is Number of buckets + Number of unique values
 // Did this code successfully run on Leetcode :  Yes
 // Any problem you faced while coding this :    Found issue to arrive the logic
 
-
 // Your code here along with comments explaining your approach
 
-    Approach has following classes
-        Bucket
-            This will hold a LinkedList to store keys from key collision 
+This approach uses two stacks main and secondary
+    push: put main stack into secondary upside down, push new element and transfer back from secondary
+    pop : Just pop top element
+    peek : return top element
 
-        HashSet
-            This will create as many buckets of key_space
-            add
-                Hash the key -> decide bucket -> update into the bucket
-            contains
-                Hash the key -> decide bucket -> check this bucket contains the value
+Eg: 
+push(4)
+push(5)
+push(6)
+    In a list [4,5,6]
+ 
+ Stack view    
+    |6|     | |
+    |5| =>  | |
+    |4|     | |
+    ___     ___
+    S1      S2
+ 
+ Stack view    
+    | |     |4|
+    | | =>  |5|
+    | |     |6|
+    ___     ___
+    S1      S2
 
- https://leetcode.com/problems/design-hashset/solution/
- 05:40
+Solution :
+    push(7)
+Stack view    
+    | |     |4|
+    | | =>  |5|
+    |7|     |6|
+    ___     ___
+    S1      S2
+
+
+
+https://leetcode.com/problems/implement-queue-using-stacks/
 '''
 
-class Node:
-    def __init__(self,data=None,nextNode=None):
-        self.data=data
-        self.next=nextNode
-        
-class Bucket:
-    def __init__(self):
-        self.head=Node(0)
-    
-    def insert(self,data):
-        if not self.exists(data):
-            newNode=Node(data,self.head.next)
-            self.head.next=newNode
-        
-    
-    def delete(self,data):
-        prev=self.head
-        curr=self.head.next
-        
-        while curr is not None:
-            if curr.data==data:
-                prev.next=curr.next
-                return
-            prev=curr
-            curr=curr.next
-            
-        
-    
-    def exists(self,data)->bool:
-        curr=self.head.next
-        while curr is not None:
-            if curr.data==data:
-                return True
-            curr=curr.next
-        return False
-        
-        
-        
 
-
-class MyHashSet:
+class MyQueue:
 
     def __init__(self):
-        self.key_space=2069
-        self.hash_table=[Bucket() for x in range(self.key_space)]
+        self.st1=[]
+        self.st2=[]
 
-    def add(self, key: int) -> None:
-        hash_key=key%self.key_space
-        self.hash_table[hash_key].insert(key)
+    def push(self, x: int) -> None:
+        while self.st1:
+            self.st2.append(self.st1.pop())
+        self.st2.append(x)
+        while self.st2:
+            self.st1.append(self.st2.pop())
 
-    def remove(self, key: int) -> None:
-        hash_key=key%self.key_space
-        self.hash_table[hash_key].delete(key)
-        
+    def pop(self) -> int:
+        if self.st1:
+            return self.st1.pop()
+        else:
+            return None
 
-    def contains(self, key: int) -> bool:
-        hash_key=key%self.key_space
-        return self.hash_table[hash_key].exists(key)
-        
+    def peek(self) -> int:
+        if self.st1:
+            return self.st1[-1]
+
+    def empty(self) -> bool:
+        if self.st1:
+            return False
+        return True
 
 
-# Your MyHashSet object will be instantiated and called as such:
-# obj = MyHashSet()
-# obj.add(key)
-# obj.remove(key)
-# param_3 = obj.contains(key)
+
+
+
+# Your MyQueue object will be instantiated and called as such:
+# obj = MyQueue()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.peek()
+# param_4 = obj.empty()
