5920295b00cd85504b4ecbee061b526643196dd7
Amrit <amritgill27195@gmail.com>
2022-03-01 21:22:25 -0500

    clean up and time/space complex

--- hashset/main.go
+++ hashset/main.go
@@ -1,33 +1,54 @@
 package main
 
 const (
-	outterSize = 1000
-	innerSize  = outterSize + 1
+	bucketSize      = 1000
+	bucketItemsSize = bucketSize + 1
 )
 
+// had to use a custom type for type assertion
+// because I could not figure how to initialize nested
+// fixed sized array in golang without pre-allocating initially
 type boolVal bool
 type MyHashSet struct {
-	items [outterSize]interface{}
+	items [bucketSize]interface{}
 }
 
 func Constructor() MyHashSet {
 	return MyHashSet{
-		items: [outterSize]interface{}{},
+		items: [bucketSize]interface{}{},
 	}
 }
 
-func (s *MyHashSet) bucket(key int) int     { return key % outterSize }
-func (s *MyHashSet) bucketItem(key int) int { return key / innerSize }
+/*
+	time: o(1)
+	space: o(1)
+*/
+func (s *MyHashSet) bucket(key int) int { return key % bucketSize }
 
+/*
+	time: o(1)
+	space: o(1)
+*/
+func (s *MyHashSet) bucketItem(key int) int { return key / bucketItemsSize }
+
+/*
+	time: o(1)
+	space: o(1) - but amortized
+				- will be o($bucketItemSize) at-least once for each idx
+*/
 func (s *MyHashSet) Add(key int) {
 	bucket := s.bucket(key)
 	bucketItem := s.bucketItem(key)
 	if s.items[bucket] == nil {
-		s.items[bucket] = make([]boolVal, innerSize)
+		s.items[bucket] = make([]boolVal, bucketItemsSize)
 	}
 	s.items[bucket].([]boolVal)[bucketItem] = true
 }
 
+/*
+	time: o(1)
+	space: o(1)
+*/
 func (s *MyHashSet) Contains(key int) bool {
 	bucket := s.bucket(key)
 	bucketItem := s.bucketItem(key)
@@ -37,6 +58,10 @@ func (s *MyHashSet) Contains(key int) bool {
 	return bool(s.items[bucket].([]boolVal)[bucketItem])
 }
 
+/*
+	time: o(1)
+	space: o(1)
+*/
 func (s *MyHashSet) Remove(key int) {
 	bucket := s.bucket(key)
 	bucketItem := s.bucketItem(key)
