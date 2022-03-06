11fff00f0fb6ef5289735e1c2de4888892ea6ec0
nisargushah <58926548+nisargushah@users.noreply.github.com>
2022-03-05 23:24:36 -0500

    Update hashset.py

--- hashset.py
+++ hashset.py
@@ -1,30 +1,37 @@
-# Time Complexity O(1)
-# Space Compelxity O(n)
+## Time Complexity-> O(1)
+## Space Complexity -> O(n)
 
 
 class MyHashSet:
-
+    #HashCode = 1000
     def __init__(self):
-        """
-        Initialize your data structure here.
-        """
-        self.hash = set()
-
+        self.hashset = [None]*10000
+    
+    def GetPrimaryHash(self, key):
+        return key % 10000
+    
+    def GetSecondaryHash(self,key):
+        return key//10000
     def add(self, key: int) -> None:
-        
-        self.hash.add(key)
-
+        if self.hashset[self.GetPrimaryHash(key)] == None:
+            temp = [None]*10000
+            temp[self.GetSecondaryHash(key)] = True
+            self.hashset[self.GetPrimaryHash(key)] = temp
+        else:
+             self.hashset[self.GetPrimaryHash(key)][self.GetSecondaryHash(key)] = True
+            
+    
     def remove(self, key: int) -> None:
-        self.hash.discard(key)
-
+        if self.hashset[self.GetPrimaryHash(key)] == None:
+            pass
+        else:
+            self.hashset[self.GetPrimaryHash(key)][self.GetSecondaryHash(key)] = None
+            
+    
     def contains(self, key: int) -> bool:
-        """
-        Returns true if this set contains the specified element
-        """
-        if key in self.hash:
-            return True
-        return False
-        
+        if self.hashset[self.GetPrimaryHash(key)] == None:
+            return False
+        return self.hashset[self.GetPrimaryHash(key)][self.GetSecondaryHash(key)]
 
 
 # Your MyHashSet object will be instantiated and called as such:
