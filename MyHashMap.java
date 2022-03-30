c363d4d6842def20ebcd4b7cfa2420716863f75c
Varun Gollapalli <gvarun1999@gmail.com>
2022-03-30 16:47:20 -0700

    Create MyHashMap.java

---
+++ MyHashMap.java
@@ -0,0 +1,20 @@
+
+import java.util.*;
+public class MyHashMap {
+    
+    public MyHashMap() {
+        
+    }
+    
+    public void put(int key, int value) {
+        
+    }
+    
+    public int get(int key) {
+        
+    }
+    
+    public void remove(int key) {
+        
+    }
+}
