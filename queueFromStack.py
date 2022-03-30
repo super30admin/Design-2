067fa18715ad32281cd3724ae6fb4d9e170aa333
Sowmya Mruthyunjaya <sowmya@sowmyas-mbp.lan>
2022-03-30 15:35:29 -0700

    completed problems 1 & 2

---
+++ queueFromStack.py
@@ -0,0 +1,51 @@
+class MyQueue(object):
+
+    def __init__(self):
+        self.stack1 = []
+        self.stack2 = []
+
+    def push(self, x):
+        """
+        :type x: int
+        :rtype: None
+        """
+        self.stack1.append(x)
+        
+
+    def pop(self):
+        """
+        :rtype: int
+        """
+        if not self.stack2:
+            while self.stack1:
+                self.stack2.append(self.stack1.pop())
+        return self.stack2.pop()
+
+        
+
+    def peek(self):
+        """
+        :rtype: int
+        """
+        if not self.stack2:
+            while self.stack1:
+                self.stack2.append(self.stack1.pop())
+        return self.stack2[-1]
+
+        
+        
+
+    def empty(self):
+        """
+        :rtype: bool
+        """
+        return (not self.stack1 and not self.stack2)
+        
+
+
+# Your MyQueue object will be instantiated and called as such:
+# obj = MyQueue()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.peek()
+# param_4 = obj.empty()
