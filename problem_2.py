a445c5765edf67968eff03619a7b7cbb0e7fa606
Shrey Patel <shrey1124@gmail.com>
2022-03-31 01:43:53 -0500

    homework: added another approach

--- problem_2.py
+++ problem_2.py
@@ -1,8 +1,8 @@
+""" Using Double Hashing """
 # Time Complexity : O(1) all operations
 # Space Complexity : O(N) for N is the Max Key value 
 # Did this code successfully run on Leetcode : Yes
 # Any problem you faced while coding this : No
-
 class MyHashSet:
 
     def __init__(self):
@@ -45,9 +45,68 @@ class MyHashSet:
         else:
             return False        
 
-
 # Your MyHashSet object will be instantiated and called as such:
 # obj = MyHashSet()
 # obj.add(key)
 # obj.remove(key)
 # param_3 = obj.contains(key)
+
+
+""" Using array + Linkedlist """
+class Node:
+    def __init__(self, key: int):
+        self.key = key 
+        self.next = None
+
+class MyHashSet_usingLinkedList:
+
+    def __init__(self):
+        # given 0 <= key <= 10^6 So select max size 10^3 as an optimal value
+        self.max = 10**4
+        # init primary array that will contain None initially,
+        # and then refrence to the secondary array. 
+        # Using Double Hash Function Method.
+        self.arr = [None for _ in range(self.max)]
+    
+    
+    def _search(self, head, key): # search LinkedList for a key
+        prev = head # (-1) dummy node
+        current = head.next # first actial pair node
+        while current is not None and current.key != key:
+            prev = current
+            current = current.next
+        return prev
+    
+    # O(1)
+    def add(self, key: int) -> None:
+        hash_1 = key % self.max # hash value
+        
+        if self.arr[hash_1] is None:
+            head = Node(-1)
+            head.next = Node(key)
+            self.arr[hash_1] = head
+        else:
+            prev = self._search(self.arr[hash_1], key)
+            if prev.next is None:
+                prev.next = Node(key)
+            else:
+                prev.next.key = key
+
+    # O(N)
+    def remove(self, key: int) -> None:
+        hash_1 = key % self.max # First hash value
+        if self.arr[hash_1] is None:
+            return
+        else:
+            prev = self._search(self.arr[hash_1], key)
+            if prev.next is not None:
+                prev.next = prev.next.next
+
+    # O(N)
+    def contains(self, key: int) -> bool:
+        hash_1 = key % self.max # hash value        
+        if not self.arr[hash_1]:
+            return False
+        else:
+            prev = self._search(self.arr[hash_1], key)
+            return False if prev.next is None else True
