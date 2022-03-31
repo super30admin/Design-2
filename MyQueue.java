68ca3544e94bc5a324f7f83207afb081e00a745d
Pratik Kamble <54627841+pratik-kamble@users.noreply.github.com>
2022-03-31 13:48:50 -0400

    MyQueue Completed

---
+++ MyQueue.java
@@ -0,0 +1,49 @@
+import java.util.Stack;
+
+class MyQueue {
+    
+    Stack<Integer> in;
+    Stack<Integer> out;
+
+
+    public MyQueue() {
+        
+        in = new Stack<>();
+        out = new Stack<>();
+        
+    }
+    
+    public void push(int x) {
+        in.push(x);
+        
+    }
+    
+    public int pop() {
+       peek();
+        return out.pop();
+    }
+    
+    public int peek() {
+        if(out.isEmpty()){
+            while(!in.isEmpty()){
+                out.push(in.pop());
+            }
+        }
+        return out.peek();
+    }
+    
+    public boolean empty() {
+        return (in.isEmpty() && out.isEmpty());
+    }
+
+    public static void main(String args[]){
+        MyQueue obj = new MyQueue();
+        obj.push(1);
+        obj.push(2);
+        System.out.println(obj.pop());
+        System.out.println(obj.peek());
+        System.out.println(obj.empty());
+        
+    }
+}
+
