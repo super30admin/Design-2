784435a0372648b7a503f7106940c5610debe175
jigar99 <jigarvaishnav06@gmail.com>
2022-03-31 19:17:22 -0400

    complete design 2

--- Sample.java
+++ Sample.java
@@ -59,3 +59,94 @@ public:
  * bool param_4 = obj->empty();
  */
 
+class MyHashMap {
+    
+    
+public:
+      struct Node{
+        int key,value;
+        Node*next;
+          
+          Node(int key,int value){
+              this->key = key;
+              this->value = value;
+             this->next = NULL;
+          }
+    
+    };
+     int bucket = 10000;
+     Node* node[10000];
+    
+    MyHashMap() { 
+         for (int i=0;i<bucket;++i)
+            node[i] = NULL;
+    }
+    
+    Node* find(Node* head,int key) {
+        Node *pre = head;
+        Node *curr = head->next;
+        
+        while(curr != NULL && curr->key != key){
+            pre = curr;
+            curr = curr->next;
+        }
+        
+        return pre;
+    }
+    
+    int getHash(int key) {
+        return key%bucket;
+    }
+    
+    void put(int key, int value) {
+        int index = getHash(key);
+        
+        if(node[index] == NULL) {
+            node[index] = new Node(-1,-1);
+        } 
+        Node *prev = find(node[index],key);
+        
+        if(prev->next == NULL) {
+            prev->next = new Node(key,value);
+        } else {
+            prev->next->value = value;
+        }
+        
+    }
+    
+    int get(int key) {
+        int index = getHash(key);
+        if(node[index] == NULL) {
+            return -1;
+        }
+        Node *prev = find(node[index],key);
+        if(prev->next == NULL) {
+            return -1;
+        } else {
+           return prev->next->value;
+        }
+    }
+    
+    void remove(int key) {
+         int index = getHash(key);
+        if(node[index] == NULL) {
+            return;
+        }
+        
+        Node *prev = find(node[index],key);
+        if(prev->next == NULL) {
+            return;
+        } else {
+          prev->next = prev->next->next;
+        }
+        
+    }
+};
+
+/**
+ * Your MyHashMap object will be instantiated and called as such:
+ * MyHashMap* obj = new MyHashMap();
+ * obj->put(key,value);
+ * int param_2 = obj->get(key);
+ * obj->remove(key);
+ */
