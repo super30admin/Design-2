e46dc2214f6a8e60255e263ee38eecfa9dc2a898
jigar99 <jigarvaishnav06@gmail.com>
2022-03-31 16:08:21 -0400

    complete design 2 one problem

--- Sample.java
+++ Sample.java
@@ -1,7 +1,61 @@
-// Time Complexity :
+// Time Complexity : 
 // Space Complexity :
 // Did this code successfully run on Leetcode :
 // Any problem you faced while coding this :
 
 
 // Your code here along with comments explaining your approach
+
+class MyQueue {
+    stack<int>in;
+    stack<int>out;
+public:
+    MyQueue() {
+        
+    }
+    
+    //o(1)
+    void push(int x) {
+        in.push(x);
+    }
+    
+    //o(n)
+    int pop() {
+        if(out.empty()) {
+            while(!in.empty()) {
+                out.push(in.top());
+                in.pop();
+            }
+            
+        } 
+        int top = out.top();
+        out.pop();
+       return top;
+    }
+    
+    //o(n)
+    int peek() {
+         if(out.empty()) {
+            while(!in.empty()) {
+                out.push(in.top());
+                in.pop();
+            }
+        } 
+       return out.top();   
+    }
+    
+    //o(1)
+    bool empty() {
+        return in.empty() && out.empty();
+    }
+};
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue* obj = new MyQueue();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->peek();
+ * bool param_4 = obj->empty();
+ */
+
