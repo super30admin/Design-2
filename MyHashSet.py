e04f2548878ffe5202138d18ceb352d84d57a503
Meena-hash <meena.bhat1995@gmail.com>
2022-04-02 18:43:18 -0700

    Update MyHashSet.py
    edit to the first approach - this approach uses the bucket method taught in class

--- MyHashSet.py
+++ MyHashSet.py
@@ -1,41 +1,54 @@
-# // Time Complexity : O(n)
-# // Space Complexity : O(n)
+# // Time Complexity : O(1)
+# // Space Complexity : O(n * k) - if n is the initial size of the bucket, then n * (number of buckets whose bucket items are not empty)
+# doubtful on the space complexity - please review.
 # // Did this code successfully run on Leetcode : yes
-# // Any problem you faced while coding this : Yes
-# I am not sure of the solution I implemented because I am not hashing the key
+# // Any problem you faced while coding this : No, reimplementation using buckets method taught in class.
 class MyHashSet(object):
 
     def __init__(self):
-        self.hashset = []
+        self.bucket_size = 1000
+        self.bucket = [None] * self.bucket_size
+
+    def hashFunction(self, key):
+        bucket_index = key % self.bucket_size
+        bucket_item_index = key / self.bucket_size
+        return bucket_index, bucket_item_index
 
     def add(self, key):
         """
         :type key: int
         :rtype: None
         """
-        if key not in self.hashset:
-            self.hashset.append(key)
+        bucket_index, bucket_item_index = self.hashFunction(key)
+        if self.bucket[bucket_index] is None:
+            if bucket_index == 0:
+                self.bucket[bucket_index] = [False] * (self.bucket_size + 1)
+            else:
+                self.bucket[bucket_index] = [False] * self.bucket_size
+        self.bucket[bucket_index][bucket_item_index] = True
 
     def remove(self, key):
         """
         :type key: int
         :rtype: None
         """
-        if key in self.hashset:
-            self.hashset.remove(key)
+        bucket_index, bucket_item_index = self.hashFunction(key)
+        if self.bucket[bucket_index] is not None:
+            self.bucket[bucket_index][bucket_item_index] = False
 
     def contains(self, key):
         """
         :type key: int
         :rtype: bool
         """
-        return key in self.hashset
+        bucket_index, bucket_item_index = self.hashFunction(key)
+        if self.bucket[bucket_index] is not None:
+            return self.bucket[bucket_index][bucket_item_index]
+        return False
 
 
 # Your MyHashSet object will be instantiated and called as such:
-key = 1
-obj = MyHashSet()
-obj.add(key)
-obj.remove(key)
-param_3 = obj.contains(key)
-print(param_3)
+# obj = MyHashSet()
+# obj.add(key)
+# obj.remove(key)
+# param_3 = obj.contains(key)
