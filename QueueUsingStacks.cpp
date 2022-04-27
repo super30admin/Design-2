871cd2ba06b4e008fa84683c6a90a1181ce2fab0
Hemanth Teja <42966988+HemanthTejaY@users.noreply.github.com>
2022-04-27 19:19:34 -0400

    Update QueueUsingStacks.cpp

--- QueueUsingStacks.cpp
+++ QueueUsingStacks.cpp
@@ -4,9 +4,15 @@
 // Any problem you faced while coding this : No
 
 // Your code here along with comments explaining your approach
+//We use one stack to create and maintain the order of the queue and the second stack to manipulate the elements to ensure the order in stack one. 
+//In case of push, we first move elements of the order stack to the second stack(s1) and then push the new element to first stack(s2) and then bring back the
+//elements from S1 to S2
+
+//For pop, peek and empty - we simply use the first element on top of S2 since it maintains the order of queue. 
+
 class MyQueue {
 public:
-    stack<int> s1;
+    stack<int> s1; 
     stack<int> s2;
     
     MyQueue() {}
