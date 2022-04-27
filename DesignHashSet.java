3d390da1f379464a0997f8b790e711abc0eb4c25
Mrigesh Dasgupta <dasgupta.m@northeastern.edu>
2022-04-27 01:29:19 -0400

    Solution-2: Fixed Edge Case

--- DesignHashSet.java
+++ DesignHashSet.java
@@ -1,43 +1,54 @@
+// Space Complexity O(1)
+// Time Complexity O(1)
 class MyHashSet {
     
-    int first_size = 1000;
-    int second_size = 1000;
+    int buckets; 
+    int bucketItems;
     
-    boolean array[][] = new boolean[first_size][];
+    boolean array[][] ;
     
     public int first_hash(int val){
-        return val%first_size;
+        return val%buckets;
     }
     public int second_hash(int val){
-        return val/first_size;
+        return val/buckets;
     }
     
     public MyHashSet() {
-        
+        this.buckets=1000;
+        this.bucketItems=1000;
+      this.array  = new boolean[buckets][];
     }
     
     public void add(int key) {
-        int first = first_hash(key);
-        int second = second_hash(key);
+        int bucket = first_hash(key);
+        int bucketItem = second_hash(key);
         
-        if(array[first]==null){
-            array[first] = new boolean[second_size];
+        if(array[bucket]==null){
+            if(bucket==0){
+            //case to handle last element as 1000000/1000=0, we need extra space at the end
+                array[bucket] = new boolean[bucketItems+1];
+            }else{
+                array[bucket] = new boolean[bucketItems];
+            }
+            
         }
-        array[first][second]= true;
+        //assign true to that index
+        array[bucket][bucketItem]= true;
     }
     
     public void remove(int key) {
-        int first = first_hash(key);
-        int second = second_hash(key);
-        if(array[first]!=null){
-            array[first][second]= false;
+        int bucket = first_hash(key);
+        int bucketItem = second_hash(key);
+        if(array[bucket]!=null){
+            array[bucket][bucketItem]= false;
         }
     }
     
     public boolean contains(int key) {
-        int first = first_hash(key);
-        int second = second_hash(key);
+        int bucket = first_hash(key);
+        int bucketItem = second_hash(key);
         
-        return array[first]!=null && array[first][second];
+        return array[bucket]!=null && array[bucket][bucketItem];
     }
 }
