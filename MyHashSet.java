1d88d89fdba28d40a92d559852e6ef1017acae7b
arshmohd <irfan.arshad4@gmail.com>
2022-04-27 07:26:37 +0530

    Created HashSet implementation with few inital assumptions of ArraySize, max_Value, parentList for bucket in HashTable and then for each bucket a list

--- MyHashSet.java
+++ MyHashSet.java
@@ -1,64 +1,64 @@
-mport java.util.*;
+import java.util.*;
 
 import java.util.*;
 
 public class MyHashSet {
-    private final int MAX_VAL = 1000000;
-    private final int sizeOfArray = 100;
-    private List<List<Integer>> mainBucket;
+  private final int MAX_VAL = 1000000;
+  private final int sizeOfArray = 100;
+  private List<List<Integer>> mainBucket;
 
-    public MyHashSet() {
-        mainBucket = new ArrayList<>(sizeOfArray);
-        for (int idx = 0; idx < sizeOfArray; idx++) {
-            mainBucket.add(null);
-        }
+  public MyHashSet() {
+    mainBucket = new ArrayList<>(sizeOfArray);
+    for (int idx = 0; idx < sizeOfArray; idx++) {
+      mainBucket.add(null);
+    }
+  }
+
+  public void add(int key) {
+    int idx = key % sizeOfArray;
+    List<Integer> lisInBucket = mainBucket.get(idx);
+    if (lisInBucket == null) {
+      List<Integer> list = new LinkedList<>();
+      list.add(key);
+      mainBucket.set(idx, list);
+    } else {
+      if (!lisInBucket.contains(key)) {
+        lisInBucket.add(key);
+      }
+    }
+  }
+
+  public void remove(int key) {
+    int idx = key % sizeOfArray;
+    List<Integer> lisInBucket = mainBucket.get(idx);
+    if (lisInBucket != null) {
+      lisInBucket.remove(Integer.valueOf(key));
     }
 
-    public void add(int key) {
-        int idx = key % sizeOfArray;
-        List<Integer> lisInBucket = mainBucket.get(idx);
-        if (lisInBucket == null) {
-            List<Integer> list = new LinkedList<>();
-            list.add(key);
-            mainBucket.set(idx, list);
-        } else {
-            if (!lisInBucket.contains(key)) {
-                lisInBucket.add(key);
-            }
-        }
-    }
+  }
 
-    public void remove(int key) {
-        int idx = key % sizeOfArray;
-        List<Integer> lisInBucket = mainBucket.get(idx);
-        if (lisInBucket != null) {
-            lisInBucket.remove(Integer.valueOf(key));
-        }
+  public boolean contains(int key) {
+    int idx = key % sizeOfArray;
+    List<Integer> lisInBucket = mainBucket.get(idx);
+    return lisInBucket != null && lisInBucket.contains(key);
 
-    }
+  }
 
-    public boolean contains(int key) {
-        int idx = key % sizeOfArray;
-        List<Integer> lisInBucket = mainBucket.get(idx);
-        return lisInBucket != null && lisInBucket.contains(key);
-
-    }
-
-    public static void main(String args[]) {
-        MyHashSet hs = new MyHashSet();
-        hs.add(1);
-        hs.add(2);
-        System.out.println();
-        boolean is1Present = hs.contains(1);
-        System.out.println(is1Present);
-        boolean is3Present = hs.contains(3);
-        System.out.println(is3Present);
-        hs.add(4);
-        boolean is5Present = hs.contains(5);
-        System.out.println(is5Present);
-        boolean is4Present = hs.contains(4);
-        System.out.println(is4Present);
-    }
+  public static void main(String args[]) {
+    MyHashSet hs = new MyHashSet();
+    hs.add(1);
+    hs.add(2);
+    System.out.println();
+    boolean is1Present = hs.contains(1);
+    System.out.println(is1Present);
+    boolean is3Present = hs.contains(3);
+    System.out.println(is3Present);
+    hs.add(4);
+    boolean is5Present = hs.contains(5);
+    System.out.println(is5Present);
+    boolean is4Present = hs.contains(4);
+    System.out.println(is4Present);
+  }
 }
 
 /**
