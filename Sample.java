7c70be3bfd00adb6a19e8a19fdeddf79760a5910
cart1997 <kn329@njit.edu>
2022-04-27 17:36:40 -0400

    completed, Please review it

--- Sample.java
+++
@@ -1,7 +0,0 @@
-// Time Complexity :   O(1)
-// Space Complexity :  O(1)
-// Did this code successfully run on Leetcode :   Yes
-// Any problem you faced while coding this :    Nope
-
-
-// Your code here along with comments explaining your approach
