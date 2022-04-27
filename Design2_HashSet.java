1938ffa37c7ad952bb83d027341410b76903e130
Rutuja Mandlik <mandlik.r@northeastern.edu>
2022-04-27 01:31:23 -0400

    Design Hashset - Updated with edge case

--- Design2_HashSet.java
+++ Design2_HashSet.java
@@ -1,74 +1,67 @@
-/* Time Complexity: O(1) */
-/* Space Complexity: O(n) */
-/* Did this code successfully run on Leetcode : Yes */
-/* Any problem you faced while coding this : I faced some syntactical adn compilation errors. ALoso i had to take help from the google solution to proceed with the problem*/
-
+//TC: O(1) and will not be taking TC of constructor bcz its one time activity and its not user focused 
+//using double hashing
+//primary arr needs to store nulls as the pointers and secondary arr initiated as boolean will be zero by default 
 class MyHashSet {
     
-    private final int max_value = 1000000;
-    private final int array_size = 1000;
-    private List<List<Integer>> parentList;
+    private boolean [][] storage;
+    int buckets;
+    int bucketItems;
 
-    
-    /* Constructor where I initiliazed my Data structure */
-    public MyHashSet() {
-        parentList = new ArrayList<>(array_size);
-        for(int i = 0 ; i < array_size ; i++){
-            parentList.add(null);
-        }
-    }
-    
-    public void add(int key) {
-       /*Mapping int to one bucket in parentList using modulo  */
-        int index =  key % array_size;
-        
-        /*Taking childList for that bucket where the key needs to go */
-        List<Integer> childList = parentList.get(index);
-        if (childList == null){
-            List<Integer> List = new LinkedList();
-            List.add(key);
-            parentList.set(index, List);
-        } 
-        else {
-            if(!childList.contains(key)){
-                childList.add(key);
-            }
-            
-        }
-        
-    
-    }
-    
-    public void remove(int key) {
-        
-    /* To find the bucket where that key might be present */
-        int index = key % array_size;
-        
-    /*Taking childList for that bucket where the key is present */
-        List<Integer> childList = parentList.get(index);
-        
-        if(childList != null){
-            childList.remove(Integer.valueOf(key));  /* Used Integer.valueOf() to remove the int element & not place */
-            
-        }
-        
-    }
-    
-    public boolean contains(int key) {
-        /* To find the bucket where that key might be present */
-        int index = key % array_size;
-        
-    /*Taking childList for that bucket where the key is present */
-        List<Integer> childList = parentList.get(index);
-        
-        return childList !=null && childList.contains(key);
-    }
+   
+   public int bucket(int value){ 
+       return value%buckets;
+   }
+
+   public int bucketItem(int value){ 
+       return value/buckets;
+   }
+
+   /** Initialize your data structure here. */
+
+   public MyHashSet() {
+       this.buckets = 1000;
+       this.bucketItems = 1000;
+       this.storage = new boolean[buckets][];
+
+   }
+
+   public void add(int value ) {
+
+       int bucket = bucket(value);      
+       int bucketItem = bucketItem(value);
+       
+       if(storage[bucket] == null){
+           if(bucket == 0) {
+               //edge case to handle last element as 1000000/1000=0, we need extra space at the end
+                storage[bucket] = new boolean[bucketItems + 1]; 
+           } else {
+                storage[bucket] = new boolean[bucketItems];
+           }
+          
+       }
+       storage[bucket][bucketItem] = true;
+
+   }
+
+   public void remove(int value) {
+
+        int bucket = bucket(value);
+        int bucketItem = bucketItem(value);
+
+       if(storage[bucket] != null){
+           storage[bucket][bucketItem] = false;
+       }
+
+   }
+
+   /** Returns true if this set contains the specified element */
+
+   public boolean contains(int value) {
+       
+       int bucket = bucket(value);
+       int bucketItem = bucketItem(value);
+       return storage[bucket] != null && storage[bucket][bucketItem];
+
+   }
+
 }
-
-/**
- * Your MyHashSet object will be instantiated and called as such:
- * MyHashSet obj = new MyHashSet();
- * obj.add(key);
- * obj.remove(key);
- * boolean param_3 = obj.contains(key);
- */
