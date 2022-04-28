6e6a222040e6866d8812016c8b8bc9d5692d6e44
sejalvyas13 <sejalvyas13@gmail.com>
2022-04-27 22:30:38 -0400

    Update QueueUsingStack.py

--- QueueUsingStack.py
+++ QueueUsingStack.py
@@ -1,7 +1,7 @@
-// Time Complexity :
-// Space Complexity :
-// Did this code successfully run on Leetcode :
-// Any problem you faced while coding this :
+// Time Complexity : push O(1), pop O(N), peek O(N), empty O(N)
+// Space Complexity : O(1)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : No
 
 
 class MyQueue:
