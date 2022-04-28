cc006e3f32e92e35e2e8a733171869520303113d
aashutoshtaikar <aashutoshtaikar@gmail.com>
2022-04-27 21:24:49 -0400

    Hashset impl complete

--- Problem2.java
+++ Problem2.java
@@ -1,4 +1,4 @@
-class HashSet{
+class MyHashSet{
     private boolean[][] storage;
     private final int BUCKETS;
     private final int BUCKET_ITEMS;
@@ -10,15 +10,21 @@ class HashSet{
     }
 
     // first hash function
+    // Time Complexity : O(1)
+    // Space Complexity : O(1)
     private int bucketHash(int key){
         return key % BUCKETS;
     }
 
     // second hash function
+    // Time Complexity : O(1)
+    // Space Complexity : O(1)
     private int bucketItemsHash(int key){
         return key / BUCKET_ITEMS;
     }
     
+    // Time Complexity : O(1)
+    // Space Complexity : O(1)
     public void add(int key){
         int bucket = bucketHash(key);
         int bucketItem = bucketItemsHash(key);
@@ -31,6 +37,8 @@ class HashSet{
         storage[bucket][bucketItem] = true;
     }
     
+    // Time Complexity : O(1)
+    // Space Complexity : O(1)
     public boolean contains(int key){
         int bucket = bucketHash(key);
         int bucketItem = bucketItemsHash(key);
@@ -38,6 +46,8 @@ class HashSet{
         return storage[bucket][bucketItem];
     }
     
+    // Time Complexity : O(1)
+    // Space Complexity : O(1)
     public void remove(int key){
         int bucket = bucketHash(key);
         int bucketItem = bucketItemsHash(key);
@@ -48,6 +58,10 @@ class HashSet{
 
 public class Problem2 {
     public static void main(String[] args) {
-        
+        MyHashSet obj = new MyHashSet();
+        obj.add(22);
+        obj.remove(22);
+        boolean param_3 = obj.contains(22);
+        System.out.println(param_3);
     }
 }
