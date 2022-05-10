57b5d791fa402e1cdde356684eaebbdea9ce46c1
Leelamanoj-06 <68564835+Leelamanoj-06@users.noreply.github.com>
2022-05-09 22:32:18 -0400

    Completed Design 2

---
+++ MyQueue.java
@@ -0,0 +1,32 @@
+// Implement Queue using Stacks
+class MyQueue {
+    private Stack<Integer> in;
+    private Stack<Integer> out;
+
+    public MyQueue() {
+        in = new Stack<>();
+        out = new Stack<>();
+    }
+    
+    public void push(int x) {
+        in.push(x);
+    }
+    
+    public int pop() {
+        peek();
+        return out.pop();
+    }
+    
+    public int peek() {
+        if(out.isEmpty()){
+            while(!in.isEmpty()){
+                out.push(in.pop());
+            }
+        }
+        return out.peek();
+    }
+    
+    public boolean empty() {
+        return in.isEmpty() && out.isEmpty();
+    }
+}
