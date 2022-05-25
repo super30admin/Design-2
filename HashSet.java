9c0ad501793637ff5c25f4be790d9b4e14447990
Vraj Himanshu Reshamdalal <30725458+vraj9845@users.noreply.github.com>
2022-05-25 10:00:51 -0400

    Update HashSet.java

--- HashSet.java
+++ HashSet.java
@@ -1,87 +1,58 @@
 class MyHashSet {
-    class Node{
-        int val;
-        Node next;
-        public Node(int x) {
-this.val=x;
-this.next=null;
-        }
+    int buckets;
+    int bucketItems;
+    boolean storage[][];
+    
+    public int hashFunc(int key){
+        return key%1000;
     }
-
-    private Node[] arr;
-    private int size;
-
+     public int hashFunc2(int key){
+        return key/1000;
+    }
 
     public MyHashSet() {
-this.size=8;
-this.arr = new Node[size];
+        this.buckets=1000;
+        this.bucketItems=1000;
+        this.storage= new boolean[buckets][];
     }
-
-    public int hashFunc(int key){
-        return key%10;
-    }
-
+    
     public void add(int key) {
         int index = hashFunc(key);
-        if(arr[index]==null){
-        arr[index]= new Node(key);
-        } else if(arr[index].val==key) {
-            return;
-        } else {
-            //find the next FREE index
-            Node head = arr[index];
-            while(head.next!=null){
-                if(head.val==key) {
-                    return;
-                }
-                head=head.next;
+        int index2=hashFunc2(key);
+        if(storage[index]==null){
+            if(index==0){
+                storage[index]= new boolean[bucketItems+1];
+            } else {
+                storage[index]= new boolean[bucketItems];
             }
-            head.next = new Node(key);
         }
+        storage[index][index2]=true;
     }
-
+    
     public void remove(int key) {
         int index = hashFunc(key);
-            Node head = arr[index];
-            if(head!=null&&head.val==key) {
-                System.out.println("Successfully removed.");
-            head=null;
+        int index2=hashFunc2(key);
+        if(storage[index]==null){
             return;
-            }
-            Node head2 = head.next;
-            while (head2!=null){
-                if(head.val==key){
-                    System.out.println("Successfully removed.");
-                    head.next=head2.next;
-                }
-                head=head.next;
-                head2=head2.next;
-            }
+        }
+        storage[index][index2]=false;
+        
     }
-
+    
     public boolean contains(int key) {
         int index = hashFunc(key);
-
-            Node head = arr[index];
-            while (head!=null){
-                if(head.val==key){
-                    System.out.println("Successfully Found.");
-                    return true;
-
-                }
-            }
-        return false;
-    }
-    public static void main(String[] args){
-        MyHashSet hashSet = new MyHashSet();
-        hashSet.add(1);
-        hashSet.add(2);
-       System.out.println(hashSet.contains(1));    // returns true
-        System.out.println(hashSet.contains(3));    // returns false (not found)
-        hashSet.add(2);
-        System.out.println(hashSet.contains(2));    // returns true
-        hashSet.remove(2);
-        hashSet.remove(2);
-        System.out.println(hashSet.contains(2));    // returns false (already removed)
+        int index2=hashFunc2(key);
+        if(storage[index]==null){
+            return false;
+        }
+        return storage[index][index2];
     }
 }
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet obj = new MyHashSet();
+ * obj.add(key);
+ * obj.remove(key);
+ * boolean param_3 = obj.contains(key);
+ */
