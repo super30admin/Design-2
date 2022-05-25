45710bcab234c62062ad20587b887d0a63a75133
rohithjallipalli <rohith.jallipalli@gmail.com>
2022-05-25 17:20:25 -0500

    Done

--- Queue using Stack.py
+++ Queue using Stack.py
@@ -34,9 +34,9 @@ class MyQueue:
     def empty(self) -> bool:
         return self.s1 == []
 
-# Your MyQueue object will be instantiated and called as such:
-# obj = MyQueue()
-# obj.push(x)
-# param_2 = obj.pop()
-# param_3 = obj.peek()
-# param_4 = obj.empty()
+obj = MyQueue()
+obj.push(1)
+obj.push(2)
+print(obj.peek())
+print(obj.pop())
+print(obj.empty())
