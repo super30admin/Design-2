81169902215a3aab1f641ef51d5a3cedcf3069a0
Mukta Parab <78190285+mukta2695@users.noreply.github.com>
2022-05-26 11:23:24 -0400

    Update QueueUsingStack.py

--- QueueUsingStack.py
+++ QueueUsingStack.py
@@ -1,8 +1,19 @@
+'''// Time Complexity : O(1)
+// Space Complexity : O(n)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : No
+
+
+// Your code here along with comments explaining your approach'''
+
 from collections import deque
 class MyQueue(object):
 
     def __init__(self):
+        #Input stack
         self.stack1=deque()
+        
+        #Output Stack
         self.stack2=deque()
 
     def push(self, x):
@@ -10,6 +21,7 @@ class MyQueue(object):
         :type x: int
         :rtype: None
         """
+        #Append only in the input stack
         self.stack1.append(x)
         
 
@@ -17,22 +29,33 @@ class MyQueue(object):
         """
         :rtype: int
         """
+        #If both the stacks are empty
         if self.empty():
             print("Queue is empty")
+            
+         #else if only when ouput stack is empty
         elif len(self.stack2) ==0:
+            #Transfer elements from input stack to output stack until input stack is not empty
             while len(self.stack1) != 0:
                 self.stack2.append(self.stack1.pop())
+                
+        #return the removed top element from the output stack 
         return self.stack2.pop()
     
     def peek(self):
         """
         :rtype: int
         """
-        if len(self.stack1) == 0 and len(self.stack2) ==0:
+        #If both the stacks are empty
+        if self.empty():
             print("Queue is empty")
+        #else if only when ouput stack is empty
         elif len(self.stack2) ==0:
+            #Transfer elements from input stack to output stack until input stack is not empty
             while len(self.stack1) != 0:
                 self.stack2.append(self.stack1.pop())
+         
+         #return the top element from the output stack, Don't remove
         return self.stack2[-1]
         
 
