1a47a4bfcda20a3309424a8aff07d29ac5625439
Siddesh2044 <60492570+Siddesh2044@users.noreply.github.com>
2022-05-26 13:13:19 -0400

    Update Sample.java
    Completed design-hashmap in Design-2

--- Sample.java
+++ Sample.java
@@ -5,3 +5,65 @@
 
 
 // Your code here along with comments explaining your approach
+// Used double hashing technique
+
+// Two hash functions used in this problem are hash1 and hash2
+
+//Initialized the primary array to 1000 and secondary array to 1000 because the we are the data range of 1000000
+
+//Secondary array is initialized only if we get a hash code value of o from hash1 function
+
+class MyHashSet {
+    
+    private boolean[][] storage;
+    int buckets;
+    int bucketLists;
+
+    public MyHashSet() {
+        buckets = 1000;
+        bucketLists = 1000;
+        storage = new boolean[buckets][];
+        
+    }
+    
+    private int hash1(int key) {
+        return key % 1000;
+    }
+    
+    private int hash2(int key) {
+        return key / 1000;
+    }
+    
+    public void add(int key) {
+        int hash1 = hash1(key);
+        int hash2 = hash2(key);
+        if(storage[hash1] == null) {
+            if(hash1 == 0){
+                storage[hash1] = new boolean[bucketLists + 1];
+            }
+                
+            else {
+                storage[hash1] = new boolean[bucketLists];
+            }
+        }    
+        
+        storage[hash1][hash2] = true;
+        
+    }
+    
+    public void remove(int key) {
+        int hash1 = hash1(key);
+        int hash2 = hash2(key);
+        if(storage[hash1] == null) return;
+        storage[hash1][hash2] = false;
+        
+    }
+    
+    public boolean contains(int key) {
+        int hash1 = hash1(key);
+        int hash2 = hash2(key);
+        if(storage[hash1] == null) return false;
+        return storage[hash1][hash2];
+        
+    }
+}
