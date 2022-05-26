558085dfe938c133950fc1c2a13890691facf6b2
Prasad Gagarani <52399176+prasadgagarani@users.noreply.github.com>
2022-05-26 13:13:35 -0400

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -90,6 +90,10 @@ class MyHashMap {
         
     }
 //****DESIGN A QUEUE****
+//Time complexity:o(1);
+//space complexity:o(1);
+// Did this code successfully run on Leetcode : yes
+// Any problem you faced while coding this : NO
 
 class MyQueue {
     Stack<Integer> instack;
