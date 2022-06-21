63f5596362feffcdeb6f6c29a45bca988f9c2455
kunalvmw <kunalmwadhwa@gmail.com>
2022-06-22 03:18:11 +0530

    Completed Sprint HashSet

--- MyHashSet.java
+++ MyHashSet.java
@@ -1,47 +1,57 @@
-//time complexity is 0(n)
-// space complexity is 0(n)
-
-import java.util.ArrayList;
-import java.util.List;
-
+// time complexity 0(1)
+// space complexity 0(n)
 class MyHashSet {
-    List<Integer> set;
+    boolean [][]storage;
+    int buckets;
+    int bucketItems;
     public MyHashSet() {
-        set= new ArrayList<>();
-
+        buckets = 1000;
+        bucketItems =1000;
+        storage = new boolean[buckets][];
     }
-
+    private int getBucket(int key)
+    {
+        return key % buckets;
+    }
+    private int getBucketItems(int key)
+    {
+        return key / bucketItems;
+    }
     public void add(int key) {
-        if(set.contains(key)==false)
+        int bucket = getBucket(key);
+        int bucketItem = getBucketItems(key);
+        if(storage[bucket]== null)
         {
-            set.add(key);
+            if(bucket==0)
+            {
+                storage[bucket]=new boolean[bucketItems+1];
+            }
+            else
+            {
+                storage[bucket]=new boolean[bucketItems];
+            }
         }
+
+        storage[bucket][bucketItem]=true;
 
     }
 
     public void remove(int key) {
-        if(set.contains(key)==true)
-        {
-            set.remove(set.indexOf(key));
-        }
+        int bucket = getBucket(key);
+        int bucketItem = getBucketItems(key);
+        if(storage[bucket]== null)
+            return;
+        else
+            storage[bucket][bucketItem]=false;
+
     }
 
     public boolean contains(int key) {
-        if(set.contains(key)==true)
-        {
-            return true;
-        }
-        else
-        {
+        int bucket = getBucket(key);
+        int bucketItem = getBucketItems(key);
+        if(storage[bucket]== null)
             return false;
-        }
+        return storage[bucket][bucketItem];
+
     }
 }
-
-/**
- * Your MyHashSet object will be instantiated and called as such:
- * MyHashSet obj = new MyHashSet();
- * obj.add(key);
- * obj.remove(key);
- * boolean param_3 = obj.contains(key);
- */
