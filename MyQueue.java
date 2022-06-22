b7addf829e7cf801d8351eedcfb8d933a40e968a
Tushar <tusharpawar611>
2022-06-21 22:56:39 -0700

    queue design submitted

--- MyQueue.java
+++ MyQueue.java
@@ -7,19 +7,32 @@ class MyQueue {
     }
     
     public void push(int x) {
-        
+        s1.push(x);
     }
     
     public int pop() {
-        
+        while(!s1.isEmpty()){
+            s2.push(s1.pop());
+        }
+        return s2.pop();
+        while(!s2.isEmpty()){
+            s1.push(s2.pop());
+        }
     }
     
     public int peek() {
-        
+        while(!s1.isEmpty()){
+            s2.push(s1.pop());
+        }
+        return s2.peek();
+
+        while(!s2.isEmpty()){
+            s1.push(s2.pop());
+        }
     }
     
     public boolean empty() {
-        
+        return s1.isEmpty();
     }
 }
 
