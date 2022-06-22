2f6ec1be4478a514f6849b9f6b60d83840b90a74
AishwaryaBabu2 <107521970+AishwaryaBabu2@users.noreply.github.com>
2022-06-21 21:05:48 -0700

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -5,84 +5,48 @@
 
 
 // Your code here along with comments explaining your approach
-class MyHashSet {
-    
-    boolean [][] storage;
-    int OutterArrayLen;
-    int InnerArrayLen;
-    
-public MyHashSet() {
-        OutterArrayLen=1000;
-        InnerArrayLen=1000;
-        storage= new boolean[OutterArrayLen][];
-    }
-    private int hash1(int key)
+       class MinStack {
+int min=Integer.MAX_VALUE;
+        Stack<Integer> stack=new Stack<>();
+    public MinStack() {
         
-    {
-        return key % OutterArrayLen;
     }
     
-    private int hash2(int key)
-    {
-        return key/InnerArrayLen;
-    }
-    public void add(int key) 
-    {
-        int OutterArrayIndex =hash1(key);
-        int InnerArrayIndex=hash2(key);
-        if(storage[OutterArrayIndex]==null)
+    public void push(int val) {
+        if(val<=min)
         {
-            if(OutterArrayIndex==0)
-            {
-               storage[OutterArrayIndex]=new boolean[InnerArrayLen+1]; 
-            }
-            else
-            {
-               storage[OutterArrayIndex]=new boolean[InnerArrayLen];  
-            }
+            stack.push(min);
+               min=val;
+            
         }
-        storage[OutterArrayIndex][InnerArrayIndex]=true;
-    }
-    
-      public void remove(int key)
-    {
-        int OutterArrayIndex =hash1(key);
-        int InnerArrayIndex=hash2(key);
-          if(storage[OutterArrayIndex]!=null)
-          {
-               if(storage[OutterArrayIndex][InnerArrayIndex]==true)
-                    {
-                        storage[OutterArrayIndex][InnerArrayIndex]=false;
-                    }
-          }
-       
+        stack.push(val);
         
     }
     
-     public boolean contains(int key)  {
+    public void pop() {
         
-        
-        int OutterArrayIndex =hash1(key);
-        int InnerArrayIndex=hash2(key);
-         if(storage[OutterArrayIndex]==null)
-             
-         {
-             return false;
-             
-         }
-         else
-         {
-              return storage[OutterArrayIndex][InnerArrayIndex];
-         }
-       
+        if(min==stack.pop())
+        {
+            min=stack.pop();
+        }
         
     }
-};
+    
+    public int top() {
+        return stack.peek();
+        
+    }
+    
+    public int getMin() {
+        return min;
+    } 
+}
 
 /**
- * Your MyHashSet object will be instantiated and called as such:
- * MyHashSet* obj = new MyHashSet();
- * obj->add(key);
- * obj->remove(key);
- * bool param_3 = obj->contains(key);
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(val);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
  */
