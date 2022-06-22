c3b2d0c782b35e85a0e8b45b123e40c485e52779
Tushar <tusharpawar611>
2022-06-21 22:56:00 -0700

    queue design submitted

---
+++ MyQueue.java
@@ -0,0 +1,33 @@
+class MyQueue {
+    Stack<Integer> s1 = new Stack();
+    Stack<Integer> s2 = new Stack();
+
+    public MyQueue() {
+        
+    }
+    
+    public void push(int x) {
+        
+    }
+    
+    public int pop() {
+        
+    }
+    
+    public int peek() {
+        
+    }
+    
+    public boolean empty() {
+        
+    }
+}
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue obj = new MyQueue();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.peek();
+ * boolean param_4 = obj.empty();
+ */
