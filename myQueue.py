e27e4a28a61f5b56053d338c2696b9dcec816b0c
Pranjali Seth <pranjaliseth@Pranjalis-MacBook-Air.local>
2022-06-22 21:04:37 -0700

    Updated File

---
+++ myQueue.py
@@ -0,0 +1,42 @@
+#Time Complexity = O(1)
+#Space Complexity = O(n)
+
+
+
+class MyQueue:
+
+    def __init__(self):
+        self.inStack = []
+        self.outStack = []
+        
+
+    def push(self, x: int) -> None:
+        self.inStack.append(x)
+        
+
+    def pop(self) -> int:    
+        if not self.outStack:
+            while(self.inStack):
+                self.outStack.append(self.inStack.pop())
+        return self.outStack.pop()
+        
+        
+
+    def peek(self) -> int:
+        if not self.outStack:
+            while(self.inStack):
+                self.outStack.append(self.inStack.pop())
+        
+        return self.outStack[-1]
+    
+    
+    def empty(self) -> bool:
+        return not self.inStack and not self.outStack
+
+
+# Your MyQueue object will be instantiated and called as such:
+# obj = MyQueue()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.peek()
+# param_4 = obj.empty()
