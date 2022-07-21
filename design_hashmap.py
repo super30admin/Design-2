576eca93c460d8e976172bbc8bd77a33e29b5041
Saiteja Garlapati <86909787+lostrider07@users.noreply.github.com>
2022-07-21 13:05:54 -0700

    Update design_hashmap.py

--- design_hashmap.py
+++ design_hashmap.py
@@ -26,9 +26,54 @@ class MyHashMap:
             return -1
         
 
+# Using Linear Chaining
 
-# Your MyHashMap object will be instantiated and called as such:
-# obj = MyHashMap()
-# obj.put(key,value)
-# param_2 = obj.get(key)
-# obj.remove(key)
+# class Node:
+#     def __init__(self, key, value):
+#         self.key = key
+#         self.value = value
+#         self.next = None
+
+# class MyHashMap:
+#     def __init__(self):
+#         self.size = 10000
+#         self.hashmap = [None] * self.size
+
+#     def hash(self, key):
+#         return key % self.size
+
+#     def put(self, key, value):
+#         x = self.hash(key)
+#         temp = self.hashmap[x]
+#         if not temp:
+#             self.hashmap[x] = Node(key, value)
+#         else:
+#             while temp:
+#                 if temp.key == key:
+#                     temp.value = value
+#                     return
+#                 prev = temp
+#                 temp = temp.next
+#             prev.next = Node(key, value)
+
+#     def get(self, key):
+#         x = self.hash(key)
+#         temp = self.hashmap[x]
+#         while temp:
+#             if temp.key == key:
+#                 return temp.value
+#             temp = temp.next
+#         return -1
+
+#     def remove(self, key):
+#         x = self.hash(key)
+#         temp = self.hashmap[x]
+#         if temp and temp.key == key:
+#             self.hashmap[x] = temp.next
+#             return
+#         while temp:
+#             if temp.key == key:
+#                 prev.next = temp.next
+#                 return
+#             prev = temp
+#             temp = temp.next
