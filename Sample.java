6211d947c47412c8db0b77d1cf4dd429feb80827
tanayapanditrao15 <89812452+tanayapanditrao15@users.noreply.github.com>
2022-08-17 16:04:49 -0500

    Update Design2 MinStack

--- Sample.java
+++ Sample.java
@@ -1,7 +1,52 @@
-// Time Complexity :
-// Space Complexity :
+// Time Complexity : O(1)
+// Space Complexity : O(n) as we are using two stacks
 // Did this code successfully run on Leetcode :
 // Any problem you faced while coding this :
 
 
 // Your code here along with comments explaining your approach
+
+class MinStack {
+    private Stack<Integer> st;
+    private Stack<Integer> minStack;
+    private int min;
+
+    public MinStack() {
+        this.st = new Stack<>();
+        this.minStack = new Stack<>();
+        this.min = Integer.MAX_VALUE;
+        minStack.push(min);
+    }
+   
+   
+    public void push(int val) {
+        min = Math.min(min,val);
+        st.push(val);
+        minStack.push(min);
+       
+    }
+   
+    public void pop() {
+        st.pop();
+        minStack.pop();
+        min = minStack.peek();
+    }
+   
+    public int top() {
+        return st.peek();
+       
+    }
+   
+    public int getMin() {
+        return min;
+    }
+}
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(val);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
