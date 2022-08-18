66a25535d56f8bbde7a1282ff317044b171bd35b
cskp009 <110018417+cskp009@users.noreply.github.com>
2022-08-18 01:32:38 -0700

    Create 706. Design HashMap.java

---
+++ 706. Design HashMap.java
@@ -0,0 +1,19 @@
+class MyHashMap {
+
+    public MyHashMap() {
+        
+    }
+    
+    public void put(int key, int value) {
+        
+    }
+    
+    public int get(int key) {
+        
+    }
+    
+    public void remove(int key) {
+        
+    }
+}
+
