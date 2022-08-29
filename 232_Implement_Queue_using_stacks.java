5dfe12a5851ea7f6f7981224d7692fc25bab81bc
Dhruvil Shah <dhruvilshah800@gmail.com>
2022-08-29 11:19:23 -0700

    Please Review

---
+++ 232_Implement_Queue_using_stacks.java
@@ -0,0 +1,30 @@
+class MyQueue {
+    Stack<Integer> in;
+    Stack<Integer> out;
+    public MyQueue() {
+        this.in = new Stack<>();
+        this.out = new Stack<>();
+    }
+    
+    public void push(int x) {
+        in.push(x);
+    }
+    
+    public int pop() {
+        peek();
+        return out.pop();
+    }
+    
+    public int peek() {
+        if(out.isEmpty()){
+            while(!in.isEmpty()){
+                out.push(in.pop());
+            }
+        }
+        return out.peek();
+    }
+    
+    public boolean empty() {
+        return in.isEmpty() && out.isEmpty(); 
+    }
+}
