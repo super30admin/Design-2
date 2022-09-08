19cd608a9b5a1cd5745d8a276822df73a5a02c77
SagarKhandagale <khandagale.s@northeastern.edu>
2022-09-07 20:53:42 -0400

    Completed Design-2

---
+++ Problem1.java
@@ -0,0 +1,55 @@
+// Time Complexity : O(1)
+// Space Complexity : O(n)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : No
+
+
+// Your code here along with comments explaining your approach
+
+class MyQueue {
+    Stack<Integer> s1;
+    Stack<Integer> s2;
+
+    public MyQueue() {
+        s1 = new Stack<>();
+        s2 = new Stack<>();
+    }
+
+    public void push(int x) {
+        // push new value to stack 1
+        s1.push(x);
+    }
+
+    public int pop() {
+        // if stack 2 is empty then pop all values from stack 1 and push them to stack 2 (This will give values desired order as queue)
+        if(s2.isEmpty()){
+            while(!s1.isEmpty()){
+                s2.push(s1.pop());
+            }
+        }
+        return s2.pop();
+    }
+
+    public int peek() {
+        if(s2.isEmpty()){
+            while(!s1.isEmpty()){
+                s2.push(s1.pop());
+            }
+        }
+        return s2.peek();
+    }
+
+    public boolean empty() {
+        // If both the stacks are empty then we can say queue is empty
+        return (s1.isEmpty() && s2.isEmpty());
+    }
+}
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue obj = new MyQueue();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.peek();
+ * boolean param_4 = obj.empty();
+ */
