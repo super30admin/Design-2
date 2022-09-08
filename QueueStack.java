990444d9fa4e381a44ba769636d4dcf0eeceeffb
Saurabh Kulkarni <srbhkulkarni13@gmail.com>
2022-09-08 22:58:54 +0000

    Added TS Complexities

--- QueueStack.java
+++ QueueStack.java
@@ -1,3 +1,11 @@
+// Time Complexity : O(1)
+// Space Complexity : O(n)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : None.
+
+
+// Your code here along with comments explaining your approach
+
 class MyQueue {
 
 	Stack<Integer> in;
