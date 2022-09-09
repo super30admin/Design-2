79a539ee229e250c62bcab9260be6b13e92e3235
Ruchika Sharma <sharma.ruc@northeastern.edu>
2022-09-08 20:47:37 -0400

    Completed Design-2 Please review

---
+++ QueueUsingStack.java
@@ -0,0 +1,43 @@
+class MyQueue {
+
+    Stack<Integer> in;
+    Stack<Integer> out;
+
+    public MyQueue() {
+        in = new Stack<>();
+        out= new Stack<>();
+    }
+    
+    //O(1)
+    public void push(int x) {
+        in.push(x);
+    }
+    
+    //O(1)
+    public int pop() {
+        peek();
+        return out.pop();
+    }
+    //O(1) - average
+    public int peek() {
+        if(out.isEmpty()){
+            while(!in.isEmpty()){
+                out.push(in.pop());
+            }
+        }
+        return out.peek();
+    }
+    
+    public boolean empty() {
+        return in.isEmpty() && out.isEmpty();
+    }
+}
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue obj = new MyQueue();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.peek();
+ * boolean param_4 = obj.empty();
+ */
