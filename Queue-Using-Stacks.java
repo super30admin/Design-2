132191b2c6b9d2bb7f2c26e169e8c1618ec76f2f
Nimay Patel <60466954+Nimay-1709@users.noreply.github.com>
2022-09-08 17:26:37 -0700

    Create Queue-Using-Stacks.java

---
+++ Queue-Using-Stacks.java
@@ -0,0 +1,39 @@
+class MyQueue {
+private Stack<Integer> s1 = new Stack<>();
+private Stack<Integer> s2 = new Stack<>();
+int front;
+// Push element x to the back of queue.
+public void push(int x) {
+    if (s1.empty())
+        front = x;
+    s1.push(x);
+}
+     public int pop() {
+        if (s2.isEmpty()) {
+            while (!s1.isEmpty())
+                s2.push(s1.pop());
+        }
+        return s2.pop();
+    }
+    
+// Return whether the queue is empty.
+public boolean empty() {
+    return s1.isEmpty() && s2.isEmpty();
+}
+   // Get the front element.
+public int peek() {
+    if (!s2.isEmpty()) {
+            return s2.peek();
+    }
+    return front;
+} 
+}
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue obj = new MyQueue();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.peek();
+ * boolean param_4 = obj.empty();
+ */
