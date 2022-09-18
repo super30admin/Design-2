869c5303ecb62c31b1fb2f1a2daf3dfa0cbfb68c
Suhas Dhar <suhasdhar@gmail.com>
2022-09-18 16:00:42 -0500

    code

--- Sample.java
+++ Sample.java
@@ -1,5 +1,5 @@
-// Time Complexity :
-// Space Complexity :
+// Time Complexity : O(1) for push & pop
+// Space Complexity : O(n)
 // Did this code successfully run on Leetcode :
 // Any problem you faced while coding this :
 
