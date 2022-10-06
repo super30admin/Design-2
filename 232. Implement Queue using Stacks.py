447415c0e48e7e6c7a2465bb212a36c796c938e0
Krishna Vardhan <36471981+krishnamandanapu@users.noreply.github.com>
2022-10-06 18:10:07 -0400

    Completed LC 232

---
+++ 232. Implement Queue using Stacks.py
@@ -0,0 +1,25 @@
+class MyQueue:
+
+    def __init__(self):
+        self.stack1 = []
+        self.stack2 = []
+        self.size = 0
+
+    def push(self, x: int) -> None:
+        self.stack1.append(x)
+
+    def pop(self) -> int:
+        if not self.stack2:
+            self.peek()
+
+        return self.stack2.pop()
+
+    def peek(self) -> int:
+        if not self.stack2:
+            while self.stack1:
+                self.stack2.append(self.stack1.pop())
+
+        return self.stack2[-1]
+
+    def empty(self) -> bool:
+        return len(self.stack1) + len(self.stack2) == 0
