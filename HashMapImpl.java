553f424b462826b8ffe28cb641beb29c5719042c
katta332 <praveenkumar.kpc@gmail.com>
2022-10-26 21:29:04 -0600

    Completed Design -2 assignment please review

--- HashMapImpl.java
+++ HashMapImpl.java
@@ -1,37 +1,22 @@
-{\rtf1\ansi\ansicpg1252\cocoartf2636
-\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fnil\fcharset0 Menlo-Regular;}
-{\colortbl;\red255\green255\blue255;\red0\green0\blue255;\red255\green255\blue255;\red0\green0\blue0;
-\red32\green108\blue135;\red0\green0\blue109;\red101\green76\blue29;\red19\green118\blue70;\red157\green0\blue210;
+//Time complexity is O(1) as array lookup is O(1)
+//Space complexity is O(N) where N is 1000006
+class MyHashMap {
+    int size = (int)Math.pow(10, 6)+1;
+    int[] hm = new int[size];
+    public MyHashMap() {
+        Arrays.fill(hm, -1);
+    }
+    
+    public void put(int key, int value) {
+        hm[key] = value;
+    }
+    
+    public int get(int key) {
+        return hm[key];
+    }
+    
+    public void remove(int key) {
+        hm[key]=-1;
+    }
 }
-{\*\expandedcolortbl;;\cssrgb\c0\c0\c100000;\cssrgb\c100000\c100000\c100000;\cssrgb\c0\c0\c0;
-\cssrgb\c14902\c49804\c60000;\cssrgb\c0\c6275\c50196;\cssrgb\c47451\c36863\c14902;\cssrgb\c3529\c52549\c34510;\cssrgb\c68627\c0\c85882;
-}
-\margl1440\margr1440\vieww11520\viewh8400\viewkind0
-\deftab720
-\pard\pardeftab720\partightenfactor0
 
-\f0\fs26 \cf2 \cb3 \expnd0\expndtw0\kerning0
-\outl0\strokewidth0 \strokec2 //Time complexity is O(1) as array lookup is O(1)\
-//Space complexity is O(N) where N is 1000006\
-class\cf0 \strokec4  \cf5 \strokec5 MyHashMap\cf0 \strokec4  \{\cb1 \
-\pard\pardeftab720\partightenfactor0
-\cf0 \cb3     \cf5 \strokec5 int\cf0 \strokec4  \cf6 \strokec6 size\cf0 \strokec4  = (\cf5 \strokec5 int\cf0 \strokec4 )\cf6 \strokec6 Math\cf0 \strokec4 .\cf7 \strokec7 pow\cf0 \strokec4 (\cf8 \strokec8 10\cf0 \strokec4 , \cf8 \strokec8 6\cf0 \strokec4 )+\cf8 \strokec8 1\cf0 \strokec4 ;\cb1 \
-\cb3     \cf5 \strokec5 int\cf0 \strokec4 [] \cf6 \strokec6 hm\cf0 \strokec4  = \cf9 \strokec9 new\cf0 \strokec4  \cf5 \strokec5 int\cf0 \strokec4 [size];\cb1 \
-\cb3     \cf2 \strokec2 public\cf0 \strokec4  \cf7 \strokec7 MyHashMap\cf0 \strokec4 () \{\cb1 \
-\cb3         \cf6 \strokec6 Arrays\cf0 \strokec4 .\cf7 \strokec7 fill\cf0 \strokec4 (hm, -\cf8 \strokec8 1\cf0 \strokec4 );\cb1 \
-\cb3     \}\cb1 \
-\cb3     \cb1 \
-\cb3     \cf2 \strokec2 public\cf0 \strokec4  \cf5 \strokec5 void\cf0 \strokec4  \cf7 \strokec7 put\cf0 \strokec4 (\cf5 \strokec5 int\cf0 \strokec4  \cf6 \strokec6 key\cf0 \strokec4 , \cf5 \strokec5 int\cf0 \strokec4  \cf6 \strokec6 value\cf0 \strokec4 ) \{\cb1 \
-\cb3         hm[key] = value;\cb1 \
-\cb3     \}\cb1 \
-\cb3     \cb1 \
-\cb3     \cf2 \strokec2 public\cf0 \strokec4  \cf5 \strokec5 int\cf0 \strokec4  \cf7 \strokec7 get\cf0 \strokec4 (\cf5 \strokec5 int\cf0 \strokec4  \cf6 \strokec6 key\cf0 \strokec4 ) \{\cb1 \
-\cb3         \cf9 \strokec9 return\cf0 \strokec4  hm[key];\cb1 \
-\cb3     \}\cb1 \
-\cb3     \cb1 \
-\cb3     \cf2 \strokec2 public\cf0 \strokec4  \cf5 \strokec5 void\cf0 \strokec4  \cf7 \strokec7 remove\cf0 \strokec4 (\cf5 \strokec5 int\cf0 \strokec4  \cf6 \strokec6 key\cf0 \strokec4 ) \{\cb1 \
-\cb3         hm[key]=-\cf8 \strokec8 1\cf0 \strokec4 ;\cb1 \
-\cb3     \}\cb1 \
-\cb3 \}\cb1 \
-\
-}
--- QueueImpl.java
+++ QueueImpl.java
@@ -1,63 +1,47 @@
-{\rtf1\ansi\ansicpg1252\cocoartf2636
-\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fnil\fcharset0 Menlo-Regular;}
-{\colortbl;\red255\green255\blue255;\red15\green112\blue1;\red255\green255\blue255;\red0\green0\blue0;
-\red0\green0\blue255;\red32\green108\blue135;\red0\green0\blue109;\red157\green0\blue210;\red101\green76\blue29;
-\red19\green118\blue70;}
-{\*\expandedcolortbl;;\cssrgb\c0\c50196\c0;\cssrgb\c100000\c100000\c100000;\cssrgb\c0\c0\c0;
-\cssrgb\c0\c0\c100000;\cssrgb\c14902\c49804\c60000;\cssrgb\c0\c6275\c50196;\cssrgb\c68627\c0\c85882;\cssrgb\c47451\c36863\c14902;
-\cssrgb\c3529\c52549\c34510;}
-\margl1440\margr1440\vieww11520\viewh8400\viewkind0
-\deftab720
-\pard\pardeftab720\partightenfactor0
-
-\f0\fs26 \cf2 \cb3 \expnd0\expndtw0\kerning0
-\outl0\strokewidth0 \strokec2 //Time complexity is O(N)\cf0 \cb1 \strokec4 \
-\cf2 \cb3 \strokec2 //Space complexity is O(N)\cf0 \cb1 \strokec4 \
-\cf2 \cb3 \strokec2 //Able to submit in leetcode\cf0 \cb1 \strokec4 \
-\pard\pardeftab720\partightenfactor0
-\cf5 \cb3 \strokec5 class\cf0 \strokec4  \cf6 \strokec6 MyQueue\cf0 \strokec4  \{\cb1 \
-\pard\pardeftab720\partightenfactor0
-\cf0 \cb3     \cf6 \strokec6 Stack\cf0 \strokec4 <\cf6 \strokec6 Integer\cf0 \strokec4 > \cf7 \strokec7 front\cf0 \strokec4  = \cf8 \strokec8 new\cf0 \strokec4  \cf6 \strokec6 Stack\cf0 \strokec4 <>();\cb1 \
-\cb3     \cf6 \strokec6 Stack\cf0 \strokec4 <\cf6 \strokec6 Integer\cf0 \strokec4 > \cf7 \strokec7 end\cf0 \strokec4  = \cf8 \strokec8 new\cf0 \strokec4  \cf6 \strokec6 Stack\cf0 \strokec4 <>();\cb1 \
-\cb3     \cf5 \strokec5 public\cf0 \strokec4  \cf9 \strokec9 MyQueue\cf0 \strokec4 () \{\cb1 \
-\cb3         \cb1 \
-\cb3     \}\cb1 \
-\cb3     \cb1 \
-\cb3     \cf5 \strokec5 public\cf0 \strokec4  \cf6 \strokec6 void\cf0 \strokec4  \cf9 \strokec9 push\cf0 \strokec4 (\cf6 \strokec6 int\cf0 \strokec4  \cf7 \strokec7 x\cf0 \strokec4 ) \{\cb1 \
-\cb3         \cf7 \strokec7 front\cf0 \strokec4 .\cf9 \strokec9 push\cf0 \strokec4 (x);\cb1 \
-\cb3     \}\cb1 \
-\cb3     \cb1 \
-\cb3     \cf5 \strokec5 public\cf0 \strokec4  \cf6 \strokec6 int\cf0 \strokec4  \cf9 \strokec9 pop\cf0 \strokec4 () \{\cb1 \
-\cb3         \cf8 \strokec8 if\cf0 \strokec4 (!\cf7 \strokec7 end\cf0 \strokec4 .\cf9 \strokec9 isEmpty\cf0 \strokec4 ())\{\cb1 \
-\cb3             \cf8 \strokec8 return\cf0 \strokec4  \cf7 \strokec7 end\cf0 \strokec4 .\cf9 \strokec9 pop\cf0 \strokec4 ();\cb1 \
-\cb3         \}\cb1 \
-\cb3         \cf8 \strokec8 else\cf0 \strokec4 \{\cb1 \
-\cb3             \cf6 \strokec6 int\cf0 \strokec4  \cf7 \strokec7 result\cf0 \strokec4 =-\cf10 \strokec10 1\cf0 \strokec4 ;\cb1 \
-\cb3             \cf8 \strokec8 while\cf0 \strokec4 (!\cf7 \strokec7 front\cf0 \strokec4 .\cf9 \strokec9 isEmpty\cf0 \strokec4 ())\{\cb1 \
-\cb3                 result = \cf7 \strokec7 front\cf0 \strokec4 .\cf9 \strokec9 pop\cf0 \strokec4 ();\cb1 \
-\cb3                 \cf7 \strokec7 end\cf0 \strokec4 .\cf9 \strokec9 push\cf0 \strokec4 (result);\cb1 \
-\cb3             \}\cb1 \
-\cb3             \cf7 \strokec7 end\cf0 \strokec4 .\cf9 \strokec9 pop\cf0 \strokec4 ();\cb1 \
-\cb3             \cf8 \strokec8 return\cf0 \strokec4  result;\cb1 \
-\cb3         \}\cb1 \
-\cb3     \}\cb1 \
-\cb3     \cb1 \
-\cb3     \cf5 \strokec5 public\cf0 \strokec4  \cf6 \strokec6 int\cf0 \strokec4  \cf9 \strokec9 peek\cf0 \strokec4 () \{\cb1 \
-\cb3         \cf8 \strokec8 if\cf0 \strokec4 (!\cf7 \strokec7 end\cf0 \strokec4 .\cf9 \strokec9 isEmpty\cf0 \strokec4 ())\{\cb1 \
-\cb3             \cf8 \strokec8 return\cf0 \strokec4  \cf7 \strokec7 end\cf0 \strokec4 .\cf9 \strokec9 peek\cf0 \strokec4 ();\cb1 \
-\cb3         \}\cb1 \
-\cb3         \cf8 \strokec8 else\cf0 \strokec4 \{\cb1 \
-\cb3             \cf6 \strokec6 int\cf0 \strokec4  \cf7 \strokec7 result\cf0 \strokec4  = -\cf10 \strokec10 1\cf0 \strokec4 ;\cb1 \
-\cb3             \cf8 \strokec8 while\cf0 \strokec4 (!\cf7 \strokec7 front\cf0 \strokec4 .\cf9 \strokec9 isEmpty\cf0 \strokec4 ())\{\cb1 \
-\cb3                 result = \cf7 \strokec7 front\cf0 \strokec4 .\cf9 \strokec9 pop\cf0 \strokec4 ();\cb1 \
-\cb3                 \cf7 \strokec7 end\cf0 \strokec4 .\cf9 \strokec9 push\cf0 \strokec4 (result);\cb1 \
-\cb3             \}\cb1 \
-\cb3             \cf8 \strokec8 return\cf0 \strokec4  result;\cb1 \
-\cb3         \}\cb1 \
-\cb3     \}\cb1 \
-\cb3     \cb1 \
-\cb3     \cf5 \strokec5 public\cf0 \strokec4  \cf6 \strokec6 boolean\cf0 \strokec4  \cf9 \strokec9 empty\cf0 \strokec4 () \{\cb1 \
-\cb3         \cf8 \strokec8 return\cf0 \strokec4  \cf7 \strokec7 front\cf0 \strokec4 .\cf9 \strokec9 isEmpty\cf0 \strokec4 () && \cf7 \strokec7 end\cf0 \strokec4 .\cf9 \strokec9 isEmpty\cf0 \strokec4 ();\cb1 \
-\cb3     \}\cb1 \
-\cb3 \}\cb1 \
+//Time complexity is O(N)
+//Space complexity is O(N)
+//Able to submit in leetcode
+class MyQueue {
+    Stack<Integer> front = new Stack<>();
+    Stack<Integer> end = new Stack<>();
+    public MyQueue() {
+        
+    }
+    
+    public void push(int x) {
+        front.push(x);
+    }
+    
+    public int pop() {
+        if(!end.isEmpty()){
+            return end.pop();
+        }
+        else{
+            int result=-1;
+            while(!front.isEmpty()){
+                result = front.pop();
+                end.push(result);
+            }
+            end.pop();
+            return result;
+        }
+    }
+    
+    public int peek() {
+        if(!end.isEmpty()){
+            return end.peek();
+        }
+        else{
+            int result = -1;
+            while(!front.isEmpty()){
+                result = front.pop();
+                end.push(result);
+            }
+            return result;
+        }
+    }
+    
+    public boolean empty() {
+        return front.isEmpty() && end.isEmpty();
+    }
 }
