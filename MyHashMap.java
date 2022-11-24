fad72fef064e6eeb9730c90a9ef733d7669ed563
Arbind Singh <getarbind@gmail.com>
2022-11-24 16:15:07 -0500

    commit2

--- MyHashMap.java
+++ MyHashMap.java
@@ -1,3 +1,11 @@
+// Time Complexity : O(1)
+// Space Complexity : O(n)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : No
+
+
+// Your code here along with comments explaining your approach - Imepleneted using hashing and chaining 
+
 class MyHashMap {
     Node[] bucket;
     int bucketSize;
