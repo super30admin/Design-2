b10c371227c0a32fd763d0b7f510c4b4cad027e8
Swapnil Katkar <skatkar225@gmail.com>
2023-05-14 20:43:07 +0530

    Update MyHashSet.java

--- MyHashSet.java
+++ MyHashSet.java
@@ -1,55 +1,69 @@
 import java.util.ArrayList;
 import java.util.List;
 
-public class MyHashSet {
+public 
+class MyHashSet {
+  private Bucket[] bucketArray;
+  private int keyRange;
 
-    final int ARRAY_SIZE = 100;
+  /** Initialize your data structure here. */
+  public MyHashSet() {
+    this.keyRange = 769;
+    this.bucketArray = new Bucket[this.keyRange];
+    for (int i = 0; i < this.keyRange; ++i)
+      this.bucketArray[i] = new Bucket();
+  }
 
-    List<List<Integer>> mainList;
+  protected int _hash(int key) {
+    return (key % this.keyRange);
+  }
 
-    public MyHashSet() {
-        mainList = new ArrayList<>(ARRAY_SIZE);
-        for(int i=0; i< ARRAY_SIZE; i++){
-            mainList.add(null);
-        }
-    }
+  public void add(int key) {
+    int bucketIndex = this._hash(key);
+    this.bucketArray[bucketIndex].insert(key);
+  }
 
-    public void add(int key) {
-        int index = findIndex(key);
-        List<Integer> childList = mainList.get(index);
+  public void remove(int key) {
+    int bucketIndex = this._hash(key);
+    this.bucketArray[bucketIndex].delete(key);
+  }
 
-        if(childList == null) {
-            List<Integer> newList = new ArrayList<>();
-            newList.add(key);
-            mainList.set(index,newList);
-        }else{
-            childList.add(key);
-        }
-    }
-
-    public void remove(int key) {
-
-    }
-
-    public boolean contains(int key) {
-        int index = findIndex(key);
-
-        List<Integer> childList = mainList.get(index);
-        if(childList != null){
-            return childList.contains(key);
-        }
-        return false;
-    }
-
-    private int findIndex(int key) {
-        return key % ARRAY_SIZE;
-    }
-
-    public static void main(String[] args) {
-        MyHashSet obj = new MyHashSet();
-        obj.add(3);
-        obj.add(4);
-        boolean param_3 = obj.contains(30);
-        System.out.println("param_3 = " + param_3);
-    }
+  /** Returns true if this set contains the specified element */
+  public boolean contains(int key) {
+    int bucketIndex = this._hash(key);
+    return this.bucketArray[bucketIndex].exists(key);
+  }
 }
+
+
+class Bucket {
+  private LinkedList<Integer> container;
+
+  public Bucket() {
+    container = new LinkedList<Integer>();
+  }
+
+  public void insert(Integer key) {
+    int index = this.container.indexOf(key);
+    if (index == -1) {
+      this.container.addFirst(key);
+    }
+  }
+
+  public void delete(Integer key) {
+    this.container.remove(key);
+  }
+
+  public boolean exists(Integer key) {
+    int index = this.container.indexOf(key);
+    return (index != -1);
+  }
+}
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet obj = new MyHashSet();
+ * obj.add(key);
+ * obj.remove(key);
+ * boolean param_3 = obj.contains(key);
+ */
