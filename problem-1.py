f5391b4cbdb04bf35f9ea11a79179c355060a8eb
tnagire1 <ntssteja@gmail.com>
2023-05-24 18:54:18 -0700

    Design-2 Completed

---
+++ problem-1.py
@@ -0,0 +1,25 @@
+class MyQueue:
+
+    def __init__(self):
+        self.items = []
+
+    def push(self, x: int) -> None:
+        self.items.append(x)
+
+    def pop(self) -> int:
+
+        return self.items.pop(0)
+
+    def peek(self) -> int:
+         return self.items[0]
+
+    def empty(self) -> bool:
+        return self.items == []
+
+
+# Your MyQueue object will be instantiated and called as such:
+# obj = MyQueue()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.peek()
+# param_4 = obj.empty()
