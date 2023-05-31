3f4459f6a256a9612887abf659c56876f4a37e13
Sana Pathan <sanapathan28@gmail.com>
2023-05-30 19:37:14 -0700

    706. Design HashMap 74. Search a 2D Matrix 232. Implement Queue using Stacks

---
+++ MyQueue.java
@@ -0,0 +1,48 @@
+import java.util.Stack;
+
+class MyQueue {
+
+    Stack<Integer> st;
+
+    /** Initialize your data structure here. */
+    public MyQueue() {
+        st = new Stack();
+    }
+
+    /** Push element x to the back of queue. */
+    public void push(int x) {
+        // st.push(x);
+        Stack<Integer> temp = new Stack();
+        while(!st.isEmpty()){
+            temp.push(st.pop());
+        }
+        st.push(x);
+        while(!temp.isEmpty()){
+            st.push(temp.pop());
+        }
+    }
+
+    /** Removes the element from in front of queue and returns that element. */
+    public int pop() {
+        return st.pop();
+    }
+
+    /** Get the front element. */
+    public int peek() {
+        return st.peek();
+    }
+
+    /** Returns whether the queue is empty. */
+    public boolean empty() {
+        return st.isEmpty();
+    }
+}
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue obj = new MyQueue();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.peek();
+ * boolean param_4 = obj.empty();
+ */
