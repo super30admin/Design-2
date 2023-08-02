2d6df0b64ceb26901bb8dd95d0db1bdd8379b81a
agill2144 <120353980+agill2144@users.noreply.github.com>
2023-08-01 21:14:29 -0700

    Update main.go

--- queueUsingStacks/main.go
+++ queueUsingStacks/main.go
@@ -1,56 +1,72 @@
-package main
-
+/*
+    push(1)
+    push(2)
+    push(3)
+    
+    in a queue = [1,2,3]
+    
+    in a stack = [1,2,3]
+    
+    so pushes are identical
+    
+    pop() - in stack removes top, in queue removes first
+    which means inorder to return 1 , we need to flip the stack into a new stack
+    therefore we need 2 stacks
+    
+*/
 type MyQueue struct {
-	s1, s2 []int
+    st1 []int
+    st2 []int
 }
+
 
 func Constructor() MyQueue {
-	return MyQueue{
-		s1: []int{},
-		s2: []int{},
-	}
+    return MyQueue{
+        st1: []int{},
+        st2: []int{},
+    }
 }
 
-// time: o(1)
-// space: o(1)
-func (this *MyQueue) Push(x int) {
-	this.s1 = append(this.s1, x)
+
+// time ; o(1)
+// space ; o(1)
+func (this *MyQueue) Push(x int)  {
+    this.st1 = append(this.st1, x)
 }
 
-// time: o(n)
-// space: o(1)
-func (this *MyQueue) flipOneIntoTwo() {
-	if len(this.s2) > 0 {
-		return
-	}
-	for len(this.s1) != 0 {
-		top := this.s1[len(this.s1)-1]
-		this.s1 = this.s1[:len(this.s1)-1]
-		this.s2 = append(this.s2, top)
-	}
+// time ; o(n) ; worst case
+// space ; o(1)
+func(this *MyQueue) flipSt1IntoSt2() {
+    if len(this.st2) > 0 {return}
+    for len(this.st1) != 0 {
+        top := this.st1[len(this.st1)-1]
+        this.st1 = this.st1[:len(this.st1)-1]
+        this.st2 = append(this.st2, top)
+    }
 }
 
-// time: o(n) - worse case, in best case this will be o(1)
-// space: o(1)
+
+// time ; o(n) ; worst case if we need to flip
+// space ; o(1)
 func (this *MyQueue) Pop() int {
-	this.flipOneIntoTwo()
-	top := this.s2[len(this.s2)-1]
-	this.s2 = this.s2[:len(this.s2)-1]
-	return top
+    this.flipSt1IntoSt2()
+    top := this.st2[len(this.st2)-1]
+    this.st2 = this.st2[:len(this.st2)-1]
+    return top
 }
 
-// time: o(n) - worse case, in best case this will be o(1)
-// space: o(1)
+// time ; o(n) ; worst case if we need to flip
+// space ; o(1)
 func (this *MyQueue) Peek() int {
-	this.flipOneIntoTwo()
-	return this.s2[len(this.s2)-1]
+    this.flipSt1IntoSt2()
+    return this.st2[len(this.st2)-1]
 }
 
-// time: o(1)
-// space: o(1)
+
 func (this *MyQueue) Empty() bool {
-	return len(this.s1) == 0 && len(this.s2) == 0
+    return len(this.st1) == 0 && len(this.st2) == 0 
 }
+
 
 /**
  * Your MyQueue object will be instantiated and called as such:
