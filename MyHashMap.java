78e6a90acd8fd957bcd8637c7f92935660645c0b
harichandanamone <harichandanamone@gmail.com>
2023-08-31 01:28:58 -0500

    Completed Design-2

---
+++ MyHashMap.java
@@ -0,0 +1,82 @@
+// Time complexity : put- O(N), get-O(n),remove - O(n)
+// Space complexity : O(1)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : No
+
+class MyHashMap {
+    class Node{
+        int key;
+        int value;
+        Node next;
+        public Node(int key,int value){
+            this.key = key;
+            this.value = value;
+        }
+    }
+    Node find(Node dummy, int key){
+        Node prev = dummy;
+        Node curr = dummy.next;
+        while(curr != null && curr.key != key){
+            prev = curr;
+            curr = curr.next;
+        }
+        return prev;
+    }
+
+    int buckets;
+    Node[] storage;
+
+    int getBucket(int key){
+        return Integer.hashCode(key) %buckets;
+    }
+
+    public MyHashMap() {
+        buckets = 10000;
+        storage = new Node[buckets];
+
+    }
+
+    public void put(int key, int value) {
+        int bucket = getBucket(key);
+        if(storage[bucket] == null){
+            storage[bucket] = new Node(-1,-1);
+        }
+        Node prev = find(storage[bucket], key);
+        if(prev.next == null){
+            prev.next = new Node(key,value);
+        }else{
+            prev.next.value = value;
+        }
+
+    }
+
+    public int get(int key) {
+        int bucket = getBucket(key);
+        if(storage[bucket] == null) return -1;
+        Node prev = find(storage[bucket],key);
+        if(prev.next == null){
+            return -1;
+        }
+        return prev.next.value;
+
+    }
+
+    public void remove(int key) {
+        int bucket = getBucket(key);
+        if(storage[bucket] == null) return;
+        Node prev = find(storage[bucket],key);
+        if(prev .next == null){
+            return;
+        }
+        prev.next = prev.next.next;
+
+    }
+}
+
+/**
+ * Your MyHashMap object will be instantiated and called as such:
+ * MyHashMap obj = new MyHashMap();
+ * obj.put(key,value);
+ * int param_2 = obj.get(key);
+ * obj.remove(key);
+ */
