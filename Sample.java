8a84ec9860dd6e2c77417176694043f54ee9aa06
Rucha Tambe <rtambe@usc.edu>
2023-10-01 11:59:23 -0700

    Completed Queue using 2 stacks

--- Sample.java
+++ Sample.java
@@ -1,7 +1,59 @@
 // Time Complexity :
-// Space Complexity :
-// Did this code successfully run on Leetcode :
-// Any problem you faced while coding this :
-
+// 			1. Push Operation: O(1)
+// 			2. Pop Operation: Worst case O(n), Amortized O(1)
+// 			3. Peek Operation: Worst case O(n), Amortized O(1)
+// 			4. empty(): O(1)
+// Space Complexity : O(n)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : None
 
 // Your code here along with comments explaining your approach
+class MyQueue {
+    Stack<Integer> inStack;
+    Stack<Integer> outStack;
+
+    public MyQueue() {
+        inStack = new Stack<Integer>();
+        outStack = new Stack<Integer>();
+    }
+    
+    public void push(int x) {
+        inStack.push(x);
+    }
+    
+    public int pop() {
+        if(empty())
+            return 0;
+        //Outstack maintains the correct order of elements. Pop elements from inStack and push them into outstack 1 by 1
+        if(outStack.isEmpty()){
+            while(!inStack.isEmpty()){
+                outStack.push(inStack.pop());
+            }
+            
+        }
+        return outStack.pop();
+    }
+    
+    public int peek() {
+         if(outStack.isEmpty()){
+            while(!inStack.isEmpty()){
+                outStack.push(inStack.pop());
+            }
+            
+        }
+        return outStack.peek();
+    }
+    
+    public boolean empty() {
+        return inStack.isEmpty() && outStack.isEmpty();
+    }
+}
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue obj = new MyQueue();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.peek();
+ * boolean param_4 = obj.empty();
+ */
