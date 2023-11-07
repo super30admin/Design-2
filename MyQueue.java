3d6ae701b2911f1f11760eb478ec18a723db60a9
sirisha pydimarri <sirishapydimarri23@gmail.com>
2023-11-07 19:05:38 -0400

    FAANG Problem #2 QUeue implementation LC-232

---
+++ MyQueue.java
@@ -0,0 +1,40 @@
+//Implementing Queue using two stacks with amortized cost of each operation O(1)
+class MyQueue {
+
+    Stack<Integer> input = new Stack();
+    Stack<Integer> output = new Stack();
+
+    public void push(int x) {
+        input.push(x);
+    }
+
+    //Once you get front element from peek, just pop it from Output
+    public int pop() {
+        int value = peek();
+        output.pop();
+        return value;
+    }
+
+    //Peek is popping from input and pushing to output stack so that sequence is reversed
+    //and peek shows the front element as Queue is FIFO
+    public int peek() {
+        if(output.empty())
+            while(!input.empty())
+                output.push(input.pop());
+        return output.peek();
+    }
+
+    //Return true only if both the stacks are empty
+    public boolean empty() {
+        return input.empty() && output.empty();
+    }
+}
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue obj = new MyQueue();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.peek();
+ * boolean param_4 = obj.empty();
+ */
