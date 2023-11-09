fdd511f6c488931b1beb7ba5dc49feb3cf70a9d4
yashfirke <98770091+yashfirke@users.noreply.github.com>
2023-11-08 23:17:18 -0500

    Added Both Implementation

--- MyHashMap.java
+++ MyHashMap.java
@@ -1,26 +1,84 @@
 import java.util.Arrays;
 
+// Time Complexity :O(1)
+// Space Complexity :O(1)
+// Did this code successfully run on Leetcode : yes
+// Any problem you faced while coding this :
+
+
+// Your code here along with comments explaining your approach
+//It utilizes an array of linked lists (buckets) to store key-value pairs,
+// employing modular hashing to determine the index for insertion, retrieval, and deletion operations.
 public class MyHashMap {
-    int[] arr;
+        Node[] storage;
+        int buckets;
+class Node{
+    int key;
+    int value;
+    Node next;
+
+    public Node(int key, int value){
+        this.key = key;
+        this.value = value;
+    }
+}
 
     public MyHashMap() {
-        arr = new int[1000001];
-        Arrays.fill(arr,-1);
-
+        this.buckets = 10000;
+        storage = new Node[buckets];
+    }
+    private int getHash(int key){
+        return key % buckets;
     }
 
+    private Node findPrev(Node head, int key){
+        Node prev = null;
+        Node curr = head;
+        while(curr!=null && curr.key !=key){
+            prev = curr;
+            curr = curr.next;
+        }
+        return prev;
+    }
+    // Prev gives the latest node
     public void put(int key, int value) {
-        arr[key] = value;
-
+        int hash = getHash(key);
+        if(storage[hash] == null){
+            storage[hash]= new Node(-1,-1);
+            storage[hash].next = new Node(key, value);
+        }else{
+            Node prev = findPrev(storage[hash],key);
+            if(prev.next == null){
+                prev.next = new Node(key,value);
+            }else{
+                prev.next.value = value;
+            }
+        }
     }
 
     public int get(int key) {
-        return arr[key];
+        int hash = getHash(key);
+        if(storage[hash] == null)return -1;
+        Node prev = findPrev(storage[hash], key);
+        if(prev.next == null) return -1;
+        return prev.next.value;
 
     }
 
     public void remove(int key) {
-        arr[key]=-1;
+        int hash = getHash(key);
+        if(storage[hash] == null) return;
+        Node prev = findPrev(storage[hash], key);
+        if(prev.next == null) return;
+        prev.next = prev.next.next;
 
     }
 }
+
+/**
+ * Your MyHashMap object will be instantiated and called as such:
+ * MyHashMap obj = new MyHashMap();
+ * obj.put(key,value);
+ * int param_2 = obj.get(key);
+ * obj.remove(key);
+ */
